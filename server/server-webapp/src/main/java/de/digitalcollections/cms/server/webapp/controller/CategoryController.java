ba1bb831f94501c2e2ab3313e156338de5b027a7
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2017-06-09 14:34:38 +0200

    Refactor client server modules

--- server/server-webapp/src/main/java/de/digitalcollections/cms/server/webapp/controller/CategoryController.java
+++
@@ -1,9 +0,0 @@
-package de.digitalcollections.cms.server.webapp.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/category")
-public class CategoryController {
-}
