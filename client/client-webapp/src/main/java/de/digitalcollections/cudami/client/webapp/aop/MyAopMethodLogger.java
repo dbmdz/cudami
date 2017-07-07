afcbb2d6a7d77ed13d35f04533e65aa45621fdc7
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2017-07-07 14:46:09 +0200

    Fix compile for client

--- client/client-webapp/src/main/java/de/digitalcollections/cudami/client/webapp/aop/MyAopMethodLogger.java
+++ client/client-webapp/src/main/java/de/digitalcollections/cudami/client/webapp/aop/MyAopMethodLogger.java
@@ -5,7 +5,6 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class MyAopMethodLogger extends AbstractAopMethodLogger {
 
-  // TODO change packagenames for business and backend
   @Pointcut("execution(public * de.digitalcollections.cudami.client.webapp.controller..*Controller.*(..)) || "
           + "execution(public * de.digitalcollections.cudami.client.business.impl.service..*Service.*(..)) || "
           + "execution(public * de.digitalcollections.cudami.client.backend.impl.repository..*.*Repository.*(..))")
