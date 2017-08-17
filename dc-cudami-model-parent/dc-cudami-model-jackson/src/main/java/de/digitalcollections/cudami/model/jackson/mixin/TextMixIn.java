360f656326b5cff22e72bd684fdf9b1a825f5d5b
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2017-08-17 10:32:48 +0200

    Add ignore of duplicate data fields

--- dc-cudami-model-parent/dc-cudami-model-jackson/src/main/java/de/digitalcollections/cudami/model/jackson/mixin/TextMixIn.java
+++ dc-cudami-model-parent/dc-cudami-model-jackson/src/main/java/de/digitalcollections/cudami/model/jackson/mixin/TextMixIn.java
@@ -1,11 +1,18 @@
 package de.digitalcollections.cudami.model.jackson.mixin;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import de.digitalcollections.cudami.model.impl.TextImpl;
+import java.util.Collection;
 
 @JsonDeserialize(as = TextImpl.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public interface TextMixIn {
 
+  @JsonIgnore
+  Collection<String> getLanguages();
+
+  @JsonIgnore
+  String getText();
 }
