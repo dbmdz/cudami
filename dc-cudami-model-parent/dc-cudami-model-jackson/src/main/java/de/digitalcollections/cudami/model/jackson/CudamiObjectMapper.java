e1a975bffbb96abac67fd06cf9274e585c8e92f8
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2017-08-17 10:28:15 +0200

    Serialize LocalDateTime not as list of integers but as timestamp string

--- dc-cudami-model-parent/dc-cudami-model-jackson/src/main/java/de/digitalcollections/cudami/model/jackson/CudamiObjectMapper.java
+++ dc-cudami-model-parent/dc-cudami-model-jackson/src/main/java/de/digitalcollections/cudami/model/jackson/CudamiObjectMapper.java
@@ -3,6 +3,7 @@ package de.digitalcollections.cudami.model.jackson;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import de.digitalcollections.core.model.jackson.DcCoreModelModule;
 
@@ -19,6 +20,7 @@ public class CudamiObjectMapper extends ObjectMapper {
     objectMapper.registerModule(new CudamiModule());
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
     objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     return objectMapper;
   }
 }
