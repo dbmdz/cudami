76b809705b58b77a0b679aa232e3fdf1ba5c772d
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2017-08-30 13:06:08 +0200

    Fix test: Added missing repository

--- dc-cudami-client/dc-cudami-client-business/src/test/java/de/digitalcollections/cudami/config/SpringConfigBackendForTest.java
+++ dc-cudami-client/dc-cudami-client-business/src/test/java/de/digitalcollections/cudami/config/SpringConfigBackendForTest.java
@@ -1,5 +1,6 @@
 package de.digitalcollections.cudami.config;
 
+import de.digitalcollections.cudami.client.backend.api.repository.LocaleRepository;
 import de.digitalcollections.cudami.client.backend.api.repository.UserRepository;
 import de.digitalcollections.cudami.client.backend.api.repository.WebsiteRepository;
 import org.mockito.Mockito;
@@ -11,6 +12,11 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SpringConfigBackendForTest {
+
+  @Bean
+  public LocaleRepository localeRepository() {
+    return Mockito.mock(LocaleRepository.class);
+  }
 
   @Bean
   public UserRepository userRepository() {
