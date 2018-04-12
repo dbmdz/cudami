d9962ff4fc4264e6217d7f1bcba523bb8347e821
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2018-04-12 16:23:22 +0200

    Fixed jsondoc

--- dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/config/SpringConfigWeb.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/config/SpringConfigWeb.java
@@ -2,8 +2,11 @@ package de.digitalcollections.cudami.server.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.digitalcollections.cudami.model.jackson.CudamiObjectMapper;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.MediaType;
@@ -21,13 +24,37 @@ public class SpringConfigWeb extends WebMvcConfigurerAdapter implements Initiali
   @Autowired
   ObjectMapper objectMapper;
 
+  @Value("${server.port:80}")
+  Integer port;
+
+  @Value("${info.app.project.version:unknown}")
+  String projectVersion;
+
   @Override
   public void afterPropertiesSet() throws Exception {
     // customize default spring boot jackson objectmapper
     CudamiObjectMapper.customize(objectMapper);
+
+    setupJsondoc();
   }
 
-//  @Bean
+  private void setupJsondoc() {
+    String hostName;
+    try {
+      InetAddress addr = InetAddress.getLocalHost();
+      hostName = addr.getCanonicalHostName();
+      if (!hostName.contains(".")) {
+        hostName = "localhost";
+      }
+    } catch (UnknownHostException e) {
+      hostName = "localhost";
+    }
+
+    System.setProperty("jsondoc.basePath", "http://" + hostName + ":" + port);
+    System.setProperty("jsondoc.version", projectVersion);
+  }
+
+  //  @Bean
 //  public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 //    return new PropertySourcesPlaceholderConfigurer();
 //  }
