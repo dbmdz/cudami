f092ac4a12eba1bff6cd692d183ccbb5ee683cd8
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2018-04-17 13:53:30 +0200

    Fix relations of webpage to resource and node

--- dc-cudami-model-parent/dc-cudami-model/src/main/java/de/digitalcollections/cudami/model/impl/identifiable/resource/WebpageImpl.java
+++ dc-cudami-model-parent/dc-cudami-model/src/main/java/de/digitalcollections/cudami/model/impl/identifiable/resource/WebpageImpl.java
@@ -1,21 +1,55 @@
 package de.digitalcollections.cudami.model.impl.identifiable.resource;
 
+import de.digitalcollections.cudami.model.api.identifiable.Identifiable;
+import de.digitalcollections.cudami.model.api.identifiable.Node;
 import de.digitalcollections.cudami.model.api.identifiable.parts.Text;
 import de.digitalcollections.cudami.model.api.identifiable.resource.ContentBlocksContainer;
 import de.digitalcollections.cudami.model.api.identifiable.resource.ResourceType;
 import de.digitalcollections.cudami.model.api.identifiable.resource.Webpage;
 import de.digitalcollections.cudami.model.impl.identifiable.NodeImpl;
+import java.util.List;
 
-public class WebpageImpl extends NodeImpl<Webpage> implements Webpage {
+public class WebpageImpl extends ResourceImpl implements Webpage, Node<Webpage> {
 
   private Text contentBlocks;
 
   private ContentBlocksContainer contentBlocksContainer;
-  private ResourceType resourceType;
+  private final NodeImpl<Webpage> node;
 
   public WebpageImpl() {
     super();
+    this.node = new NodeImpl<>();
     this.resourceType = ResourceType.WEBPAGE;
+  }
+
+  @Override
+  public void addChild(Webpage child) {
+    node.addChild(child);
+  }
+
+  @Override
+  public void addContent(Identifiable identifiable) {
+    node.addContent(identifiable);
+  }
+
+  @Override
+  public List<Webpage> getChildren() {
+    return node.getChildren();
+  }
+
+  @Override
+  public void setChildren(List<Webpage> children) {
+    node.setChildren(children);
+  }
+
+  @Override
+  public List<Identifiable> getContent() {
+    return node.getContent();
+  }
+
+  @Override
+  public void setContent(List<Identifiable> content) {
+    node.setContent(content);
   }
 
   @Override
@@ -36,6 +70,16 @@ public class WebpageImpl extends NodeImpl<Webpage> implements Webpage {
   @Override
   public void setContentBlocksContainer(ContentBlocksContainer contentBlocksContainer) {
     this.contentBlocksContainer = contentBlocksContainer;
+  }
+
+  @Override
+  public Webpage getParent() {
+    return node.getParent();
+  }
+
+  @Override
+  public void setParent(Webpage parent) {
+    node.setParent(parent);
   }
 
   @Override
