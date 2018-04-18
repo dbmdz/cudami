e6cf8e14ada424d2144f6126a2e2b0a3d9fcc454
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2018-04-18 09:10:38 +0200

    Rename last import of admin webapp package

--- dc-cudami-admin/dc-cudami-admin-business/src/test/java/de/digitalcollections/cudami/config/SpringConfigBackendForTest.java
+++ dc-cudami-admin/dc-cudami-admin-business/src/test/java/de/digitalcollections/cudami/config/SpringConfigBackendForTest.java
@@ -1,9 +1,9 @@
 package de.digitalcollections.cudami.config;
 
-import de.digitalcollections.cudami.client.backend.api.repository.LocaleRepository;
-import de.digitalcollections.cudami.client.backend.api.repository.identifiable.entity.ContentTreeRepository;
-import de.digitalcollections.cudami.client.backend.api.repository.security.UserRepository;
-import de.digitalcollections.cudami.client.backend.api.repository.identifiable.entity.WebsiteRepository;
+import de.digitalcollections.cudami.admin.backend.api.repository.LocaleRepository;
+import de.digitalcollections.cudami.admin.backend.api.repository.identifiable.entity.ContentTreeRepository;
+import de.digitalcollections.cudami.admin.backend.api.repository.identifiable.entity.WebsiteRepository;
+import de.digitalcollections.cudami.admin.backend.api.repository.security.UserRepository;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
