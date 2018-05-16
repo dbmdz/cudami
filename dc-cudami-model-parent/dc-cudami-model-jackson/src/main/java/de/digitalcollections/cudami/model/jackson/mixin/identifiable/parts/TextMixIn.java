6071bcefa7ed3292912cd1daf6cc5ce51b1a081d
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2018-05-16 14:11:00 +0200

    Add locales of text to json ignore

--- dc-cudami-model-parent/dc-cudami-model-jackson/src/main/java/de/digitalcollections/cudami/model/jackson/mixin/identifiable/parts/TextMixIn.java
+++ dc-cudami-model-parent/dc-cudami-model-jackson/src/main/java/de/digitalcollections/cudami/model/jackson/mixin/identifiable/parts/TextMixIn.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import de.digitalcollections.cudami.model.impl.identifiable.parts.TextImpl;
 import java.util.Collection;
+import java.util.Locale;
 
 @JsonDeserialize(as = TextImpl.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -12,6 +13,9 @@ public interface TextMixIn {
 
   @JsonIgnore
   Collection<String> getLanguages();
+
+  @JsonIgnore
+  Collection<Locale> getLocales();
 
   @JsonIgnore
   String getText();
