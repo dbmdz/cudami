bc33b488a8b4a3f75851b4c2c22161fa19da08b6
Stefan Schweter <stefan.schweter@bsb-muenchen.de>
2018-06-29 15:19:38 +0200

    validation: add unit tests for password validation. Maybe a more elegant solution for  can be found later...

--- dc-cudami-admin/dc-cudami-admin-business/src/test/java/de/digitalcollections/cudami/admin/business/impl/validator/PasswordsValidatorTest.java
+++ dc-cudami-admin/dc-cudami-admin-business/src/test/java/de/digitalcollections/cudami/admin/business/impl/validator/PasswordsValidatorTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -42,5 +44,49 @@ public class PasswordsValidatorTest {
     boolean expResult = true;
     boolean result = instance.containsSpecialChar(password);
     assertEquals(expResult, result);
+  }
+
+  @Test
+  public void testValidateMinimumPasswordLengthFalse() {
+    String password = "Hopfen";
+    PasswordsValidatorParams params = new PasswordsValidatorParams(password, password, "hash");
+    PasswordsValidator instance = new PasswordsValidator();
+    Errors errors = new BeanPropertyBindingResult(params, "params");
+
+    instance.validate(params, errors);
+
+    String expectedErrorCode = "error.password_min_length";
+    String result = errors.getGlobalErrors().get(0).getCodes()[1];
+
+    assertEquals(errors.getErrorCount(), 1);
+    assertEquals(expectedErrorCode, result);
+  }
+
+  @Test
+  public void testValidateMaximumPasswordLengthFalse() {
+    String password = "Hopfenzeitung089Hopfenzeitung089";
+    PasswordsValidatorParams params = new PasswordsValidatorParams(password, password, "hash");
+    PasswordsValidator instance = new PasswordsValidator();
+    Errors errors = new BeanPropertyBindingResult(params, "params");
+
+    instance.validate(params, errors);
+
+    String expectedErrorCode = "error.password_max_length";
+    String result = errors.getGlobalErrors().get(0).getCodes()[1];
+
+    assertEquals(errors.getErrorCount(), 1);
+    assertEquals(expectedErrorCode, result);
+  }
+
+  @Test
+  public void testValidateTrue() {
+    String password = "Hopfenzeitung089!";
+    PasswordsValidatorParams params = new PasswordsValidatorParams(password, password, "hash");
+    PasswordsValidator instance = new PasswordsValidator();
+    Errors errors = new BeanPropertyBindingResult(params, "params");
+
+    instance.validate(params, errors);
+
+    assertEquals(errors.getErrorCount(), 0);
   }
 }
