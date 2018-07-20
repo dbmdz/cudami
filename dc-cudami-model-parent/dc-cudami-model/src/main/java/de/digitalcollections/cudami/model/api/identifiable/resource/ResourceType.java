f437ab6742af6bf410dfc254bf639b15bb29b6b4
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2018-07-20 13:50:15 +0200

    Fix resource type of content nodes

--- dc-cudami-model-parent/dc-cudami-model/src/main/java/de/digitalcollections/cudami/model/api/identifiable/resource/ResourceType.java
+++ dc-cudami-model-parent/dc-cudami-model/src/main/java/de/digitalcollections/cudami/model/api/identifiable/resource/ResourceType.java
@@ -4,7 +4,7 @@ package de.digitalcollections.cudami.model.api.identifiable.resource;
  * All {@link Resource} types cudami can handle
  */
 public enum ResourceType {
-  BINARY_CONTENT, CONTENTNODE, IIIF_IMAGE, IIIF_MANIFEST, WEBPAGE;
+  BINARY_CONTENT, CONTENT_NODE, IIIF_IMAGE, IIIF_MANIFEST, WEBPAGE;
 
   @Override
   public String toString() {
