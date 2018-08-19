e145c9591613a55b2d0956823d5b71c2653c90ee
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2018-08-19 21:32:00 +0200

    Code formatting

--- dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/resource/WebpagesController.java
+++ dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/resource/WebpagesController.java
@@ -72,8 +72,9 @@ public class WebpagesController extends AbstractController implements MessageSou
   }
 
   @RequestMapping(value = "/webpages/new", method = RequestMethod.POST)
-  public String create(@ModelAttribute @Valid Webpage webpage, BindingResult results, Model model, SessionStatus status, RedirectAttributes redirectAttributes, @RequestParam("parentType") String parentType, @RequestParam(
-          "parentUuid") UUID parentUuid) {
+  public String create(@ModelAttribute @Valid Webpage webpage, BindingResult results, Model model, SessionStatus status, RedirectAttributes redirectAttributes,
+          @RequestParam("parentType") String parentType,
+          @RequestParam("parentUuid") UUID parentUuid) {
     verifyBinding(results);
     if (results.hasErrors()) {
       model.addAttribute("isNew", true);
