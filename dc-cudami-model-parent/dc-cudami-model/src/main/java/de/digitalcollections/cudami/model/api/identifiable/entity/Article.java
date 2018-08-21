9af1e54c241211811f3ab77fc41c3bf2b0b8fca5
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2018-08-21 13:36:34 +0200

    Remove no longer used model module

--- dc-cudami-model-parent/dc-cudami-model/src/main/java/de/digitalcollections/cudami/model/api/identifiable/entity/Article.java
+++
@@ -1,9 +0,0 @@
-package de.digitalcollections.cudami.model.api.identifiable.entity;
-
-import de.digitalcollections.cudami.model.api.identifiable.Node;
-
-/**
- * Article is used to manage cultural articles and their hierarchy.
- */
-public interface Article extends Entity, Node<Article> {
-}
--- dc-cudami-model-parent/dc-cudami-model/src/main/java/de/digitalcollections/cudami/model/api/identifiable/resource/parts/ContentBlockType.java
+++
@@ -1,8 +0,0 @@
-package de.digitalcollections.cudami.model.api.identifiable.resource.parts;
-
-/**
- * Supported content block types.
- */
-public enum ContentBlockType {
-  HTML, UNORDERED_LIST, ORDERED_LIST, HEADING, IMAGE, AUDIO, VIDEO, IIIF_PRESENTATION;
-}
