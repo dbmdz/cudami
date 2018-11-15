2f9481d575ff5280fb92fdda01edceb508be8305
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2018-11-15 16:18:28 +0100

    Not needed as there is a UserMixin defined already

--- dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/converter/UserJsonFilter.java
+++
@@ -1,9 +0,0 @@
-package de.digitalcollections.cudami.admin.converter;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public abstract class UserJsonFilter {
-
-  @JsonIgnore
-  abstract String getPasswordHash();
-}
