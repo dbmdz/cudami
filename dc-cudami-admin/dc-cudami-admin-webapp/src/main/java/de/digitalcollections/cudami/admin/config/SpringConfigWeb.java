9b14f12015e4139a03552161b14923c9f19ee7ea
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2018-12-12 11:25:32 +0100

    Activate LogSessionIDFilter and add example for logging sessionID

--- dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/config/SpringConfigWeb.java
+++ dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/config/SpringConfigWeb.java
@@ -2,6 +2,7 @@ package de.digitalcollections.cudami.admin.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
+import de.digitalcollections.commons.servlet.filter.LogSessionIdFilter;
 import de.digitalcollections.commons.springmvc.config.SpringConfigCommonsMvc;
 import de.digitalcollections.commons.springmvc.controller.ErrorController;
 import de.digitalcollections.cudami.admin.converter.GrantedAuthorityJsonFilter;
@@ -14,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -121,12 +123,12 @@ public class SpringConfigWeb implements WebMvcConfigurer, InitializingBean {
     configurer.enable();
   }
 
-//  @Bean
-//  public FilterRegistrationBean logSessionIdFilter() {
-//    FilterRegistrationBean registration = new FilterRegistrationBean();
-//    registration.setFilter(new LogSessionIdFilter());
-//    // In case you want the filter to apply to specific URL patterns only (defaults to "/*")
-//    registration.addUrlPatterns("/*");
-//    return registration;
-//  }
+  @Bean
+  public FilterRegistrationBean logSessionIdFilter() {
+    FilterRegistrationBean registration = new FilterRegistrationBean();
+    registration.setFilter(new LogSessionIdFilter());
+    // In case you want the filter to apply to specific URL patterns only (defaults to "/*")
+    registration.addUrlPatterns("/*");
+    return registration;
+  }
 }
