91f5b1a488dda07afb949b2f89e20983f3474798
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-01-28 12:08:52 +0100

    Fix one more spotbugs issue

--- dc-cudami-client/dc-cudami-client-openfeign/src/main/java/de/digitalcollections/cudami/client/feign/exceptions/HttpException.java
+++ dc-cudami-client/dc-cudami-client-openfeign/src/main/java/de/digitalcollections/cudami/client/feign/exceptions/HttpException.java
@@ -4,6 +4,7 @@ import feign.Response;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
 public class HttpException extends Exception {
@@ -13,12 +14,12 @@ public class HttpException extends Exception {
 
   public HttpException(String methodKey, Response response) {
     super(String.format("Got %d for backend call %s.%n⤷ %s",
-            response.status(), methodKey, response.request()));
+      response.status(), methodKey, response.request()));
 
     if (response.body() != null) {
 
       try {
-        try (InputStreamReader ir = new InputStreamReader(response.body().asInputStream())) {
+        try (InputStreamReader ir = new InputStreamReader(response.body().asInputStream(), StandardCharsets.UTF_8)) {
           errorMessage = new BufferedReader(ir).lines().parallel().collect(Collectors.joining(" "));
           errorMessage = errorMessage.replaceFirst("^\"", "").replaceFirst("\"$", "");
         }
