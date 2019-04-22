9b3b8e83b029bd9efa4d313e35f45737e065d03b
ralf <ralf@fujitsu>
2019-04-22 19:11:50 +0200

    Add entity relation

--- dc-cudami-server/dc-cudami-server-backend-api/src/main/java/de/digitalcollections/cudami/server/backend/api/repository/identifiable/entity/EntityRepository.java
+++ dc-cudami-server/dc-cudami-server-backend-api/src/main/java/de/digitalcollections/cudami/server/backend/api/repository/identifiable/entity/EntityRepository.java
@@ -2,10 +2,36 @@ package de.digitalcollections.cudami.server.backend.api.repository.identifiable.
 
 import de.digitalcollections.cudami.server.backend.api.repository.identifiable.IdentifiableRepository;
 import de.digitalcollections.model.api.identifiable.entity.Entity;
+import de.digitalcollections.model.api.identifiable.entity.EntityRelation;
+import de.digitalcollections.model.api.identifiable.resource.FileResource;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @param <E> entity instance
  */
 public interface EntityRepository<E extends Entity> extends IdentifiableRepository<E> {
 
+  void addRelation(EntityRelation<E> relation);
+
+  void addRelation(UUID subjectEntityUuid, String predicate, UUID objectEntityUuid);
+
+  List<EntityRelation> getRelations(E subjectEntity);
+
+  List<EntityRelation> getRelations(UUID subjectEntityUuid);
+
+  List<EntityRelation> saveRelations(List<EntityRelation> relations);
+
+  void addRelatedFileresource(E entity, FileResource fileResource);
+
+  void addRelatedFileresource(UUID entityUuid, UUID fileResourceUuid);
+
+  LinkedHashSet<FileResource> getRelatedFileResources(E entity);
+
+  LinkedHashSet<FileResource> getRelatedFileResources(UUID entityUuid);
+
+  LinkedHashSet<FileResource> saveRelatedFileResources(E entity, LinkedHashSet<FileResource> fileResources);
+
+  LinkedHashSet<FileResource> saveRelatedFileResources(UUID entityUuid, LinkedHashSet<UUID> fileResourcesUuids);
 }
---
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/entity/EntityRelationMapper.java
@@ -0,0 +1,52 @@
+package de.digitalcollections.cudami.server.backend.impl.jdbi.identifiable.entity;
+
+import de.digitalcollections.model.api.identifiable.entity.Entity;
+import de.digitalcollections.model.api.identifiable.entity.EntityRelation;
+import de.digitalcollections.model.api.identifiable.entity.enums.EntityType;
+import de.digitalcollections.model.api.identifiable.parts.LocalizedText;
+import de.digitalcollections.model.api.identifiable.parts.structuredcontent.LocalizedStructuredContent;
+import de.digitalcollections.model.impl.identifiable.entity.EntityImpl;
+import de.digitalcollections.model.impl.identifiable.entity.EntityRelationImpl;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+import org.jdbi.v3.core.config.ConfigRegistry;
+import org.jdbi.v3.core.mapper.ColumnMapper;
+import org.jdbi.v3.core.mapper.ColumnMappers;
+import org.jdbi.v3.core.mapper.NoSuchMapperException;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+public class EntityRelationMapper<E extends Entity> implements RowMapper<EntityRelation> {
+
+  private final E subjectEntity;
+
+  public EntityRelationMapper(E subjectEntity) {
+    this.subjectEntity = subjectEntity;
+  }
+
+  @Override
+  public EntityRelation map(ResultSet rs, StatementContext ctx) throws SQLException {
+    ConfigRegistry config = ctx.getConfig();
+    ColumnMappers columnMappers = config.get(ColumnMappers.class);
+    ColumnMapper<LocalizedStructuredContent> lscMapper = columnMappers.findFor(LocalizedStructuredContent.class).orElseThrow(() -> new NoSuchMapperException("LocalizedStructuredContent"));
+    ColumnMapper<LocalizedText> ltMapper = columnMappers.findFor(LocalizedText.class).orElseThrow(() -> new NoSuchMapperException("LocalizedText"));
+
+    EntityRelation result = new EntityRelationImpl();
+
+    result.setSubject(subjectEntity);
+    result.setPredicate(rs.getString("predicate"));
+
+    Entity objectEntity = new EntityImpl();
+    objectEntity.setCreated(rs.getTimestamp("created").toLocalDateTime());
+    objectEntity.setDescription(lscMapper.map(rs, "description", ctx));
+    objectEntity.setEntityType(EntityType.valueOf(rs.getString("entity_type")));
+    objectEntity.setLabel(ltMapper.map(rs, "label", ctx));
+    objectEntity.setLastModified(rs.getTimestamp("last_modified").toLocalDateTime());
+//    objectEntity.setType(IdentifiableType.valueOf(rs.getString("identifiable_type"))); // set in constructor
+    objectEntity.setUuid(rs.getObject("uuid", UUID.class));
+
+    result.setObject(objectEntity);
+    return result;
+  }
+}
