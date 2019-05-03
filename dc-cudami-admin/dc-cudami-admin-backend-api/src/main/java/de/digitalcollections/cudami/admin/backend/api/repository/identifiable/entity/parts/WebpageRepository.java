a3e7f8efafceb62933a58a2488560c9ebff0d585
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2019-05-03 16:26:55 +0200

    Refactor admin backend api

--- dc-cudami-admin/dc-cudami-admin-backend-api/src/main/java/de/digitalcollections/cudami/admin/backend/api/repository/identifiable/entity/parts/WebpageRepository.java
+++ dc-cudami-admin/dc-cudami-admin-backend-api/src/main/java/de/digitalcollections/cudami/admin/backend/api/repository/identifiable/entity/parts/WebpageRepository.java
@@ -1,20 +1,18 @@
 package de.digitalcollections.cudami.admin.backend.api.repository.identifiable.entity.parts;
 
-import de.digitalcollections.cudami.admin.backend.api.repository.identifiable.IdentifiablesContainerRepository;
 import de.digitalcollections.cudami.admin.backend.api.repository.identifiable.NodeRepository;
-import de.digitalcollections.model.api.identifiable.Identifiable;
+import de.digitalcollections.model.api.identifiable.entity.Entity;
 import de.digitalcollections.model.api.identifiable.entity.parts.Webpage;
 import java.util.UUID;
 
 /**
  * Repository for Webpage persistence handling.
  *
- * @param <W> webpage instance
- * @param <I> identifiable instance
+ * @param <E> entity type
  */
-public interface WebpageRepository<W extends Webpage, I extends Identifiable> extends NodeRepository<W>, IdentifiablesContainerRepository<W, I> {
+public interface WebpageRepository<E extends Entity> extends NodeRepository<Webpage>, EntityPartRepository<Webpage, E> {
 
-  W saveWithParentWebsite(W webpage, UUID parentWebsiteUUID);
+  Webpage saveWithParentWebsite(Webpage webpage, UUID parentWebsiteUUID);
 
-  W saveWithParentWebpage(W webpage, UUID parentWebpageUUID);
+  Webpage saveWithParentWebpage(Webpage webpage, UUID parentWebpageUUID);
 }
--- dc-cudami-admin/dc-cudami-admin-backend-api/src/main/java/de/digitalcollections/cudami/admin/backend/api/repository/identifiable/resource/CudamiFileResourceRepository.java
+++ dc-cudami-admin/dc-cudami-admin-backend-api/src/main/java/de/digitalcollections/cudami/admin/backend/api/repository/identifiable/resource/CudamiFileResourceRepository.java
@@ -5,14 +5,11 @@ import de.digitalcollections.model.api.identifiable.resource.FileResource;
 import de.digitalcollections.model.api.identifiable.resource.exceptions.ResourceIOException;
 import java.io.InputStream;
 
-/**
- * @param <F> resource instance
- */
-public interface CudamiFileResourceRepository<F extends FileResource> extends IdentifiableRepository<F> {
+public interface CudamiFileResourceRepository extends IdentifiableRepository<FileResource> {
 
-  public F save(FileResource fileResource, byte[] bytes);
+  public FileResource save(FileResource fileResource, byte[] bytes);
 
-  public F upload(InputStream inputStream, String filename, String contentType) throws ResourceIOException;
-  
-  public F upload(byte[] bytes, String filename, String contentType) throws ResourceIOException;
+  public FileResource upload(InputStream inputStream, String filename, String contentType) throws ResourceIOException;
+
+  public FileResource upload(byte[] bytes, String filename, String contentType) throws ResourceIOException;
 }
