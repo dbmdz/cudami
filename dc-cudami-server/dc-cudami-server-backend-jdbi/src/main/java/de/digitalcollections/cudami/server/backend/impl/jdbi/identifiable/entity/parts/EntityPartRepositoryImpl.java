023e16a7be9b5538b7d26a8582330d3eb070b4d9
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2019-05-08 13:20:58 +0200

    Add missing implementation for database actions

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/entity/parts/EntityPartRepositoryImpl.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/entity/parts/EntityPartRepositoryImpl.java
@@ -5,9 +5,14 @@ import de.digitalcollections.cudami.server.backend.impl.jdbi.identifiable.Identi
 import de.digitalcollections.model.api.identifiable.entity.Entity;
 import de.digitalcollections.model.api.identifiable.entity.parts.EntityPart;
 import de.digitalcollections.model.api.identifiable.resource.FileResource;
+import de.digitalcollections.model.impl.identifiable.entity.EntityImpl;
+import de.digitalcollections.model.impl.identifiable.resource.FileResourceImpl;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.PreparedBatch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -20,63 +25,134 @@ public class EntityPartRepositoryImpl<P extends EntityPart, E extends Entity> ex
   }
 
   @Override
-  public void addRelatedEntity(EntityPart entityPart, Entity entity) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public void addRelatedEntity(P entityPart, E entity) {
+    addRelatedEntity(entityPart.getUuid(), entity.getUuid());
   }
 
   @Override
   public void addRelatedEntity(UUID entityPartUuid, UUID entityUuid) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    Integer sortIndex = selectNextSortIndexForParentChildren(dbi, "rel_entitypart_entities", "entitypart_uuid", entityPartUuid);
+    dbi.withHandle(h -> h.createUpdate("INSERT INTO rel_entitypart_entities(entitypart_uuid, entity_uuid, sortindex) VALUES (:entitypart_uuid, :entity_uuid, :sortindex)")
+        .bind("entitypart_uuid", entityPartUuid)
+        .bind("entity_uuid", entityUuid)
+        .bind("sortindex", sortIndex)
+        .execute());
   }
 
   @Override
-  public void addRelatedFileresource(EntityPart entityPart, FileResource fileResource) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public void addRelatedFileresource(P entityPart, FileResource fileResource) {
+    addRelatedFileresource(entityPart.getUuid(), fileResource.getUuid());
   }
 
   @Override
   public void addRelatedFileresource(UUID entityPartUuid, UUID fileResourceUuid) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    Integer sortIndex = selectNextSortIndexForParentChildren(dbi, "rel_entitypart_fileresources", "entitypart_uuid", entityPartUuid);
+    dbi.withHandle(h -> h.createUpdate("INSERT INTO rel_entitypart_fileresources(entitypart_uuid, fileresource_uuid, sortindex) VALUES (:entitypart_uuid, :fileresource_uuid, :sortindex)")
+        .bind("entitypart_uuid", entityPartUuid)
+        .bind("fileresource_uuid", fileResourceUuid)
+        .bind("sortindex", sortIndex)
+        .execute());
   }
 
   @Override
-  public LinkedHashSet getRelatedEntities(EntityPart entityPart) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public LinkedHashSet<E> getRelatedEntities(P entityPart) {
+    return getRelatedEntities(entityPart.getUuid());
   }
 
   @Override
-  public LinkedHashSet getRelatedEntities(UUID entityPartUuid) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public LinkedHashSet<E> getRelatedEntities(UUID entityPartUuid) {
+    StringBuilder query = new StringBuilder("SELECT *")
+        .append(" FROM entities e INNER JOIN rel_entitypart_entities ref ON e.uuid=ref.entity_uuid")
+        .append(" WHERE ref.entitypart_uuid = :entityPartUuid")
+        .append(" ORDER BY ref.sortindex");
+
+    List<EntityImpl> list = dbi.withHandle(h -> h.createQuery(query.toString())
+        .bind("entityPartUuid", entityPartUuid)
+        .mapToBean(EntityImpl.class)
+        .list());
+
+    if (list.isEmpty()) {
+      return new LinkedHashSet<>();
+    }
+
+    // TODO maybe does not work, then we have to refactor to LinkedHashSet<Entity>...
+    LinkedHashSet<E> result = list.stream()
+        .map(s -> (E) s)
+        .collect(Collectors.toCollection(LinkedHashSet::new));
+    return result;
   }
 
   @Override
-  public LinkedHashSet getRelatedFileResources(EntityPart entityPart) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public LinkedHashSet<FileResource> getRelatedFileResources(P entityPart) {
+    return getRelatedFileResources(entityPart.getUuid());
   }
 
   @Override
-  public LinkedHashSet getRelatedFileResources(UUID entityPartUuid) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public LinkedHashSet<FileResource> getRelatedFileResources(UUID entityPartUuid) {
+    StringBuilder query = new StringBuilder("SELECT *")
+        .append(" FROM fileresources f INNER JOIN rel_entitypart_fileresources ref ON f.uuid=ref.fileresource_uuid")
+        .append(" WHERE ref.entitypart_uuid = :entityPartUuid")
+        .append(" ORDER BY ref.sortindex");
+
+    List<FileResourceImpl> result = dbi.withHandle(h -> h.createQuery(query.toString())
+        .bind("entityPartUuid", entityPartUuid)
+        .mapToBean(FileResourceImpl.class)
+        .list());
+    return new LinkedHashSet<>(result);
   }
 
   @Override
-  public LinkedHashSet saveRelatedEntities(EntityPart entityPart, LinkedHashSet entities) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public LinkedHashSet<E> saveRelatedEntities(P entityPart, LinkedHashSet<E> entities) {
+    return saveRelatedEntities(entityPart.getUuid(), entities);
   }
 
   @Override
-  public LinkedHashSet saveRelatedEntities(UUID entityPartUuid, LinkedHashSet entities) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public LinkedHashSet<E> saveRelatedEntities(UUID entityPartUuid, LinkedHashSet<E> entities) {
+    // as we store the whole list new: delete old entries
+    dbi.withHandle(h -> h.createUpdate("DELETE FROM rel_entitypart_entities WHERE entitypart_uuid = :uuid")
+        .bind("uuid", entityPartUuid).execute());
+
+    if (entities != null) {
+      // we assume that the entities are already saved...
+      dbi.useHandle(handle -> {
+        PreparedBatch preparedBatch = handle.prepareBatch("INSERT INTO rel_entitypart_entities(entitypart_uuid, entity_uuid, sortIndex) VALUES(:uuid, :entityUuid, :sortIndex)");
+        for (Entity entity : entities) {
+          preparedBatch.bind("uuid", entityPartUuid)
+              .bind("entityUuid", entity.getUuid())
+              .bind("sortIndex", getIndex(entities, entity))
+              .add();
+        }
+        preparedBatch.execute();
+      });
+    }
+    return getRelatedEntities(entityPartUuid);
   }
 
   @Override
-  public LinkedHashSet saveRelatedFileResources(EntityPart entityPart, LinkedHashSet fileResources) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public LinkedHashSet<FileResource> saveRelatedFileResources(P entityPart, LinkedHashSet<FileResource> fileResources) {
+    return saveRelatedFileResources(entityPart.getUuid(), fileResources);
   }
 
   @Override
-  public LinkedHashSet saveRelatedFileResources(UUID entityPartUuid, LinkedHashSet fileResources) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public LinkedHashSet<FileResource> saveRelatedFileResources(UUID entityPartUuid, LinkedHashSet<FileResource> fileResources) {
+    if (fileResources == null) {
+      return null;
+    }
+    // as we store the whole list new: delete old entries
+    dbi.withHandle(h -> h.createUpdate("DELETE FROM rel_entitypart_fileresources WHERE entitypart_uuid = :uuid")
+        .bind("uuid", entityPartUuid).execute());
+
+    dbi.useHandle(handle -> {
+      PreparedBatch preparedBatch = handle.prepareBatch("INSERT INTO rel_entity_fileresources(entitypart_uuid, fileresource_uuid, sortIndex) VALUES(:uuid, :fileResourceUuid, :sortIndex)");
+      for (FileResource fileResource : fileResources) {
+        preparedBatch.bind("uuid", entityPartUuid)
+            .bind("fileResourceUuid", fileResource.getUuid())
+            .bind("sortIndex", getIndex(fileResources, fileResource))
+            .add();
+      }
+      preparedBatch.execute();
+    });
+    return getRelatedFileResources(entityPartUuid);
   }
 
 }
