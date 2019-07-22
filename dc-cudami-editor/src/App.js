4adfe8682edc10506d958e264a0012e611c32250
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-07-22 10:40:24 +0200

    Restore possibility of local development

---
+++ dc-cudami-editor/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import IdentifiableForm from './components/IdentifiableForm';
+
+const App = () => {
+  return (
+    <IdentifiableForm activeLocale="de_DE" type="website" uuid="bac6b652-6834-4c2e-9d84-70dd8e9c66a7" />
+  );
+};
+
+export default App;
