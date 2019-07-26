fccb8b73e6f3091bb2fab8fc5cd5dc41b6d18996
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-07-26 09:22:59 +0200

    Remove mock data and replace with real api calls

--- dc-cudami-editor/src/Api.js
+++ dc-cudami-editor/src/Api.js
@@ -1,57 +1,17 @@
 export async function loadAvailableLocales () {
-  return ['es', 'fr'];
+  try {
+    const result = await fetch('/api/locales');
+    return result.json();
+  } catch(err) {
+    return [];
+  }
 };
 
 export async function loadIdentifiable (type, uuid) {
-  return {
-    "created": "2018-08-21T16:22:52.033",
-    "description": {
-      "en": {
-        "type": "doc",
-        "content": [
-          {
-            "type": "paragraph",
-            "content": [
-              {
-                "type": "text",
-                "text": "Englische Beschreibung"
-              }
-            ]
-          }
-        ]
-      }
-    },
-    "label": {
-      "de": "Deutscher Titel",
-      "en": "Englischer Titel"
-    },
-    "lastModified": "2018-08-21T16:22:52.033",
-    "text": {
-      "de": {
-        "type": "doc",
-        "content": [
-          {
-            "type": "paragraph",
-            "content": [
-              {
-                "type": "text",
-                "text": "Deutscher Text"
-              }
-            ]
-          }
-        ]
-      }
-    },
-    "type": "ENTITY",
-    "uuid": "546aac6c-ee00-41b9-a47e-9673a398f171",
-    "entityType": "WEBSITE",
-    "rootPages": [],
-    "url": "http://bsb.de"
-  };
-  /*try {
-    const result = await fetch(`/latest/${type}/${uuid}.json`);
+  try {
+    const result = await fetch(`/api/${type}s/${uuid}`);
     return result.json();
   } catch(err) {
     return {};
-  }*/
+  }
 };
