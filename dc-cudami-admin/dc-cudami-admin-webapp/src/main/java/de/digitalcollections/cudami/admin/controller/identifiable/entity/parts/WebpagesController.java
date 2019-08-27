806d804177f5cba77d3a4e399ac540c57e73ed78
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-08-27 15:36:07 +0200

    Remove unused endpoints

--- dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/parts/WebpagesController.java
+++ dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/parts/WebpagesController.java
@@ -11,12 +11,9 @@ import de.digitalcollections.model.api.identifiable.entity.parts.Webpage;
 import de.digitalcollections.model.api.identifiable.resource.FileResource;
 import de.digitalcollections.model.api.paging.PageRequest;
 import de.digitalcollections.model.api.paging.PageResponse;
-import de.digitalcollections.model.impl.identifiable.entity.parts.WebpageImpl;
 import java.net.URI;
 import java.util.LinkedHashSet;
-import java.util.Objects;
 import java.util.UUID;
-import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,19 +25,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Controller for webpage management pages.
@@ -151,73 +143,4 @@ public class WebpagesController extends AbstractController {
 
     return "webpages/view";
   }
-
-  @RequestMapping(value = "/webpages/new", method = RequestMethod.POST)
-  public String create(@ModelAttribute @Valid WebpageImpl webpage, BindingResult results, Model model, SessionStatus status, RedirectAttributes redirectAttributes,
-                       @RequestParam("parentType") String parentType,
-                       @RequestParam("parentUuid") UUID parentUuid) {
-    verifyBinding(results);
-    if (results.hasErrors()) {
-      return "webpages/create";
-    }
-    Webpage webpageDb = null;
-    try {
-      if (Objects.equals(parentType, "website")) {
-        webpageDb = service.saveWithParentWebsite(webpage, parentUuid);
-        LOGGER.info("Successfully saved top-level webpage");
-      } else if (Objects.equals(parentType, "webpage")) {
-        webpageDb = service.saveWithParentWebpage(webpage, parentUuid);
-        LOGGER.info("Successfully saved webpage");
-      }
-    } catch (Exception e) {
-      if (Objects.equals(parentType, "website")) {
-        LOGGER.info("Cannot save top-level webpage: ", e);
-      } else if (Objects.equals(parentType, "webpage")) {
-        LOGGER.error("Cannot save webpage: ", e);
-      }
-      String message = ""; //messageSource.getMessage("msg.error", null, LocaleContextHolder.getLocale());
-      redirectAttributes.addFlashAttribute("error_message", message);
-      return "redirect:/websites";
-    }
-    if (results.hasErrors()) {
-      return "webpages/create";
-    }
-    status.setComplete();
-    String message = ""; //messageSource.getMessage("msg.created_successfully", null, LocaleContextHolder.getLocale());
-    redirectAttributes.addFlashAttribute("success_message", message);
-    return "redirect:/webpages/" + webpageDb.getUuid().toString();
-  }
-
-  @RequestMapping(value = "/webpages/{pathUuid}/edit", method = RequestMethod.POST)
-  public String edit(@PathVariable UUID pathUuid, @ModelAttribute @Valid WebpageImpl webpage, BindingResult results, Model model, SessionStatus status, RedirectAttributes redirectAttributes) {
-    verifyBinding(results);
-    if (results.hasErrors()) {
-      return "webpages/edit";
-    }
-
-    try {
-      // get webpage from db
-      Webpage webpageDb = (Webpage) service.get(pathUuid);
-      // just update the fields, that were editable
-      webpageDb.setLabel(webpage.getLabel());
-      webpageDb.setDescription(webpage.getDescription());
-      webpageDb.setText(webpage.getText());
-
-      service.update(webpageDb);
-    } catch (IdentifiableServiceException e) {
-      String message = "Cannot save webpage with uuid=" + pathUuid + ": " + e;
-      LOGGER.error(message, e);
-      redirectAttributes.addFlashAttribute("error_message", message);
-      return "redirect:/webpages/" + pathUuid + "/edit";
-    }
-
-    if (results.hasErrors()) {
-      return "webpages/edit";
-    }
-    status.setComplete();
-    String message = ""; //messageSource.getMessage("msg.changes_saved_successfully", null, LocaleContextHolder.getLocale());
-    redirectAttributes.addFlashAttribute("success_message", message);
-    return "redirect:/webpages/" + pathUuid;
-  }
-
 }
