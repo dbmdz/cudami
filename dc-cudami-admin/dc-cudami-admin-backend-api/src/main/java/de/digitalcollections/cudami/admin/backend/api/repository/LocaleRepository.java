9d48ca472591990da231737be32bbb435cd50d93
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-09-18 15:33:07 +0200

    Readd missing docstring

--- dc-cudami-admin/dc-cudami-admin-backend-api/src/main/java/de/digitalcollections/cudami/admin/backend/api/repository/LocaleRepository.java
+++ dc-cudami-admin/dc-cudami-admin-backend-api/src/main/java/de/digitalcollections/cudami/admin/backend/api/repository/LocaleRepository.java
@@ -13,6 +13,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Repository for Locale persistence handling.
+ */
 public interface LocaleRepository {
 
   @RequestLine("GET /latest/languages")
