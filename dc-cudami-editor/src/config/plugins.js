c699edc1b23f4c476db9a982a2f3e3d723d7a99d
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-11-05 13:15:37 +0100

    Remove placeholder from editor when content is empty

--- dc-cudami-editor/src/config/plugins.js
+++ dc-cudami-editor/src/config/plugins.js
@@ -2,13 +2,11 @@ import {history} from 'prosemirror-history'
 import {dropCursor} from 'prosemirror-dropcursor'
 import {gapCursor} from 'prosemirror-gapcursor'
 import {columnResizing, tableEditing} from 'prosemirror-tables'
-import {placeholder} from '@aeaton/prosemirror-placeholder'
 import {footnotes} from '@aeaton/prosemirror-footnotes'
 
 import 'prosemirror-tables/style/tables.css'
 import 'prosemirror-gapcursor/style/gapcursor.css'
 import '@aeaton/prosemirror-footnotes/style/footnotes.css'
-import '@aeaton/prosemirror-placeholder/style/placeholder.css'
 
 import keys from './keys'
 import rules from './rules'
@@ -16,7 +14,6 @@ import rules from './rules'
 export default [
   rules,
   keys,
-  placeholder(),
   footnotes(),
   dropCursor(),
   gapCursor(),
