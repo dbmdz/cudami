85443c277efa6e6d42f83f5af42b7870b4e009d1
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-11-11 14:17:03 +0100

    Remove useless endpoints

--- dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/parts/WebpagesController.java
+++ dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/parts/WebpagesController.java
@@ -1,8 +1,5 @@
 package de.digitalcollections.cudami.admin.controller.identifiable.entity.parts;
 
-import de.digitalcollections.commons.springdata.domain.PageConverter;
-import de.digitalcollections.commons.springdata.domain.PageWrapper;
-import de.digitalcollections.commons.springdata.domain.PageableConverter;
 import de.digitalcollections.commons.springmvc.controller.AbstractController;
 import de.digitalcollections.cudami.admin.backend.api.repository.LocaleRepository;
 import de.digitalcollections.cudami.admin.business.api.service.exceptions.IdentifiableServiceException;
@@ -10,8 +7,6 @@ import de.digitalcollections.cudami.admin.business.api.service.identifiable.enti
 import de.digitalcollections.cudami.admin.util.LanguageSortingHelper;
 import de.digitalcollections.model.api.identifiable.entity.parts.Webpage;
 import de.digitalcollections.model.api.identifiable.resource.FileResource;
-import de.digitalcollections.model.api.paging.PageRequest;
-import de.digitalcollections.model.api.paging.PageResponse;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
@@ -20,9 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -96,20 +88,6 @@ public class WebpagesController extends AbstractController {
   @ResponseBody
   public Webpage get(@PathVariable UUID uuid) {
     return (Webpage) service.get(uuid);
-  }
-
-  @GetMapping("/webpages")
-  public String list(
-      Model model,
-      @PageableDefault(
-              sort = {"email"},
-              size = 25)
-          Pageable pageable) {
-    final PageRequest pageRequest = PageableConverter.convert(pageable);
-    final PageResponse pageResponse = service.find(pageRequest);
-    Page page = PageConverter.convert(pageResponse, pageRequest);
-    model.addAttribute("page", new PageWrapper(page, "/webpages"));
-    return "webpages/list";
   }
 
   @PostMapping("/api/webpages/new")
