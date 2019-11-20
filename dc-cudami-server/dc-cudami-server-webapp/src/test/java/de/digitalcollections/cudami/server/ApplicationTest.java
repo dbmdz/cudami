ad839b062fa74da8c1c97dcb3e57e1bdbe8f9f85
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-11-20 13:17:28 +0100

    Add unit test to check application startup

---
+++ dc-cudami-server/dc-cudami-server-webapp/src/test/java/de/digitalcollections/cudami/server/ApplicationTest.java
@@ -0,0 +1,11 @@
+package de.digitalcollections.cudami.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+  @Test
+  public void contextLoads() throws Exception {}
+}
