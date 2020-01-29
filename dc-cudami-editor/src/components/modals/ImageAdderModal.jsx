7d1e1057bdfcdf89405acf99da336431d08c7a22
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-01-29 16:35:24 +0100

    Add much more extended insert-image dialog

--- dc-cudami-editor/src/components/modals/ImageAdderModal.jsx
+++ dc-cudami-editor/src/components/modals/ImageAdderModal.jsx
@@ -1,23 +1,39 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCode, faUpload} from '@fortawesome/free-solid-svg-icons'
 import {publish, subscribe} from 'pubsub-js'
 import React, {Component} from 'react'
 import {
-  Button,
-  FormGroup,
-  Input,
+  ListGroup,
+  ListGroupItem,
   Modal,
   ModalBody,
-  ModalFooter,
   ModalHeader,
 } from 'reactstrap'
 import {withTranslation} from 'react-i18next'
 
+import ImageMetadataForm from './imageAdder/ImageMetadataForm'
+import ImageRenderingHintsForm from './imageAdder/ImageRenderingHintsForm'
+import ImageUploadForm from './imageAdder/ImageUploadForm'
+import ImageUrlForm from './imageAdder/ImageUrlForm'
+
 class ImageAdderModal extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      alt: '',
-      src: '',
+    this.initialAttributes = {
+      alignment: 'left',
+      altText: '',
+      caption: '',
+      label: '',
+      linkNewTab: true,
+      linkUrl: '',
       title: '',
+      url: '',
+      width: '33%',
+    }
+    this.state = {
+      attributes: this.initialAttributes,
+      readOnly: false,
+      step: 'initial',
     }
     subscribe('editor.show-image-modal', () => {
       this.props.onToggle()
@@ -25,54 +41,118 @@ class ImageAdderModal extends Component {
   }
 
   addImageToEditor = () => {
-    this.props.onToggle()
-    publish('editor.add-image', this.state)
+    this.toggle()
+    const filteredAttributes = Object.entries(this.state.attributes).reduce(
+      (filteredAttributes, attribute) => {
+        if (attribute[1]) {
+          filteredAttributes[attribute[0]] = attribute[1]
+        }
+        return filteredAttributes
+      },
+      {}
+    )
+    publish('editor.add-image', filteredAttributes)
+  }
+
+  getStepComponent = t => {
+    const STEP_COMPONENT_MAPPING = {
+      initial: (
+        <ListGroup>
+          <ListGroupItem
+            className="text-decoration-none"
+            href="#"
+            onClick={() => this.setState({step: 'url'})}
+            tag="a"
+          >
+            <FontAwesomeIcon icon={faCode} /> URL
+          </ListGroupItem>
+          <ListGroupItem
+            className="text-decoration-none"
+            href="#"
+            onClick={() => this.setState({step: 'upload'})}
+            tag="a"
+          >
+            <FontAwesomeIcon icon={faUpload} /> Upload
+          </ListGroupItem>
+        </ListGroup>
+      ),
+      metadata: (
+        <ImageMetadataForm
+          attributes={this.state.attributes}
+          onChange={(name, value) => this.setAttribute(name, value)}
+          onSubmit={() => this.setState({step: 'renderingHints'})}
+        />
+      ),
+      renderingHints: (
+        <ImageRenderingHintsForm
+          attributes={this.state.attributes}
+          onChange={(name, value) => this.setAttribute(name, value)}
+          onSubmit={() => this.addImageToEditor()}
+        />
+      ),
+      upload: (
+        <ImageUploadForm
+          apiContextPath={this.props.apiContextPath}
+          onChange={attributes => this.setAttributes(attributes)}
+          onSubmit={() => this.setState({readOnly: true, step: 'url'})}
+        />
+      ),
+      url: (
+        <ImageUrlForm
+          attributes={this.state.attributes}
+          onChange={(name, value) => this.setAttribute(name, value)}
+          onSubmit={() => this.setState({step: 'metadata'})}
+          readOnly={this.state.readOnly}
+        />
+      ),
+    }
+    return STEP_COMPONENT_MAPPING[this.state.step]
+  }
+
+  getStepTitle = t => {
+    const STEP_TITLE_MAPPING = {
+      initial: 'Bild einfügen über',
+      renderingHints: 'Darstellung',
+      upload: 'Hochladen',
+      url: 'URL eingeben',
+      metadata: 'Metadaten eingeben',
+    }
+    return STEP_TITLE_MAPPING[this.state.step]
+  }
+
+  setAttribute = (name, value) => {
     this.setState({
-      alt: '',
-      src: '',
-      title: '',
+      attributes: {
+        ...this.state.attributes,
+        [name]: value,
+      },
+    })
+  }
+
+  setAttributes = attributes => {
+    this.setState({
+      attributes: {
+        ...this.state.attributes,
+        ...attributes,
+      },
+    })
+  }
+
+  toggle = () => {
+    this.props.onToggle()
+    this.setState({
+      attributes: this.initialAttributes,
+      readOnly: false,
+      step: 'initial',
     })
   }
 
   render() {
     const {t} = this.props
     return (
-      <Modal isOpen={this.props.isOpen} toggle={this.props.onToggle}>
-        <ModalHeader toggle={this.props.onToggle}>
-          {t('insert.image')}
-        </ModalHeader>
-        <ModalBody>
-          <FormGroup>
-            <Input
-              onChange={evt => this.setState({src: evt.target.value})}
-              placeholder="URL"
-              required
-              type="text"
-              value={this.state.src}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Input
-              onChange={evt => this.setState({alt: evt.target.value})}
-              placeholder={t('altText')}
-              type="text"
-              value={this.state.alt}
-            />
-          </FormGroup>
-          <FormGroup className="mb-0">
-            <Input
-              onChange={evt => this.setState({title: evt.target.value})}
-              placeholder={t('label')}
-              type="text"
-              value={this.state.title}
-            />
-          </FormGroup>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={this.addImageToEditor}>
-            {t('add')}
-          </Button>
-        </ModalFooter>
+      <Modal isOpen={this.props.isOpen} size="xl" toggle={this.toggle}>
+        <ModalHeader toggle={this.toggle}>{this.getStepTitle(t)}</ModalHeader>
+        <ModalBody>{this.getStepComponent(t)}</ModalBody>
       </Modal>
     )
   }
--- dc-cudami-editor/src/config/custom/ImageNode.js
+++ dc-cudami-editor/src/config/custom/ImageNode.js
@@ -1,35 +1,55 @@
 export default {
   image: {
     attrs: {
-      alt: {default: null},
-      src: {},
+      altText: {default: null},
+      caption: {default: null},
+      linkNewTab: {default: false},
+      linkUrl: {default: null},
       title: {default: null},
+      url: {default: null},
     },
     draggable: true,
     group: 'inline',
     inline: true,
     parseDOM: [
       {
-        tag: 'img[src]',
+        tag: 'figure',
         getAttrs(dom) {
+          const caption = dom.querySelector('figcaption')
+          const image = dom.querySelector('img')
+          const link = dom.querySelector('a[href]')
+          const linkAttributes = {}
+          if (link) {
+            linkAttributes.linkNewTab = link.getAttribute('target') === '_blank'
+            linkAttributes.linkUrl = link.getAttribute('href')
+          }
           return {
-            src: dom.getAttribute('src'),
-            title: dom.getAttribute('title'),
-            alt: dom.getAttribute('alt'),
+            altText: image.getAttribute('alt'),
+            caption: caption?.innerText,
+            title: image.getAttribute('title'),
+            url: image.getAttribute('src'),
+            ...linkAttributes,
           }
         },
       },
     ],
     toDOM(node) {
-      let {alt, src, title} = node.attrs
-      return [
-        'img',
-        {
-          alt,
-          src,
-          title,
-        },
-      ]
+      const {altText, caption, linkNewTab, linkUrl, title, url} = node.attrs
+      const tags = ['figure']
+      const imageTag = ['img', {alt: altText, src: url, title}]
+      if (linkUrl) {
+        tags.push([
+          'a',
+          {href: linkUrl, target: linkNewTab ? '_blank' : null},
+          imageTag,
+        ])
+      } else {
+        tags.push(imageTag)
+      }
+      if (caption) {
+        tags.push(['figcaption', caption])
+      }
+      return tags
     },
   },
 }
