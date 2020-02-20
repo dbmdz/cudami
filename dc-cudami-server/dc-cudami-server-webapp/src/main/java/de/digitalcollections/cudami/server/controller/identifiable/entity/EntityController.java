27e0c4d8c2a896abdd27d788821ffaec6708053a
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-02-20 19:28:32 +0100

    Remove ambigious between uuid and refId controller mapping

--- dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/identifiable/entity/EntityController.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/identifiable/entity/EntityController.java
@@ -27,7 +27,7 @@ public class EntityController<E extends Entity> {
 
   @ApiMethod(description = "Get entity by uuid")
   @GetMapping(
-      value = {"/latest/entities/{uuid}", "/v2/entities/{uuid}"},
+      value = {"/latest/entities/{uuid:/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i}", "/v2/entities/{uuid:/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i}"},
       produces = "application/json")
   @ApiResponseObject
   public Entity findById(@PathVariable UUID uuid) {
