db458e08101ad8ea1ac6a5c3849f9e3119de6877
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-03-11 08:52:36 +0100

    Replace yaml parsing for webjar versions with configuration properties

--- dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/controller/advice/GlobalControllerAdvice.java
+++ dc-cudami-admin/dc-cudami-admin-webapp/src/main/java/de/digitalcollections/cudami/admin/controller/advice/GlobalControllerAdvice.java
@@ -1,18 +1,19 @@
 package de.digitalcollections.cudami.admin.controller.advice;
 
+import de.digitalcollections.cudami.admin.config.WebjarProperties;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-/** Adds the webjar versions read from yaml files as global model attribute. */
+/** Adds the webjar versions read from application.yml as global model attribute. */
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-  @Autowired
-  @Value("#{webjarVersions}")
-  private Map<String, String> webjarVersions;
+  private final Map<String, String> webjarVersions;
+
+  public GlobalControllerAdvice(WebjarProperties webjarProperties) {
+    this.webjarVersions = webjarProperties.getVersions();
+  }
 
   @ModelAttribute("webjarVersions")
   public Map<String, String> getWebjarVersions() {
