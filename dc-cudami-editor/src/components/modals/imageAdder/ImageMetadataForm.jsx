f93aff451b925fa6e92d8d557b84c65bd0cba053
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-04-20 16:39:02 +0200

    Add description texts for image input fields

--- dc-cudami-editor/src/components/modals/imageAdder/ImageMetadataForm.jsx
+++ dc-cudami-editor/src/components/modals/imageAdder/ImageMetadataForm.jsx
@@ -7,8 +7,12 @@ import {
   Collapse,
   FormGroup,
   Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
 } from 'reactstrap'
 import {useTranslation} from 'react-i18next'
+import {FaQuestionCircle} from 'react-icons/fa'
 
 const ImageMetadataForm = (props) => {
   const {t} = useTranslation()
@@ -28,31 +32,52 @@ const ImageMetadataForm = (props) => {
       <Collapse isOpen={isOpen}>
         <CardBody>
           <FormGroup>
-            <Input
-              name="caption"
-              onChange={(evt) => onChange('caption', evt.target.value)}
-              placeholder={t('caption')}
-              type="text"
-              value={attributes.caption}
-            />
+            <InputGroup>
+              <Input
+                name="caption"
+                onChange={(evt) => onChange('caption', evt.target.value)}
+                placeholder={t('caption')}
+                type="text"
+                value={attributes.caption}
+              />
+              <InputGroupAddon addonType="append">
+                <InputGroupText>
+                  <FaQuestionCircle title={t('tooltips.caption')} />
+                </InputGroupText>
+              </InputGroupAddon>
+            </InputGroup>
           </FormGroup>
           <FormGroup>
-            <Input
-              name="title"
-              onChange={(evt) => onChange('title', evt.target.value)}
-              placeholder={t('tooltip')}
-              type="text"
-              value={attributes.title}
-            />
+            <InputGroup>
+              <Input
+                name="title"
+                onChange={(evt) => onChange('title', evt.target.value)}
+                placeholder={t('tooltip')}
+                type="text"
+                value={attributes.title}
+              />
+              <InputGroupAddon addonType="append">
+                <InputGroupText>
+                  <FaQuestionCircle title={t('tooltips.tooltip')} />
+                </InputGroupText>
+              </InputGroupAddon>
+            </InputGroup>
           </FormGroup>
           <FormGroup className="mb-0">
-            <Input
-              name="altText"
-              onChange={(evt) => onChange('altText', evt.target.value)}
-              placeholder={t('altText')}
-              type="text"
-              value={attributes.altText}
-            />
+            <InputGroup>
+              <Input
+                name="altText"
+                onChange={(evt) => onChange('altText', evt.target.value)}
+                placeholder={t('altText')}
+                type="text"
+                value={attributes.altText}
+              />
+              <InputGroupAddon addonType="append">
+                <InputGroupText>
+                  <FaQuestionCircle title={t('tooltips.altText')} />
+                </InputGroupText>
+              </InputGroupAddon>
+            </InputGroup>
           </FormGroup>
         </CardBody>
       </Collapse>
