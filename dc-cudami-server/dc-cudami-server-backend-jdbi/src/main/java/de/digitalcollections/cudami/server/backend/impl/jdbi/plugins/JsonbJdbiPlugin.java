0859b0bd518c9aa48401dc029a570a36d1455ee7
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-05-06 16:51:54 +0200

    Add missing new JSOB type

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/plugins/JsonbJdbiPlugin.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/plugins/JsonbJdbiPlugin.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.digitalcollections.commons.jdbi.JsonbArgumentFactory;
 import de.digitalcollections.commons.jdbi.JsonbColumnMapperFactory;
 import de.digitalcollections.model.api.identifiable.parts.LocalizedText;
+import de.digitalcollections.model.api.identifiable.parts.RenderingHintsPreviewImage;
 import de.digitalcollections.model.api.identifiable.parts.structuredcontent.LocalizedStructuredContent;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.spi.JdbiPlugin;
@@ -18,10 +19,16 @@ public class JsonbJdbiPlugin implements JdbiPlugin {
 
   @Override
   public void customizeJdbi(Jdbi db) {
+    // argument factories
     db.registerArgument(new JsonbArgumentFactory<>(LocalizedStructuredContent.class, objectMapper));
     db.registerArgument(new JsonbArgumentFactory<>(LocalizedText.class, objectMapper));
+    db.registerArgument(new JsonbArgumentFactory<>(RenderingHintsPreviewImage.class, objectMapper));
+
+    // column mapper
     db.registerColumnMapper(
         new JsonbColumnMapperFactory(LocalizedStructuredContent.class, objectMapper));
     db.registerColumnMapper(new JsonbColumnMapperFactory(LocalizedText.class, objectMapper));
+    db.registerColumnMapper(
+        new JsonbColumnMapperFactory(RenderingHintsPreviewImage.class, objectMapper));
   }
 }
