8b20b811bbed920e73b93258a821bfcb0dd6c53d
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-05-06 16:40:28 +0200

    Add tabs for different image insert ways

--- dc-cudami-editor/src/components/modals/imageAdder/ImageSelector.jsx
+++ dc-cudami-editor/src/components/modals/imageAdder/ImageSelector.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import classNames from 'classnames'
 import {
   Card,
   CardBody,
@@ -8,8 +9,13 @@ import {
   InputGroup,
   InputGroupAddon,
   InputGroupText,
+  Nav,
+  NavItem,
+  NavLink,
   Popover,
   PopoverBody,
+  TabContent,
+  TabPane,
 } from 'reactstrap'
 import {withTranslation} from 'react-i18next'
 import {FaQuestionCircle} from 'react-icons/fa'
@@ -21,8 +27,18 @@ class ImageSelector extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      activeTab: 'upload',
       labelTooltipOpen: false,
       progress: 0,
+    }
+  }
+
+  toggleTab = (activeTab) => {
+    if (activeTab !== this.state.activeTab) {
+      this.props.resetFileResource()
+      this.setState({
+        activeTab,
+      })
     }
   }
 
@@ -41,9 +57,8 @@ class ImageSelector extends Component {
     )
     const responseJson = JSON.parse(response)
     onChange({
-      label: responseJson.label,
+      ...responseJson,
       uri: `${responseJson.iiifBaseUrl}/full/full/0/default.${responseJson.filenameExtension}`,
-      uuid: responseJson.uuid,
     })
   }
 
@@ -51,64 +66,94 @@ class ImageSelector extends Component {
     const {fileResource, onChange, t} = this.props
     return (
       <Card className="mt-0">
-        <CardHeader className="font-weight-bold">{t('selectImage')}</CardHeader>
+        <CardHeader className="font-weight-bold">
+          <Nav className="card-header-tabs" tabs>
+            <NavItem>
+              <NavLink
+                className={classNames({
+                  active: this.state.activeTab === 'upload',
+                })}
+                href="#"
+                onClick={() => this.toggleTab('upload')}
+              >
+                {t('selectImage.useUpload')}
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classNames({active: this.state.activeTab === 'url'})}
+                href="#"
+                onClick={() => this.toggleTab('url')}
+              >
+                {t('selectImage.useUrl')}
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </CardHeader>
         <CardBody className="text-center">
-          <FileUploadForm
-            onChange={(file) => this.uploadImage(file)}
-            progress={this.state.progress}
-          />
-          <FormGroup className="mt-3">
-            <Input
-              name="url"
-              onChange={(evt) =>
-                onChange({uri: evt.target.value, uuid: undefined})
-              }
-              placeholder="URL"
-              required
-              type="url"
-              value={fileResource.uri}
-            />
-          </FormGroup>
-          <FormGroup>
-            <InputGroup>
-              <Input
-                name="label"
-                onChange={(evt) =>
-                  onChange({
-                    label: {
-                      [Object.keys(fileResource.label)[0]]: evt.target.value,
-                    },
-                  })
-                }
-                placeholder={t('label')}
-                required
-                type="text"
-                value={
-                  fileResource.label ? Object.values(fileResource.label)[0] : ''
-                }
+          <TabContent activeTab={this.state.activeTab} className="border-0 p-0">
+            <TabPane tabId="upload">
+              <FileUploadForm
+                onChange={(file) => this.uploadImage(file)}
+                progress={this.state.progress}
               />
-              <InputGroupAddon addonType="append">
-                <InputGroupText>
-                  <FaQuestionCircle
-                    id="label-tooltip"
-                    style={{cursor: 'pointer'}}
-                  />
-                  <Popover
-                    isOpen={this.state.labelTooltipOpen}
-                    placement="left"
-                    target="label-tooltip"
-                    toggle={() =>
-                      this.setState({
-                        labelTooltipOpen: !this.state.labelTooltipOpen,
+            </TabPane>
+            <TabPane tabId="url">
+              <FormGroup>
+                <Input
+                  name="url"
+                  onChange={(evt) => onChange({uri: evt.target.value})}
+                  placeholder="URL"
+                  required
+                  type="url"
+                  value={fileResource.uri}
+                />
+              </FormGroup>
+              <FormGroup className="mb-0">
+                <InputGroup>
+                  <Input
+                    name="label"
+                    onChange={(evt) =>
+                      onChange({
+                        label: {
+                          [Object.keys(fileResource.label)[0]]:
+                            evt.target.value,
+                        },
                       })
                     }
-                  >
-                    <PopoverBody>{t('tooltips.label')}</PopoverBody>
-                  </Popover>
-                </InputGroupText>
-              </InputGroupAddon>
-            </InputGroup>
-          </FormGroup>
+                    placeholder={t('label')}
+                    required
+                    type="text"
+                    value={
+                      fileResource.label
+                        ? Object.values(fileResource.label)[0]
+                        : ''
+                    }
+                  />
+                  <InputGroupAddon addonType="append">
+                    <InputGroupText>
+                      <FaQuestionCircle
+                        id="label-tooltip"
+                        style={{cursor: 'pointer'}}
+                      />
+                      <Popover
+                        isOpen={this.state.labelTooltipOpen}
+                        placement="left"
+                        target="label-tooltip"
+                        toggle={() =>
+                          this.setState({
+                            labelTooltipOpen: !this.state.labelTooltipOpen,
+                          })
+                        }
+                      >
+                        <PopoverBody>{t('tooltips.label')}</PopoverBody>
+                      </Popover>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                </InputGroup>
+              </FormGroup>
+            </TabPane>
+          </TabContent>
         </CardBody>
       </Card>
     )
