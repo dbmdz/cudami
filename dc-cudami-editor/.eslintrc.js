45cdba34fdde910b0c509f655e32a9151f3e80f8
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-05-25 17:39:06 +0200

    Add eslint with configuration

---
+++ dc-cudami-editor/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+}
