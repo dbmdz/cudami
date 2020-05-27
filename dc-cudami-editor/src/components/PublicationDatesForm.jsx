d1f51c796a2dff6775adae5675dcda526aa4fb4c
Stefan Schweter <stefan@schweter.it>
2020-05-27 13:59:54 +0200

    components: id-ify date picker components

--- dc-cudami-editor/src/components/PublicationDatesForm.jsx
+++ dc-cudami-editor/src/components/PublicationDatesForm.jsx
@@ -19,6 +19,7 @@ const PublicationDatesForm = (props) => {
             calendarAriaLabel={t('datePicker.toggleCalendar')}
             clearAriaLabel={t('datePicker.clearDate')}
             dayAriaLabel={t('datePicker.day')}
+            id={"publication-start-date"}
             monthAriaLabel={t('datePicker.month')}
             nativeInputAriaLabel={t('datePicker.date')}
             onChange={(date) =>
@@ -50,6 +51,7 @@ const PublicationDatesForm = (props) => {
             calendarAriaLabel={t('datePicker.toggleCalendar')}
             clearAriaLabel={t('datePicker.clearDate')}
             dayAriaLabel={t('datePicker.day')}
+            id={"publication-end-date"}
             monthAriaLabel={t('datePicker.month')}
             nativeInputAriaLabel={t('datePicker.date')}
             onChange={(date) =>
