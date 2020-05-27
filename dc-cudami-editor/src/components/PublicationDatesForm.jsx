3e8352cd8ef7231a7b542cea76bfb60185dc1a9a
Stefan Schweter <stefan@schweter.it>
2020-05-27 15:28:21 +0200

    components: refactor date formatting into own function for stupid DatePicker workaround (just add +12 hours and everything is working fine)

--- dc-cudami-editor/src/components/PublicationDatesForm.jsx
+++ dc-cudami-editor/src/components/PublicationDatesForm.jsx
@@ -6,6 +6,18 @@ import {useTranslation} from 'react-i18next'
 const PublicationDatesForm = (props) => {
   const {t} = useTranslation()
 
+  const formatDate = (date) => {
+    if (date != undefined) {
+      // Selected datetime will be displayed correctly in DatePicker.
+      // But after reloading, there's a minus one-day offset. Here
+      // we add 12 hours to the user-defined date to workaround
+      // this issue 😒
+      date.setHours(date.getHours() + 12)
+      return date.toISOString()
+    }
+    return undefined
+  }
+
   return (
     <FormGroup>
       <Row>
@@ -19,20 +31,11 @@ const PublicationDatesForm = (props) => {
             calendarAriaLabel={t('datePicker.toggleCalendar')}
             clearAriaLabel={t('datePicker.clearDate')}
             dayAriaLabel={t('datePicker.day')}
-            id={"publication-start-date"}
+            id={'publication-start-date'}
             monthAriaLabel={t('datePicker.month')}
             nativeInputAriaLabel={t('datePicker.date')}
             onChange={(date) =>
-              props.onChange(
-                'publicationStart',
-                date
-                  ? date.getFullYear() +
-                      '-' +
-                      ('0' + (date.getMonth() + 1)).slice(-2) +
-                      '-' +
-                      ('0' + date.getDate()).slice(-2)
-                  : undefined
-              )
+              props.onChange('publicationStart', formatDate(date))
             }
             required={false}
             value={
@@ -51,20 +54,11 @@ const PublicationDatesForm = (props) => {
             calendarAriaLabel={t('datePicker.toggleCalendar')}
             clearAriaLabel={t('datePicker.clearDate')}
             dayAriaLabel={t('datePicker.day')}
-            id={"publication-end-date"}
+            id={'publication-end-date'}
             monthAriaLabel={t('datePicker.month')}
             nativeInputAriaLabel={t('datePicker.date')}
             onChange={(date) =>
-              props.onChange(
-                'publicationEnd',
-                date
-                  ? date.getFullYear() +
-                      '-' +
-                      ('0' + (date.getMonth() + 1)).slice(-2) +
-                      '-' +
-                      ('0' + date.getDate()).slice(-2)
-                  : undefined
-              )
+              props.onChange('publicationEnd', formatDate(date))
             }
             required={false}
             value={
