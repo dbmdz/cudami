9825fbb0f7bc174649969a69574b3f3ee6cd7f64
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-05-27 16:10:50 +0200

    Remove unused import

--- dc-cudami-editor/src/components/PublicationDatesForm.jsx
+++ dc-cudami-editor/src/components/PublicationDatesForm.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Col, FormGroup, Label, Row} from 'reactstrap'
 import DatePicker from 'react-date-picker'
 import {useTranslation} from 'react-i18next'
