0534ff62bb53e0450ac74b09fd52e4c7beadb800
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-05-28 16:58:51 +0200

    Add javadoc

--- dc-cudami-server/dc-cudami-server-backend-api/src/main/java/de/digitalcollections/cudami/server/backend/api/repository/identifiable/entity/parts/WebpageRepository.java
+++ dc-cudami-server/dc-cudami-server-backend-api/src/main/java/de/digitalcollections/cudami/server/backend/api/repository/identifiable/entity/parts/WebpageRepository.java
@@ -14,9 +14,23 @@ import java.util.UUID;
 public interface WebpageRepository<E extends Entity>
     extends NodeRepository<Webpage>, EntityPartRepository<Webpage, E> {
 
+  /**
+   * @param webpage newly created webpage to be saved
+   * @param parentWebsiteUUID website the (root) webpage belongs to
+   * @return saved webpage
+   */
   Webpage saveWithParentWebsite(Webpage webpage, UUID parentWebsiteUUID);
 
+  /**
+   * @param webpage newly created webpage to be saved
+   * @param parentWebpageUUID parent webpage the new webpage is child of
+   * @return saved webpage
+   */
   Webpage saveWithParentWebpage(Webpage webpage, UUID parentWebpageUUID);
 
+  /**
+   * @param rootWebpageUuid uuid of a webpage (webpage must be a top level webpage of the website)
+   * @return the website the given root-webpage belongs to (webpage is top level webpage)
+   */
   Website getWebsite(UUID rootWebpageUuid);
 }
