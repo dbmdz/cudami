13eb230605f3fc58ac13540736dac205d95443e2
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-06-04 11:24:10 +0200

    Add undefined checks

--- dc-cudami-editor/src/components/modals/utils.js
+++ dc-cudami-editor/src/components/modals/utils.js
@@ -1,12 +1,13 @@
 export function getImageUrl(image, width = 'full') {
+  if (!image.iiifBaseUrl || !image.mimeType) {
+    return image.uri
+  }
   const mimeExtensionMapping = {
     gif: 'gif',
     png: 'png',
   }
   const subMimeType = image.mimeType.split('/')[1]
-  return image.iiifBaseUrl
-    ? `${image.iiifBaseUrl}/full/${width}/0/default.${
-        mimeExtensionMapping[subMimeType] ?? 'jpg'
-      }`
-    : image.uri
+  return `${image.iiifBaseUrl}/full/${width}/0/default.${
+    mimeExtensionMapping[subMimeType] ?? 'jpg'
+  }`
 }
