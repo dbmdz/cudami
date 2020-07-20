3c8072030e9bf4d2b06c475f09f0e0a6e9e23768
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-07-20 15:03:45 +0200

    Fix uri concatenation

---
+++ dc-cudami-client/src/test/java/de/digitalcollections/cudami/client/CudamiBaseClientTest.java
@@ -0,0 +1,33 @@
+package de.digitalcollections.cudami.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import de.digitalcollections.model.api.identifiable.Identifiable;
+import de.digitalcollections.model.jackson.DigitalCollectionsObjectMapper;
+import java.net.URI;
+import org.junit.jupiter.api.Test;
+
+public class CudamiBaseClientTest {
+
+  @Test
+  public void testCreateFullUriWithoutPath() {
+    String serverUrl = "http://localhost:1234";
+    String requestUrl = "/foo/bar";
+    CudamiBaseClient base =
+        new CudamiBaseClient(serverUrl, Identifiable.class, new DigitalCollectionsObjectMapper());
+    URI result = base.createFullUri(requestUrl);
+    URI expectedResult = URI.create(serverUrl + requestUrl);
+    assertThat(result).isEqualTo(expectedResult);
+  }
+
+  @Test
+  public void testCreateFullUriWithPath() {
+    String serverUrl = "http://localhost:1234/cudami";
+    String requestUrl = "/foo/bar";
+    CudamiBaseClient base =
+        new CudamiBaseClient(serverUrl, Identifiable.class, new DigitalCollectionsObjectMapper());
+    URI result = base.createFullUri(requestUrl);
+    URI expectedResult = URI.create(serverUrl + requestUrl);
+    assertThat(result).isEqualTo(expectedResult);
+  }
+}
