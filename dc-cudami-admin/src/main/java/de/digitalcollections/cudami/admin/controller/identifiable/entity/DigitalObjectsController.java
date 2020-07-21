8e1f0ce94389e070793520e40d0d638daed723a4
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-07-21 09:31:56 +0200

    Remove unused create and edit endpoints

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/DigitalObjectsController.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/DigitalObjectsController.java
@@ -10,9 +10,7 @@ import de.digitalcollections.cudami.client.exceptions.HttpException;
 import de.digitalcollections.model.api.identifiable.entity.DigitalObject;
 import de.digitalcollections.model.api.paging.PageRequest;
 import de.digitalcollections.model.api.paging.PageResponse;
-import de.digitalcollections.model.impl.identifiable.entity.DigitalObjectImpl;
 import java.util.UUID;
-import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +19,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /** Controller for digital objects management pages. */
 @Controller
@@ -47,37 +41,6 @@ public class DigitalObjectsController extends AbstractController {
   @ModelAttribute("menu")
   protected String module() {
     return "digitalobjects";
-  }
-
-  @GetMapping("/digitalobjects/new")
-  public String create(Model model) {
-    throw new UnsupportedOperationException();
-  }
-
-  @PostMapping("/digitalobjects/new")
-  public String create(
-      @ModelAttribute @Valid DigitalObjectImpl digitalObject,
-      BindingResult results,
-      Model model,
-      SessionStatus status,
-      RedirectAttributes redirectAttributes) {
-    throw new UnsupportedOperationException();
-  }
-
-  @GetMapping("/digitalobjects/{uuid}/edit")
-  public String edit(@PathVariable UUID uuid, Model model, RedirectAttributes redirectAttributes) {
-    throw new UnsupportedOperationException();
-  }
-
-  @PostMapping("/digitalobjects/{pathUuid}/edit")
-  public String edit(
-      @PathVariable UUID pathUuid,
-      @ModelAttribute @Valid DigitalObjectImpl digitalObject,
-      BindingResult results,
-      Model model,
-      SessionStatus status,
-      RedirectAttributes redirectAttributes) {
-    throw new UnsupportedOperationException();
   }
 
   @GetMapping("/digitalobjects")
