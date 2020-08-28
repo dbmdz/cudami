04bb4c7f6577f9882aa92ce384127b8bcc1d91a5
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2020-08-28 10:28:18 +0200

    Basic predicates controller

---
+++ dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/impl/service/PredicatesServiceImpl.java
@@ -0,0 +1,30 @@
+package de.digitalcollections.cudami.server.business.impl.service;
+
+/**
+ * Service for managing predicates
+ */
+import de.digitalcollections.cudami.server.backend.api.repository.LocaleRepository;
+import de.digitalcollections.cudami.server.backend.api.repository.PredicatesRepository;
+import de.digitalcollections.cudami.server.business.api.service.PredicatesService;
+import de.digitalcollections.model.api.relations.Predicate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PredicatesServiceImpl implements PredicatesService {
+
+  @Autowired
+  private PredicatesRepository repository;
+
+  @Override
+  public List<Predicate> getPredicates() {
+    return repository.findAll();
+  }
+
+  @Override
+  public Predicate save(Predicate predicate) {
+    return repository.save(predicate);
+  }
+}
