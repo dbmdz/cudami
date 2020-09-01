1bef6c032eaa11a526330431ab558468219dad63
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-09-01 10:37:33 +0200

    Refactor grid system to real table and add usage of destructuring

--- dc-cudami-editor/src/components/IdentifiableListItem.jsx
+++ dc-cudami-editor/src/components/IdentifiableListItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, ButtonGroup, Col, ListGroupItem, Row} from 'reactstrap'
+import {Button, ButtonGroup} from 'reactstrap'
 import {useTranslation} from 'react-i18next'
 import {FaExchangeAlt, FaEye, FaPencilAlt, FaUnlink} from 'react-icons/fa'
 
@@ -27,80 +27,71 @@ const IdentifiableListItem = ({
   const {t} = useTranslation()
   const viewUrl = `${apiContextPath}${type.toLowerCase()}s/${uuid}`
   return (
-    <ListGroupItem className="list-item pb-0 pt-0">
-      <Row>
-        <Col className="border-right pb-1 pt-1 text-right" md="1">
-          {index}
-        </Col>
-        <Col className="border-right pb-1 pt-1 text-center" md="1">
-          <figure className="mb-0">
-            <img
-              alt={altText?.[uiLocale] ?? ''}
-              src={
-                previewImage
-                  ? getImageUrl(previewImage, `${previewImageWidth},`)
-                  : `${apiContextPath}images/no-image.png`
-              }
-              style={{maxWidth: `${previewImageWidth}px`}}
-              title={title?.[uiLocale]}
-            />
-            {caption?.[uiLocale] && (
-              <figcaption>{caption[uiLocale]}</figcaption>
-            )}
-          </figure>
-        </Col>
-        <Col md="7" className="border-right pb-1 pt-1">
-          <a href={viewUrl}>{label}</a>
-        </Col>
-        <Col className="border-right pb-1 pt-1 text-center" md="2">
-          {new Date(lastModified).toLocaleString(uiLocale, {hour12: false})}
-        </Col>
-        <Col className="pb-1 pt-1 text-center" md="1">
-          <ButtonGroup>
+    <tr>
+      <td className="text-right" scope="row">
+        {index}
+      </td>
+      <td className="text-center">
+        <figure className="mb-0">
+          <img
+            alt={altText?.[uiLocale] ?? ''}
+            src={
+              previewImage
+                ? getImageUrl(previewImage, `${previewImageWidth},`)
+                : `${apiContextPath}images/no-image.png`
+            }
+            style={{maxWidth: `${previewImageWidth}px`}}
+            title={title?.[uiLocale]}
+          />
+          {caption?.[uiLocale] && <figcaption>{caption[uiLocale]}</figcaption>}
+        </figure>
+      </td>
+      <td>
+        <a href={viewUrl}>{label}</a>
+      </td>
+      <td className="text-center">
+        {new Date(lastModified).toLocaleString(uiLocale, {hour12: false})}
+      </td>
+      <td className="text-center">
+        <ButtonGroup>
+          <Button className="p-0" color="link" href={viewUrl} title={t('view')}>
+            <FaEye />
+          </Button>
+          {showEdit && (
             <Button
-              className="p-0"
+              className="ml-1 p-0"
               color="link"
-              href={viewUrl}
-              title={t('view')}
+              href={`${viewUrl}/edit`}
+              title={t('edit')}
             >
-              <FaEye />
+              <FaPencilAlt />
             </Button>
-            {showEdit && (
-              <Button
-                className="ml-1 p-0"
-                color="link"
-                href={`${viewUrl}/edit`}
-                title={t('edit')}
-              >
-                <FaPencilAlt />
-              </Button>
-            )}
-          </ButtonGroup>
-          <ButtonGroup>
-            {enableMove && (
-              <Button
-                className="ml-1 p-0"
-                color="link"
-                onClick={onMove}
-                title={t(`moveTo.${parentType}`)}
-              >
-                <FaExchangeAlt />
-              </Button>
-            )}
-            {enableRemove && (
-              <Button
-                className="ml-1 p-0"
-                color="link"
-                onClick={onRemove}
-                title={t(`removeFrom.${parentType}`)}
-              >
-                <FaUnlink />
-              </Button>
-            )}
-          </ButtonGroup>
-        </Col>
-      </Row>
-    </ListGroupItem>
+          )}
+        </ButtonGroup>
+        <ButtonGroup>
+          {enableMove && (
+            <Button
+              className="ml-1 p-0"
+              color="link"
+              onClick={onMove}
+              title={t(`moveTo.${parentType}`)}
+            >
+              <FaExchangeAlt />
+            </Button>
+          )}
+          {enableRemove && (
+            <Button
+              className="ml-1 p-0"
+              color="link"
+              onClick={onRemove}
+              title={t(`removeFrom.${parentType}`)}
+            >
+              <FaUnlink />
+            </Button>
+          )}
+        </ButtonGroup>
+      </td>
+    </tr>
   )
 }
 
