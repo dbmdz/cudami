f9b0cc80469f474af5b315677186d226b50371bd
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-09-03 14:24:11 +0200

    Add simplified return

--- dc-cudami-editor/src/components/InfoTooltip.jsx
+++ dc-cudami-editor/src/components/InfoTooltip.jsx
@@ -2,27 +2,25 @@ import React from 'react'
 import {Button, Popover, PopoverBody} from 'reactstrap'
 import {FaQuestionCircle} from 'react-icons/fa'
 
-const InfoTooltip = ({isOpen, name, text, toggle}) => {
-  return (
-    <>
-      <Button
-        className="border"
-        color="light"
-        id={`${name}-tooltip`}
-        type="button"
-      >
-        <FaQuestionCircle />
-      </Button>
-      <Popover
-        isOpen={isOpen}
-        placement="left"
-        target={`${name}-tooltip`}
-        toggle={toggle}
-      >
-        <PopoverBody>{text}</PopoverBody>
-      </Popover>
-    </>
-  )
-}
+const InfoTooltip = ({isOpen, name, text, toggle}) => (
+  <>
+    <Button
+      className="border"
+      color="light"
+      id={`${name}-tooltip`}
+      type="button"
+    >
+      <FaQuestionCircle />
+    </Button>
+    <Popover
+      isOpen={isOpen}
+      placement="left"
+      target={`${name}-tooltip`}
+      toggle={toggle}
+    >
+      <PopoverBody>{text}</PopoverBody>
+    </Popover>
+  </>
+)
 
 export default InfoTooltip
