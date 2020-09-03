5e63847eb4d0e29a610bc976a91346b076200f1b
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-09-03 10:54:30 +0200

    Add default values for the context with docstrings

--- dc-cudami-editor/src/components/AppContext.js
+++ dc-cudami-editor/src/components/AppContext.js
@@ -1,3 +1,10 @@
 import {createContext} from 'react'
 
-export default createContext(null)
+export default createContext({
+  // the context path of the surrounding java application
+  apiContextPath: '/',
+  // the defined default language for multilanguage fields, will be fetched from the api
+  defaultLanguage: 'en',
+  // a flag whether the api calls should be mocked, the mock data is located in /public/__mock__
+  mockApi: false,
+})
