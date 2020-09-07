627c5b318956190f021ca50db69b0007f4b2fbef
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-09-07 11:01:22 +0200

    Add feedback messages when adding and removing attached identifiables

---
+++ dc-cudami-editor/src/components/FeedbackMessage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Alert, Button} from 'reactstrap'
+import {Trans} from 'react-i18next'
+import {FaTimes} from 'react-icons/fa'
+
+const FeedbackMessage = ({className = 'mb-0', message, onClose}) => {
+  let {color = 'info', key, links = [], values} = message
+  links = links.map((link) => <a href={link}></a>)
+  return (
+    <Alert className={className} color={color}>
+      <Trans components={links} i18nKey={key} values={values} />
+      {onClose && (
+        <Button
+          className="close float-right p-0"
+          color="link"
+          onClick={onClose}
+        >
+          <FaTimes size={16} />
+        </Button>
+      )}
+    </Alert>
+  )
+}
+
+export default FeedbackMessage
