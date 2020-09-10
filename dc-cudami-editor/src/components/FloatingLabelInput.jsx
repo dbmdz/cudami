94ba23d08bf879f8b54d96db389893a9de8e1241
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-09-10 09:08:17 +0200

    Add possibility to define required of floating label input

--- dc-cudami-editor/src/components/FloatingLabelInput.jsx
+++ dc-cudami-editor/src/components/FloatingLabelInput.jsx
@@ -2,7 +2,14 @@ import classNames from 'classnames'
 import React, {useState} from 'react'
 import {Input, Label} from 'reactstrap'
 
-const FloatingLabelInput = ({label, name, onChange, type = 'text', value}) => {
+const FloatingLabelInput = ({
+  label,
+  name,
+  onChange,
+  required,
+  type = 'text',
+  value,
+}) => {
   const [focussed, setFocussed] = useState(false)
   return (
     <div className="border-0 form-control p-0 position-relative">
@@ -13,6 +20,7 @@ const FloatingLabelInput = ({label, name, onChange, type = 'text', value}) => {
         onBlur={() => setFocussed(false)}
         onChange={(evt) => onChange(evt.target.value)}
         onFocus={() => setFocussed(true)}
+        required={required}
         type={type}
         value={value}
       />
