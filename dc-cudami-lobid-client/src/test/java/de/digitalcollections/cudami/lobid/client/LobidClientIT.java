4c00af781bf97b14371242f2d7baa1bafff5efdf
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-09-11 09:42:11 +0200

    Add test assert

--- dc-cudami-lobid-client/src/test/java/de/digitalcollections/cudami/lobid/client/LobidClientIT.java
+++ dc-cudami-lobid-client/src/test/java/de/digitalcollections/cudami/lobid/client/LobidClientIT.java
@@ -2,6 +2,7 @@ package de.digitalcollections.cudami.lobid.client;
 
 import de.digitalcollections.cudami.lobid.client.exceptions.HttpException;
 import de.digitalcollections.model.api.identifiable.entity.Corporation;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LobidClientIT {
@@ -11,7 +12,6 @@ public class LobidClientIT {
   @Test
   public void getCorporationByGndId() throws HttpException {
     Corporation corporation = new LobidClient().forCorporations().getByGndId("2007744-0");
-    
-    System.out.println("test");
+    Assertions.assertEquals("Deutsche Forschungsgemeinschaft (DFG)", corporation.getLabel().getText());
   }
 }
