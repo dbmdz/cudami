6d2cebcea3f4f259f18ab7e3368823f78b70d5fc
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-09-14 14:42:28 +0200

    Move injection of link editing toolbar to place where it is really needed

--- dc-cudami-editor/src/config/custom/LinkEditingToolbar.jsx
+++ dc-cudami-editor/src/config/custom/LinkEditingToolbar.jsx
@@ -37,12 +37,6 @@ class LinkEditingToolbar {
   update(view, lastState) {
     const {dom, state} = view
 
-    // Add the toolbar to the dom if it was not alredy appended
-    const toolbar = dom.parentNode.querySelector('.link-editing-toolbar')
-    if (!toolbar) {
-      dom.parentNode.appendChild(this.toolbar)
-    }
-
     // Don't do anything if the document/selection didn't change
     if (
       lastState &&
@@ -71,6 +65,12 @@ class LinkEditingToolbar {
 
     // Also set the current selection as class attribute, it is later needed to update the mark
     this.selection = state.selection
+
+    // Add the toolbar to the dom if it was not alredy appended
+    const toolbar = dom.parentNode.querySelector('.link-editing-toolbar')
+    if (!toolbar) {
+      dom.parentNode.appendChild(this.toolbar)
+    }
 
     // Show the toolbar if we have a link
     this.toolbar.classList.remove('d-none')
