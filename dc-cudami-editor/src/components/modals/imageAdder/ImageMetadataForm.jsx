92dbcb1c6140d43580f2cac1c3441c91514428e6
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-09-15 13:18:41 +0200

    Make all form fields in modals optional

--- dc-cudami-editor/src/components/modals/imageAdder/ImageMetadataForm.jsx
+++ dc-cudami-editor/src/components/modals/imageAdder/ImageMetadataForm.jsx
@@ -17,6 +17,9 @@ import InfoTooltip from '../../InfoTooltip'
 const ImageMetadataForm = ({
   altText,
   caption,
+  enableAltText,
+  enableCaption,
+  enableTitle,
   isOpen,
   onChange,
   title,
@@ -26,7 +29,7 @@ const ImageMetadataForm = ({
 }) => {
   const {t} = useTranslation()
   return (
-    <Card className="mb-2">
+    <Card className="image-metadata mb-2">
       <CardHeader>
         <Button className="font-weight-bold p-0" color="link" onClick={toggle}>
           {t('enterMetadata')}
@@ -34,64 +37,76 @@ const ImageMetadataForm = ({
       </CardHeader>
       <Collapse isOpen={isOpen}>
         <CardBody>
-          <FormGroup>
-            <InputGroup>
-              <FloatingLabelInput
-                label={t('caption')}
-                name="image-caption"
-                onChange={(value) => onChange('caption', value)}
-                value={caption}
-              />
-              <InputGroupAddon addonType="append">
-                <InfoTooltip
-                  isOpen={tooltipsOpen.caption}
+          {enableCaption && (
+            <FormGroup>
+              <InputGroup>
+                <FloatingLabelInput
+                  label={t('caption')}
                   name="image-caption"
-                  text={t('tooltips.caption')}
-                  toggle={() => toggleTooltip('caption')}
+                  onChange={(value) => onChange('caption', value)}
+                  value={caption}
                 />
-              </InputGroupAddon>
-            </InputGroup>
-          </FormGroup>
-          <FormGroup>
-            <InputGroup>
-              <FloatingLabelInput
-                label={t('tooltip')}
-                name="image-title"
-                onChange={(value) => onChange('title', value)}
-                value={title}
-              />
-              <InputGroupAddon addonType="append">
-                <InfoTooltip
-                  isOpen={tooltipsOpen.title}
+                <InputGroupAddon addonType="append">
+                  <InfoTooltip
+                    isOpen={tooltipsOpen.caption}
+                    name="image-caption"
+                    text={t('tooltips.caption')}
+                    toggle={() => toggleTooltip('caption')}
+                  />
+                </InputGroupAddon>
+              </InputGroup>
+            </FormGroup>
+          )}
+          {enableTitle && (
+            <FormGroup>
+              <InputGroup>
+                <FloatingLabelInput
+                  label={t('tooltip')}
                   name="image-title"
-                  text={t('tooltips.title')}
-                  toggle={() => toggleTooltip('title')}
+                  onChange={(value) => onChange('title', value)}
+                  value={title}
                 />
-              </InputGroupAddon>
-            </InputGroup>
-          </FormGroup>
-          <FormGroup className="mb-0">
-            <InputGroup>
-              <FloatingLabelInput
-                label={t('altText')}
-                name="image-alttext"
-                onChange={(value) => onChange('altText', value)}
-                value={altText}
-              />
-              <InputGroupAddon addonType="append">
-                <InfoTooltip
-                  isOpen={tooltipsOpen.altText}
+                <InputGroupAddon addonType="append">
+                  <InfoTooltip
+                    isOpen={tooltipsOpen.title}
+                    name="image-title"
+                    text={t('tooltips.title')}
+                    toggle={() => toggleTooltip('title')}
+                  />
+                </InputGroupAddon>
+              </InputGroup>
+            </FormGroup>
+          )}
+          {enableAltText && (
+            <FormGroup>
+              <InputGroup>
+                <FloatingLabelInput
+                  label={t('altText')}
                   name="image-alttext"
-                  text={t('tooltips.altText')}
-                  toggle={() => toggleTooltip('altText')}
+                  onChange={(value) => onChange('altText', value)}
+                  value={altText}
                 />
-              </InputGroupAddon>
-            </InputGroup>
-          </FormGroup>
+                <InputGroupAddon addonType="append">
+                  <InfoTooltip
+                    isOpen={tooltipsOpen.altText}
+                    name="image-alttext"
+                    text={t('tooltips.altText')}
+                    toggle={() => toggleTooltip('altText')}
+                  />
+                </InputGroupAddon>
+              </InputGroup>
+            </FormGroup>
+          )}
         </CardBody>
       </Collapse>
     </Card>
   )
+}
+
+ImageMetadataForm.defaultProps = {
+  enableAltText: true,
+  enableCaption: true,
+  enableTitle: true,
 }
 
 export default ImageMetadataForm
