2e080cd314aa11592fe9973a8cd6732aa1f51931
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2020-09-17 14:09:42 +0200

    Simplified migration script

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V2_0_10__DDL_Add_Unique_Constraint_To_Identifiers.sql
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V2_0_10__DDL_Add_Unique_Constraint_To_Identifiers.sql
@@ -1,9 +1,7 @@
 DO $$
 BEGIN
-    BEGIN
-        ALTER TABLE identifiers
-            ADD CONSTRAINT unique_namespace_identifier UNIQUE(namespace, identifier);
-    EXCEPTION
-        WHEN OTHERS THEN RAISE NOTICE 'constraint unique_namespace_identifier already exists';
-    END;
+    ALTER TABLE identifiers
+        ADD CONSTRAINT unique_namespace_identifier UNIQUE(namespace, identifier);
+EXCEPTION
+    WHEN OTHERS THEN RAISE NOTICE 'constraint unique_namespace_identifier already exists';
 END $$;
