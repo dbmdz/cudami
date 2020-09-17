65249c826f66f0ff94873453a57e55389ec64754
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2020-09-17 09:24:57 +0200

    Add javadoc for deprecated method; use urlencoding for free text parameter

--- dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/CudamiPredicatesClient.java
+++ dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/CudamiPredicatesClient.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.digitalcollections.cudami.client.exceptions.HttpException;
 import de.digitalcollections.model.api.relations.Predicate;
 import de.digitalcollections.model.impl.relations.PredicateImpl;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class CudamiPredicatesClient extends CudamiBaseClient<PredicateImpl> {
@@ -19,6 +21,9 @@ public class CudamiPredicatesClient extends CudamiBaseClient<PredicateImpl> {
 
   public Predicate save(Predicate predicate) throws HttpException {
     return doPutRequestForObject(
-        String.format("/latest/predicates/%s", predicate.getValue()), (PredicateImpl) predicate);
+        String.format(
+            "/latest/predicates/%s",
+            URLEncoder.encode(predicate.getValue()), StandardCharsets.UTF_8),
+        (PredicateImpl) predicate);
   }
 }
