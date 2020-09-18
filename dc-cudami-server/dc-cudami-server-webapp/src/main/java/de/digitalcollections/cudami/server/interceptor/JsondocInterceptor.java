ba1fd9cc501fcf575eb8d471037009593a1ebc87
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2020-09-18 13:58:02 +0200

    Fix jsondoc base path

---
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/interceptor/JsondocInterceptor.java
@@ -0,0 +1,35 @@
+package de.digitalcollections.cudami.server.interceptor;
+
+import java.lang.reflect.Field;
+import java.net.URL;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsondoc.spring.boot.starter.JSONDocProperties;
+import org.jsondoc.springmvc.controller.JSONDocController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class JsondocInterceptor extends HandlerInterceptorAdapter {
+
+  @Autowired private JSONDocProperties jsonDocProperties;
+
+  @Autowired private JSONDocController jsonDocController;
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    final URL baseURLFromRequest =
+        new URL(
+            request.getScheme(),
+            request.getServerName(),
+            request.getServerPort(),
+            request.getContextPath());
+    jsonDocProperties.setBasePath(baseURLFromRequest.toExternalForm());
+    final Field basePath = jsonDocController.getClass().getDeclaredField("basePath");
+    basePath.setAccessible(true);
+    basePath.set(jsonDocController, jsonDocProperties.getBasePath());
+    return super.preHandle(request, response, handler);
+  }
+}
