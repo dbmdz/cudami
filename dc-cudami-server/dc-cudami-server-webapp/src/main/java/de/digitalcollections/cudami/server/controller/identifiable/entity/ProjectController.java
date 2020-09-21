823aba73d15fc1696ef9001c5588450b2a4f84ca
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-09-21 09:24:11 +0200

    Fix formatting

--- dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/identifiable/entity/ProjectController.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/identifiable/entity/ProjectController.java
@@ -89,7 +89,7 @@ public class ProjectController {
       throws IdentifiableServiceException {
     return projectService.getByIdentifier(namespace, id);
   }
-  
+
   // Test-URL: http://localhost:9000/latest/projects/599a120c-2dd5-11e8-b467-0ed5f89f718b
   @ApiMethod(
       description =
