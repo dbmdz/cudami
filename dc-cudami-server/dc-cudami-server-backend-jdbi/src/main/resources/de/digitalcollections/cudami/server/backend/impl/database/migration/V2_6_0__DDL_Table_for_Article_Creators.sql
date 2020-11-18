e200f4175e246fef6ede9da07435f54d50040173
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-11-18 14:02:26 +0100

    Update dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V2_6_0__DDL_Table_for_Article_Creators.sql
    Co-authored-by: Matthias Lindinger <morpheus-87@users.noreply.github.com>

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V2_6_0__DDL_Table_for_Article_Creators.sql
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V2_6_0__DDL_Table_for_Article_Creators.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS article_creators (
   sortIndex SMALLINT,
 
   PRIMARY KEY (article_uuid, agent_uuid)
+--   Unfortunately not supported for tables with inheritance:
 --   FOREIGN KEY (article_uuid) REFERENCES articles(uuid),
 --   FOREIGN KEY (agent_uuid) REFERENCES entities(uuid)
 );
