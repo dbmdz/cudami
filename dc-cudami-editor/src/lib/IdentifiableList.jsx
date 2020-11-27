ae80adc9f571d3a834b7d6667a1c12cd2a2bbcd3
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-11-27 12:05:21 +0100

    Add missing new option

--- dc-cudami-editor/src/lib/IdentifiableList.jsx
+++ dc-cudami-editor/src/lib/IdentifiableList.jsx
@@ -24,6 +24,7 @@ export default function ({
     <PagedIdentifiableList
       apiContextPath={apiContextPath}
       enableAdd={enableAdd}
+      enableChangeOfOrder={enableChangeOfOrder}
       enableMove={enableMove}
       enableRemove={enableRemove}
       existingLanguages={existingLanguages}
