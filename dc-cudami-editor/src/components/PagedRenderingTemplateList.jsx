d3ba49fd8828039ede406a27b7e4f4aa8c33ef2e
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-12-15 15:33:26 +0100

    Add new components for handling of rendering template

---
+++ dc-cudami-editor/src/components/PagedRenderingTemplateList.jsx
@@ -0,0 +1,128 @@
+import React, {useEffect, useState} from 'react'
+import {Button, Card, CardBody, Col, Nav, Row, Table} from 'reactstrap'
+import {useTranslation} from 'react-i18next'
+
+import LanguageTab from './LanguageTab'
+import ListButtons from './ListButtons'
+import ListPagination from './ListPagination'
+import {
+  loadDefaultLanguage,
+  loadIdentifiables,
+  typeToEndpointMapping,
+} from '../api'
+
+const loadData = async (context, mock, pageNumber, pageSize = 20) => {
+  const defaultLanguage = await loadDefaultLanguage(context, mock)
+  const {content, totalElements} = await loadIdentifiables(
+    context,
+    mock,
+    'renderingTemplate',
+    pageNumber,
+    pageSize
+  )
+  return {
+    defaultLanguage,
+    numberOfPages: Math.ceil(totalElements / pageSize),
+    templates: content,
+    totalElements,
+  }
+}
+
+const PagedRenderingTemplateList = ({
+  apiContextPath = '/',
+  mockApi = false,
+}) => {
+  const [defaultLanguage, setDefaultLanguage] = useState('')
+  const [numberOfPages, setNumberOfPages] = useState(0)
+  const [pageNumber, setPageNumber] = useState(0)
+  const [templates, setTemplates] = useState([])
+  const [totalElements, setTotalElements] = useState(0)
+  useEffect(() => {
+    loadData(apiContextPath, mockApi, pageNumber).then(
+      ({defaultLanguage, numberOfPages, templates, totalElements}) => {
+        setDefaultLanguage(defaultLanguage)
+        setNumberOfPages(numberOfPages)
+        setTemplates(templates)
+        setTotalElements(totalElements)
+      }
+    )
+  }, [pageNumber])
+  const {t} = useTranslation()
+  const type = 'renderingTemplate'
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>{t('renderingTemplates')}</h1>
+        </Col>
+        <Col className="text-right">
+          <Button href={`${apiContextPath}${typeToEndpointMapping[type]}/new`}>
+            {t('new')}
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <hr />
+        </Col>
+      </Row>
+      <Nav tabs>
+        <LanguageTab
+          activeLanguage={defaultLanguage}
+          language={defaultLanguage}
+          toggle={() => {}}
+        />
+      </Nav>
+      <Card className="border-top-0">
+        <CardBody>
+          <ListPagination
+            changePage={({selected}) => setPageNumber(selected)}
+            numberOfPages={numberOfPages}
+            pageNumber={pageNumber}
+            totalElements={totalElements}
+            type={type}
+          />
+          <Table bordered className="mb-0" hover responsive size="sm" striped>
+            <thead>
+              <tr>
+                <th className="text-center">{t('label')}</th>
+                <th className="text-center">{t('description')}</th>
+                <th className="text-center">{t('name')}</th>
+                <th className="text-center">{t('actions')}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {templates.map(({description, label, name, uuid}) => (
+                <tr key={uuid}>
+                  <td>{label[defaultLanguage]}</td>
+                  <td>{description[defaultLanguage]}</td>
+                  <td>{name}</td>
+                  <td className="text-center">
+                    <ListButtons
+                      editUrl={`${apiContextPath}${typeToEndpointMapping[type]}/${uuid}/edit`}
+                      showEdit
+                      showView={false}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <ListPagination
+            changePage={({selected}) => setPageNumber(selected)}
+            numberOfPages={numberOfPages}
+            pageNumber={pageNumber}
+            position="under"
+            showTotalElements={false}
+            totalElements={totalElements}
+            type={type}
+          />
+        </CardBody>
+      </Card>
+    </>
+  )
+}
+
+export default PagedRenderingTemplateList
+
+// const viewBaseUrl = `${apiContextPath}${typeToEndpointMapping[type]}`
---
+++ dc-cudami-editor/src/components/RenderingTemplateForm.jsx
@@ -0,0 +1,157 @@
+import React, {useEffect, useState} from 'react'
+import {
+  Card,
+  CardBody,
+  Col,
+  Form,
+  Nav,
+  Row,
+  TabContent,
+  TabPane,
+} from 'reactstrap'
+import {useTranslation} from 'react-i18next'
+
+import FormButtons from './FormButtons'
+import FormInput from './FormInput'
+import LanguageTab from './LanguageTab'
+import {
+  loadDefaultLanguage,
+  loadIdentifiable,
+  saveIdentifiable,
+  typeToEndpointMapping,
+  updateIdentifiable,
+} from '../api'
+import '../polyfills'
+
+const loadData = async (context, mock, uuid) => {
+  const defaultLanguage = await loadDefaultLanguage(context, mock)
+  let template = await loadIdentifiable(
+    context,
+    mock,
+    'renderingTemplate',
+    uuid
+  )
+  return {
+    defaultLanguage,
+    template: {
+      description: {},
+      label: {},
+      name: '',
+      ...template,
+    },
+  }
+}
+
+const submitData = async (context, data, uuid) => {
+  const type = 'renderingTemplate'
+  //let response
+  if (uuid) {
+    await updateIdentifiable(context, data, type, false)
+  } else {
+    await saveIdentifiable(context, data, null, null, type, false)
+  }
+  window.location.href = `${context}${typeToEndpointMapping[type]}`
+}
+
+const RenderingTemplateForm = ({
+  apiContextPath = '/',
+  mockApi = false,
+  uuid,
+}) => {
+  useEffect(() => {
+    loadData(apiContextPath, mockApi, uuid).then(
+      ({defaultLanguage, template}) => {
+        setDefaultLanguage(defaultLanguage)
+        setTemplate(template)
+      }
+    )
+  }, [])
+  const [defaultLanguage, setDefaultLanguage] = useState('')
+  const [template, setTemplate] = useState(null)
+  const {t} = useTranslation()
+  if (!template) {
+    return null
+  }
+  const {description, label, name} = template
+  return (
+    <Form
+      onSubmit={(evt) => {
+        evt.preventDefault()
+        submitData(apiContextPath, template, uuid)
+      }}
+    >
+      <Row>
+        <Col xs="6" sm="9">
+          <h1>
+            {uuid
+              ? t('editRenderingTemplate', {name})
+              : t('createRenderingTemplate')}
+          </h1>
+        </Col>
+        <Col xs="6" sm="3">
+          <FormButtons />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm="12">
+          <hr />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm="12">
+          {uuid && <FormInput id="uuid" label="ID" readOnly value={uuid} />}
+          <FormInput
+            id="name"
+            labelKey="name"
+            onChange={(name) => setTemplate({...template, name})}
+            required
+            value={name}
+          />
+          <Nav tabs>
+            <LanguageTab
+              activeLanguage={defaultLanguage}
+              language={defaultLanguage}
+              toggle={() => {}}
+            />
+          </Nav>
+          <TabContent activeTab={defaultLanguage}>
+            <TabPane tabId={defaultLanguage}>
+              <Card className="bg-light">
+                <CardBody>
+                  <FormInput
+                    id="label"
+                    labelKey="label"
+                    onChange={(label) =>
+                      setTemplate({
+                        ...template,
+                        label: {
+                          [defaultLanguage]: label,
+                        },
+                      })
+                    }
+                    value={label[defaultLanguage]}
+                  />
+                  <FormInput
+                    id="description"
+                    labelKey="description"
+                    onChange={(description) =>
+                      setTemplate({
+                        ...template,
+                        description: {
+                          [defaultLanguage]: description,
+                        },
+                      })
+                    }
+                    value={description[defaultLanguage]}
+                  />
+                </CardBody>
+              </Card>
+            </TabPane>
+          </TabContent>
+        </Col>
+      </Row>
+    </Form>
+  )
+}
+
+export default RenderingTemplateForm
