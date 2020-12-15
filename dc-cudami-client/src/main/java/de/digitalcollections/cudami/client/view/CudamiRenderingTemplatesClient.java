6622653b209c802e2b936e2bc28bedfb522be640
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-12-15 14:17:42 +0100

    Add client for rendering templates

---
+++ dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/view/CudamiRenderingTemplatesClient.java
@@ -0,0 +1,40 @@
+package de.digitalcollections.cudami.client.view;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.digitalcollections.cudami.client.CudamiBaseClient;
+import de.digitalcollections.cudami.client.exceptions.HttpException;
+import de.digitalcollections.model.api.paging.PageRequest;
+import de.digitalcollections.model.api.paging.PageResponse;
+import de.digitalcollections.model.impl.view.RenderingTemplate;
+import java.net.http.HttpClient;
+import java.util.UUID;
+
+public class CudamiRenderingTemplatesClient extends CudamiBaseClient<RenderingTemplate> {
+  public CudamiRenderingTemplatesClient(HttpClient http, String serverUrl, ObjectMapper mapper) {
+    super(http, serverUrl, RenderingTemplate.class, mapper);
+  }
+
+  public RenderingTemplate create() {
+    return new RenderingTemplate();
+  }
+
+  public PageResponse<RenderingTemplate> find(PageRequest pageRequest) throws HttpException {
+    return doGetRequestForPagedObjectList("/latest/renderingtemplates", pageRequest);
+  }
+
+  public RenderingTemplate findOne(UUID uuid) throws HttpException {
+    return doGetRequestForObject(String.format("/latest/renderingtemplates/%s", uuid));
+  }
+
+  public RenderingTemplate save(RenderingTemplate template) throws HttpException {
+    return doPostRequestForObject("/latest/renderingtemplates", template);
+  }
+
+  public RenderingTemplate update(UUID uuid, RenderingTemplate template) throws HttpException {
+    return doPutRequestForObject(String.format("/latest/renderingtemplates/%s", uuid), template);
+  }
+
+  /*
+  - find
+  */
+}
