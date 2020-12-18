e40464bc6509a7867ef4d9fba6a1dd23cf0255dd
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-12-18 13:40:28 +0100

    Add functionality to add a rendering template for a webpage

--- dc-cudami-editor/src/components/WebpageForm.jsx
+++ dc-cudami-editor/src/components/WebpageForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   Card,
   CardBody,
@@ -16,9 +16,11 @@ import FormButtons from './FormButtons'
 import FormDateInput from './FormDateInput'
 import FormEditor from './FormEditor'
 import FormInput from './FormInput'
+import FormTemplateSelector from './FormTemplateSelector'
 import LanguageAdder from './LanguageAdder'
 import LanguageTab from './LanguageTab'
 import Teaser from './Teaser'
+import SelectRenderingTemplateModal from './modals/SelectRenderingTemplateModal'
 
 const WebpageForm = ({
   activeLanguage,
@@ -30,133 +32,164 @@ const WebpageForm = ({
   onToggleLanguage,
   onUpdate,
 }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const {t} = useTranslation()
+  const {
+    description,
+    label,
+    previewImage,
+    previewImageRenderingHints,
+    publicationEnd,
+    publicationStart,
+    renderingHints,
+    text,
+    uuid,
+  } = identifiable
   return (
-    <Form
-      onSubmit={(evt) => {
-        evt.preventDefault()
-        onSubmit()
-      }}
-    >
-      <Row>
-        <Col xs="6" sm="9">
-          <h1>{identifiable.uuid ? t('editWebpage') : t('createWebpage')}</h1>
-        </Col>
-        <Col xs="6" sm="3">
-          <FormButtons />
-        </Col>
-      </Row>
-      <Row>
-        <Col sm="12">
-          <hr />
-        </Col>
-      </Row>
-      {identifiable.uuid && (
+    <>
+      <Form
+        onSubmit={(evt) => {
+          evt.preventDefault()
+          onSubmit()
+        }}
+      >
+        <Row>
+          <Col xs="6" sm="9">
+            <h1>{uuid ? t('editWebpage') : t('createWebpage')}</h1>
+          </Col>
+          <Col xs="6" sm="3">
+            <FormButtons />
+          </Col>
+        </Row>
         <Row>
           <Col sm="12">
-            <FormInput
-              id="uuid"
-              label="ID"
-              readOnly
-              value={identifiable.uuid}
+            <hr />
+          </Col>
+        </Row>
+        {uuid && (
+          <Row>
+            <Col sm="12">
+              <FormInput id="uuid" label="ID" readOnly value={uuid} />
+            </Col>
+          </Row>
+        )}
+        <Row>
+          <Col sm="3">
+            <FormDateInput
+              id="publication-start-date"
+              label={t('publicationStatus.startDate')}
+              onChange={(date) => onUpdate({publicationStart: date})}
+              value={publicationStart}
+            />
+          </Col>
+          <Col sm="3">
+            <FormDateInput
+              id="publication-end-date"
+              label={t('publicationStatus.endDate')}
+              onChange={(date) => onUpdate({publicationEnd: date})}
+              value={publicationEnd}
+            />
+          </Col>
+          <Col sm="3">
+            <FormTemplateSelector
+              onClick={() => setIsModalOpen(true)}
+              onRemove={() =>
+                onUpdate({
+                  renderingHints: {
+                    ...renderingHints,
+                    templateName: undefined,
+                  },
+                })
+              }
+              templateName={renderingHints.templateName}
             />
           </Col>
         </Row>
-      )}
-      <Row>
-        <Col sm="3">
-          <FormDateInput
-            id="publication-start-date"
-            label={t('publicationStatus.startDate')}
-            onChange={(date) => onUpdate({publicationStart: date})}
-            value={identifiable.publicationStart}
-          />
-        </Col>
-        <Col sm="3">
-          <FormDateInput
-            id="publication-end-date"
-            label={t('publicationStatus.endDate')}
-            onChange={(date) => onUpdate({publicationEnd: date})}
-            value={identifiable.publicationEnd}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col sm="12">
-          <Nav tabs>
-            {existingLanguages.map((language) => (
-              <LanguageTab
-                activeLanguage={activeLanguage}
-                key={language}
-                language={language}
-                toggle={onToggleLanguage}
-              />
-            ))}
-            {canAddLanguage && <LanguageAdder onClick={onAddLanguage} />}
-          </Nav>
-          <TabContent activeTab={activeLanguage}>
-            {existingLanguages.map((language) => (
-              /* TODO: extract as component */
-              <TabPane key={language} tabId={language}>
-                <Teaser
-                  description={identifiable.description[language]}
-                  label={identifiable.label[language]}
+        <Row>
+          <Col sm="12">
+            <Nav tabs>
+              {existingLanguages.map((language) => (
+                <LanguageTab
+                  activeLanguage={activeLanguage}
+                  key={language}
                   language={language}
-                  onUpdate={(updateKey, updateValue) =>
-                    onUpdate({
-                      [updateKey]: {
-                        ...identifiable[updateKey],
-                        [language]: updateValue,
-                      },
-                    })
-                  }
-                  previewImage={identifiable.previewImage}
-                  previewImageRenderingHints={
-                    identifiable.previewImageRenderingHints
-                  }
-                  updatePreviewImage={onUpdate}
+                  toggle={onToggleLanguage}
                 />
-                <Card className="border-top-0">
-                  <CardBody>
-                    <FormEditor
-                      document={identifiable.text[language]}
-                      type="text"
-                      onUpdate={(document) => {
-                        onUpdate({
-                          text: {
-                            ...identifiable.text,
-                            [language]: document,
-                          },
-                        })
-                      }}
-                    >
-                      <CustomInput
-                        checked={
-                          identifiable.renderingHints.showInPageNavigation
-                        }
-                        className="ml-4"
-                        id="show-in-page-navigation"
-                        inline
-                        label={t('showInPageNavigation')}
-                        onChange={(evt) => {
+              ))}
+              {canAddLanguage && <LanguageAdder onClick={onAddLanguage} />}
+            </Nav>
+            <TabContent activeTab={activeLanguage}>
+              {existingLanguages.map((language) => (
+                /* TODO: extract as component */
+                <TabPane key={language} tabId={language}>
+                  <Teaser
+                    description={description[language]}
+                    label={label[language]}
+                    language={language}
+                    onUpdate={(updateKey, updateValue) =>
+                      onUpdate({
+                        [updateKey]: {
+                          ...identifiable[updateKey],
+                          [language]: updateValue,
+                        },
+                      })
+                    }
+                    previewImage={previewImage}
+                    previewImageRenderingHints={previewImageRenderingHints}
+                    updatePreviewImage={onUpdate}
+                  />
+                  <Card className="border-top-0">
+                    <CardBody>
+                      <FormEditor
+                        document={text[language]}
+                        type="text"
+                        onUpdate={(document) => {
                           onUpdate({
-                            renderingHints: {
-                              ...identifiable.renderingHints,
-                              showInPageNavigation: evt.target.checked,
+                            text: {
+                              ...text,
+                              [language]: document,
                             },
                           })
                         }}
-                        type="switch"
-                      />
-                    </FormEditor>
-                  </CardBody>
-                </Card>
-              </TabPane>
-            ))}
-          </TabContent>
-        </Col>
-      </Row>
-    </Form>
+                      >
+                        <CustomInput
+                          checked={renderingHints.showInPageNavigation}
+                          className="ml-4"
+                          id="show-in-page-navigation"
+                          inline
+                          label={t('showInPageNavigation')}
+                          onChange={(evt) => {
+                            onUpdate({
+                              renderingHints: {
+                                ...renderingHints,
+                                showInPageNavigation: evt.target.checked,
+                              },
+                            })
+                          }}
+                          type="switch"
+                        />
+                      </FormEditor>
+                    </CardBody>
+                  </Card>
+                </TabPane>
+              ))}
+            </TabContent>
+          </Col>
+        </Row>
+      </Form>
+      <SelectRenderingTemplateModal
+        isOpen={isModalOpen}
+        onSelect={(templateName) =>
+          onUpdate({
+            renderingHints: {
+              ...renderingHints,
+              templateName,
+            },
+          })
+        }
+        toggle={() => setIsModalOpen(!isModalOpen)}
+      />
+    </>
   )
 }
 
