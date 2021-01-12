c8d31aef35eafa59ccb4819437fd335aff84c227
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-01-12 15:46:26 +0100

    Add sticky buttons to top navigation

--- dc-cudami-editor/src/components/FormButtons.jsx
+++ dc-cudami-editor/src/components/FormButtons.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
-import {Button, ButtonGroup} from 'reactstrap'
+import {Button, ButtonGroup, NavItem} from 'reactstrap'
+import {createPortal} from 'react-dom'
 import {useTranslation} from 'react-i18next'
+import {useInView} from 'react-intersection-observer'
 
-const FormButtons = () => {
+const FormButtonsInNavbar = ({buttons}) => {
+  return createPortal(
+    <NavItem className="border-left ml-2 pl-3">{buttons}</NavItem>,
+    document.querySelector('.navbar-nav')
+  )
+}
+
+const FormButtons = ({formId}) => {
+  const {inView, ref} = useInView({
+    delay: 100,
+    initialInView: true,
+    threshold: 1,
+    trackVisibility: true,
+  })
   const {t} = useTranslation()
+  const buttons = (
+    <ButtonGroup>
+      <Button color="primary" form={formId} type="submit">
+        {t('save')}
+      </Button>
+    </ButtonGroup>
+  )
   return (
-    <div className="float-right">
-      <ButtonGroup>
-        <Button color="primary" type="submit">
-          {t('save')}
-        </Button>
-      </ButtonGroup>
-    </div>
+    <>
+      {!inView && <FormButtonsInNavbar buttons={buttons} />}
+      <div className="float-right" ref={ref}>
+        {buttons}
+      </div>
+    </>
   )
 }
 
