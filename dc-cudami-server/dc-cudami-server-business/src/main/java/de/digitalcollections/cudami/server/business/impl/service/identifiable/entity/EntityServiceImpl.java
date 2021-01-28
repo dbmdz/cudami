fba5d428dcb04dcb9b37d60e92239db8b95bed89
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-01-28 09:56:09 +0100

    Add possibility to define external hooks

--- dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/impl/service/identifiable/entity/EntityServiceImpl.java
+++ dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/impl/service/identifiable/entity/EntityServiceImpl.java
@@ -1,14 +1,28 @@
 package de.digitalcollections.cudami.server.business.impl.service.identifiable.entity;
 
 import de.digitalcollections.cudami.server.backend.api.repository.identifiable.entity.EntityRepository;
+import de.digitalcollections.cudami.server.business.api.service.exceptions.IdentifiableServiceException;
 import de.digitalcollections.cudami.server.business.api.service.identifiable.entity.EntityService;
 import de.digitalcollections.cudami.server.business.impl.service.identifiable.IdentifiableServiceImpl;
+import de.digitalcollections.cudami.server.config.HookProperties;
 import de.digitalcollections.model.api.filter.Filtering;
 import de.digitalcollections.model.api.identifiable.entity.Entity;
+import de.digitalcollections.model.api.identifiable.entity.enums.EntityType;
 import de.digitalcollections.model.api.identifiable.resource.FileResource;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -16,6 +30,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class EntityServiceImpl<E extends Entity> extends IdentifiableServiceImpl<E>
     implements EntityService<E> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(EntityServiceImpl.class);
+
+  @Autowired protected HookProperties hookProperties;
+
+  protected HttpClient httpClient = HttpClient.newHttpClient();
 
   @Autowired
   public EntityServiceImpl(@Qualifier("entityRepositoryImpl") EntityRepository<E> repository) {
@@ -30,6 +50,20 @@ public class EntityServiceImpl<E extends Entity> extends IdentifiableServiceImpl
   @Override
   public void addRelatedFileresource(UUID entityUuid, UUID fileResourceUuid) {
     ((EntityRepository<E>) repository).addRelatedFileresource(entityUuid, fileResourceUuid);
+  }
+
+  /**
+   * Build a notification url by replacing placeholders in the template with the entity's uuid and
+   * type
+   */
+  protected URI buildNotificationUrl(String urlTemplate, UUID entityUuid, EntityType entityType) {
+    String url = String.format(urlTemplate, entityUuid, entityType);
+    try {
+      return new URL(url).toURI();
+    } catch (MalformedURLException | URISyntaxException e) {
+      LOGGER.warn("Something went wrong when creating the notification url: {}", e.getMessage());
+      return null;
+    }
   }
 
   protected Filtering filteringForActive() {
@@ -61,6 +95,23 @@ public class EntityServiceImpl<E extends Entity> extends IdentifiableServiceImpl
   }
 
   @Override
+  public E save(E entity) throws IdentifiableServiceException {
+    try {
+      E entityDb = super.save(entity);
+      Optional<String> hook =
+          hookProperties.getHookForActionAndType("save", entityDb.getEntityType());
+      if (hook.isPresent()) {
+        URI url = buildNotificationUrl(hook.get(), entityDb.getUuid(), entityDb.getEntityType());
+        sendNotification("POST", url);
+      }
+      return entityDb;
+    } catch (IdentifiableServiceException e) {
+      LOGGER.error("Cannot save entity " + entity + ": ", e);
+      throw e;
+    }
+  }
+
+  @Override
   public List<FileResource> saveRelatedFileResources(E entity, List<FileResource> fileResources) {
     return ((EntityRepository<E>) repository).saveRelatedFileResources(entity, fileResources);
   }
@@ -69,5 +120,54 @@ public class EntityServiceImpl<E extends Entity> extends IdentifiableServiceImpl
   public List<FileResource> saveRelatedFileResources(
       UUID entityUuid, List<FileResource> fileResources) {
     return ((EntityRepository<E>) repository).saveRelatedFileResources(entityUuid, fileResources);
+  }
+
+  /**
+   * Send a notification to an external url when an entity has changed
+   *
+   * @param httpVerb the http verb to use for the request
+   * @param url the url to send the request to
+   */
+  protected void sendNotification(String httpVerb, URI url) {
+    if (url == null) {
+      LOGGER.warn("No url given, ignoring.");
+      return;
+    }
+    HttpRequest request =
+        HttpRequest.newBuilder()
+            .uri(url)
+            .method(httpVerb, HttpRequest.BodyPublishers.noBody())
+            .build();
+    try {
+      HttpResponse response = httpClient.send(request, HttpResponse.BodyHandlers.discarding());
+      int statusCode = response.statusCode();
+      if (statusCode >= 400) {
+        LOGGER.warn(
+            "Sending a notification to {} with verb {} gave an error status code {}.",
+            url,
+            httpVerb,
+            statusCode);
+      }
+    } catch (InterruptedException | IOException e) {
+      LOGGER.warn(
+          "Something went wrong when sending a notification to {}: {}", url, e.getMessage());
+    }
+  }
+
+  @Override
+  public E update(E entity) throws IdentifiableServiceException {
+    try {
+      E entityDb = super.update(entity);
+      Optional<String> hook =
+          hookProperties.getHookForActionAndType("update", entityDb.getEntityType());
+      if (hook.isPresent()) {
+        URI url = buildNotificationUrl(hook.get(), entityDb.getUuid(), entityDb.getEntityType());
+        sendNotification("PUT", url);
+      }
+      return entityDb;
+    } catch (IdentifiableServiceException e) {
+      LOGGER.error("Cannot update identifiable " + entity + ": ", e);
+      throw e;
+    }
   }
 }
