be6c7b1333c4e8fd1b24287fd92d235e7d3f0318
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2021-02-02 10:39:07 +0100

    Add family and given names services and controller

---
+++ dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/impl/service/identifiable/agent/GivenNameServiceImpl.java
@@ -0,0 +1,32 @@
+package de.digitalcollections.cudami.server.business.impl.service.identifiable.agent;
+
+import de.digitalcollections.cudami.server.backend.api.repository.identifiable.agent.GivenNameRepository;
+import de.digitalcollections.cudami.server.business.api.service.identifiable.agent.GivenNameService;
+import de.digitalcollections.cudami.server.business.impl.service.identifiable.IdentifiableServiceImpl;
+import de.digitalcollections.model.api.identifiable.agent.GivenName;
+import de.digitalcollections.model.api.paging.PageRequest;
+import de.digitalcollections.model.api.paging.PageResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GivenNameServiceImpl extends IdentifiableServiceImpl<GivenName>
+    implements GivenNameService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GivenNameServiceImpl.class);
+
+  @Autowired
+  public GivenNameServiceImpl(GivenNameRepository repository) {
+    super(repository);
+  }
+
+  @Override
+  public PageResponse<GivenName> findByLanguageAndInitial(
+      PageRequest pageRequest, String language, String initial) {
+    PageResponse<GivenName> result =
+        ((GivenNameRepository) repository).findByLanguageAndInitial(pageRequest, language, initial);
+    return result;
+  }
+}
---
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/identifiable/agent/FamilyNameController.java
@@ -0,0 +1,140 @@
+package de.digitalcollections.cudami.server.controller.identifiable.agent;
+
+import de.digitalcollections.cudami.server.business.api.service.LocaleService;
+import de.digitalcollections.cudami.server.business.api.service.exceptions.IdentifiableServiceException;
+import de.digitalcollections.cudami.server.business.api.service.identifiable.agent.FamilyNameService;
+import de.digitalcollections.model.api.identifiable.agent.FamilyName;
+import de.digitalcollections.model.api.paging.PageRequest;
+import de.digitalcollections.model.api.paging.PageResponse;
+import de.digitalcollections.model.api.paging.Sorting;
+import de.digitalcollections.model.api.paging.enums.Direction;
+import de.digitalcollections.model.api.paging.enums.NullHandling;
+import de.digitalcollections.model.impl.paging.OrderImpl;
+import de.digitalcollections.model.impl.paging.PageRequestImpl;
+import de.digitalcollections.model.impl.paging.SortingImpl;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.UUID;
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.annotation.ApiPathParam;
+import org.jsondoc.core.annotation.ApiQueryParam;
+import org.jsondoc.core.annotation.ApiResponseObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(description = "The FamilyName controller", name = "FamilyName controller")
+public class FamilyNameController {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(FamilyNameController.class);
+
+  @Autowired FamilyNameService familyNameService;
+
+  @Autowired LocaleService localeService;
+
+  @ApiMethod(description = "get all family names")
+  @GetMapping(
+      value = {"/latest/familynames", "/v2/familynames"},
+      produces = "application/json")
+  @ApiResponseObject
+  public PageResponse<FamilyName> findAll(
+      Pageable pageable,
+      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
+      @RequestParam(name = "pageSize", required = false, defaultValue = "5") int pageSize,
+      @RequestParam(name = "sortField", required = false, defaultValue = "uuid") String sortField,
+      @RequestParam(name = "sortDirection", required = false, defaultValue = "ASC")
+          Direction sortDirection,
+      @RequestParam(name = "nullHandling", required = false, defaultValue = "NATIVE")
+          NullHandling nullHandling,
+      @RequestParam(name = "language", required = false, defaultValue = "de") String language,
+      @RequestParam(name = "initial", required = false) String initial) {
+    OrderImpl order = new OrderImpl(sortDirection, sortField, nullHandling);
+    Sorting sorting = new SortingImpl(order);
+    PageRequest pageRequest = new PageRequestImpl(pageNumber, pageSize, sorting);
+    if (initial == null) {
+      return familyNameService.find(pageRequest);
+    }
+    return familyNameService.findByLanguageAndInitial(pageRequest, language, initial);
+  }
+
+  @ApiMethod(
+      description =
+          "get a familyname as JSON or XML, depending on extension or <tt>format</tt> request parameter or accept header")
+  @GetMapping(
+      value = {"/latest/familynames/{uuid}", "/v2/familynames/{uuid}"},
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @ApiResponseObject
+  public ResponseEntity<FamilyName> get(
+      @ApiPathParam(
+              description =
+                  "UUID of the familyname, e.g. <tt>599a120c-2dd5-11e8-b467-0ed5f89f718b</tt>")
+          @PathVariable("uuid")
+          UUID uuid,
+      @ApiQueryParam(
+              name = "pLocale",
+              description =
+                  "Desired locale, e.g. <tt>de_DE</tt>. If unset, contents in all languages will be returned")
+          @RequestParam(name = "pLocale", required = false)
+          Locale pLocale)
+      throws IdentifiableServiceException {
+
+    FamilyName result;
+    if (pLocale == null) {
+      result = familyNameService.get(uuid);
+    } else {
+      result = familyNameService.get(uuid, pLocale);
+    }
+    return new ResponseEntity<>(result, HttpStatus.OK);
+  }
+
+  @ApiMethod(
+      description =
+          "get a familyname as JSON or XML, depending on extension or <tt>format</tt> request parameter or accept header")
+  @GetMapping(
+      value = {"/latest/familynames/identifier", "/v2/familynames/identifier"},
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @ApiResponseObject
+  public ResponseEntity<FamilyName> getByIdentifier(
+      @RequestParam(name = "namespace", required = true) String namespace,
+      @RequestParam(name = "id", required = true) String id)
+      throws IdentifiableServiceException {
+    FamilyName result = familyNameService.getByIdentifier(namespace, id);
+    return new ResponseEntity<>(result, HttpStatus.OK);
+  }
+
+  @ApiMethod(description = "save a newly created family")
+  @PostMapping(
+      value = {"/latest/familynames", "/v2/familynames"},
+      produces = "application/json")
+  @ApiResponseObject
+  public FamilyName save(@RequestBody FamilyName familyName, BindingResult errors)
+      throws IdentifiableServiceException {
+    return familyNameService.save(familyName);
+  }
+
+  @ApiMethod(description = "update a familyname")
+  @PutMapping(
+      value = {"/latest/familynames/{uuid}", "/v2/familynames/{uuid}"},
+      produces = "application/json")
+  @ApiResponseObject
+  public FamilyName update(
+      @PathVariable("uuid") UUID uuid, @RequestBody FamilyName familyName, BindingResult errors)
+      throws IdentifiableServiceException {
+    assert Objects.equals(uuid, familyName.getUuid());
+    return familyNameService.update(familyName);
+  }
+}
