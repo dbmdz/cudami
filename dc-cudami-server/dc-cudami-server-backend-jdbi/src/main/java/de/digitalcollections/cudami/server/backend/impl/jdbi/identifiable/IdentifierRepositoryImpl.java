0fd6e539052fc6233943dfaa3d3bf5661d6b5602
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2021-03-10 13:41:51 +0100

    Fix deletion for empty identifier lists

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/IdentifierRepositoryImpl.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/IdentifierRepositoryImpl.java
@@ -40,6 +40,9 @@ public class IdentifierRepositoryImpl extends JdbiRepositoryImpl implements Iden
 
   @Override
   public void delete(List<UUID> uuids) {
+    if (uuids==null || uuids.isEmpty()) {
+      return;
+    }
     dbi.withHandle(
         h ->
             h.createUpdate("DELETE FROM " + tableName + " WHERE uuid in (<uuids>)")
