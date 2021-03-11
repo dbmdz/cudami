ad27f25c5b50490dc95417d48cc6fd8eea4ee6f2
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2021-03-11 09:45:44 +0100

    Remove sorting for label at paging

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/agent/CorporateBodiesController.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/agent/CorporateBodiesController.java
@@ -7,12 +7,8 @@ import de.digitalcollections.cudami.client.CudamiLocalesClient;
 import de.digitalcollections.cudami.client.exceptions.HttpException;
 import de.digitalcollections.cudami.client.identifiable.entity.agent.CudamiCorporateBodiesClient;
 import de.digitalcollections.model.identifiable.entity.agent.CorporateBody;
-import de.digitalcollections.model.paging.Order;
 import de.digitalcollections.model.paging.PageRequest;
 import de.digitalcollections.model.paging.PageResponse;
-import de.digitalcollections.model.paging.Sorting;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
@@ -91,12 +87,7 @@ public class CorporateBodiesController extends AbstractController {
       @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
       @RequestParam(name = "pageSize", required = false, defaultValue = "25") int pageSize)
       throws HttpException {
-    List<Order> orders = new ArrayList<>();
-    Order labelOrder = new Order("label");
-    labelOrder.setSubProperty(localeService.getDefaultLanguage().getLanguage());
-    orders.addAll(Arrays.asList(labelOrder));
-    Sorting sorting = new Sorting(orders);
-    PageRequest pageRequest = new PageRequest(pageNumber, pageSize, sorting);
+    PageRequest pageRequest = new PageRequest(pageNumber, pageSize);
     return service.find(pageRequest);
   }
 
