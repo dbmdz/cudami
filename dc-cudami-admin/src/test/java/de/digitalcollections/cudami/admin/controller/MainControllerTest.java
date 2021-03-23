1856e09bb9b68159cd04e2e0b863f55099a2afb3
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2021-03-23 15:00:20 +0100

    Add first controller test with service mocking

---
+++ dc-cudami-admin/src/test/java/de/digitalcollections/cudami/admin/controller/MainControllerTest.java
@@ -0,0 +1,42 @@
+package de.digitalcollections.cudami.admin.controller;
+
+import de.digitalcollections.cudami.admin.business.api.service.security.UserService;
+import de.digitalcollections.cudami.admin.propertyeditor.LocalizedStructuredContentEditor;
+import de.digitalcollections.cudami.admin.propertyeditor.LocalizedTextEditor;
+import de.digitalcollections.cudami.admin.propertyeditor.RoleEditor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MainController.class)
+public class MainControllerTest {
+
+  @MockBean
+  private LocalizedStructuredContentEditor localizedStructuredContentEditor;
+
+  @MockBean
+  private LocalizedTextEditor localizedTextEditor;
+  
+  @Autowired
+  private MockMvc mockMvc;
+  
+  @MockBean
+  private RoleEditor roleEditor;
+
+  @MockBean
+  private UserService userService;
+
+  @Test
+  public void testNoAdminUserExists() throws Exception {
+    this.mockMvc.perform(get("/"))
+            .andDo(print())
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("http://localhost/login"));
+  }
+}
