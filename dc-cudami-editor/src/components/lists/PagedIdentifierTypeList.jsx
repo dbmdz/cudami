9884c1522dcf455da54d1780cc9c9df3f87c44eb
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-03-26 11:12:27 +0100

    Add usage of type variable

--- dc-cudami-editor/src/components/lists/PagedIdentifierTypeList.jsx
+++ dc-cudami-editor/src/components/lists/PagedIdentifierTypeList.jsx
@@ -21,7 +21,7 @@ const PagedIdentifierTypeList = ({apiContextPath = '/'}) => {
     <>
       <Row>
         <Col>
-          <h1>{t('types:identifierType_plural')}</h1>
+          <h1>{t(`types:${type}_plural`)}</h1>
         </Col>
         <Col className="text-right">
           <Button href={`${apiContextPath}${typeToEndpointMapping[type]}/new`}>
