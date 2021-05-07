4f7e667517233dca90ae69a8c32c34facfdddb0b
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-05-07 08:52:19 +0200

    Add react editor for identifier types

---
+++ dc-cudami-editor/src/lib/IdentifierTypeEditor.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import IdentifierTypeForm from '../components/forms/IdentifierTypeForm'
+import initI18n from '../i18n'
+
+export default function ({apiContextPath, id, uiLocale, uuid}) {
+  initI18n(uiLocale)
+  ReactDOM.render(
+    <IdentifierTypeForm apiContextPath={apiContextPath} uuid={uuid} />,
+    document.getElementById(id)
+  )
+}
