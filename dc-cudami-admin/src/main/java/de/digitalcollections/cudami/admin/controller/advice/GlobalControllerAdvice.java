9737ea8a55476704ed3f6076502d7dac5918896c
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-05-07 12:04:02 +0200

    Remove useless property editors

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/advice/GlobalControllerAdvice.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/advice/GlobalControllerAdvice.java
@@ -1,17 +1,35 @@
 package de.digitalcollections.cudami.admin.controller.advice;
 
+import de.digitalcollections.cudami.admin.propertyeditor.RoleEditor;
+import de.digitalcollections.model.security.Role;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.context.request.WebRequest;
 
-/** Adds the webjar versions read from application.yml as global model attribute. */
+/**
+ * Adds the webapp version read from application.yml as global model attribute and initializes
+ * custom property editors.
+ */
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
+  private final RoleEditor roleEditor;
   private final String version;
 
-  public GlobalControllerAdvice(@Value("${info.app.project.version}") String version) {
+  public GlobalControllerAdvice(
+      RoleEditor roleEditor, @Value("${info.app.project.version}") String version) {
+    this.roleEditor = roleEditor;
     this.version = version;
+  }
+
+  @InitBinder
+  public void registerCustomEditors(WebDataBinder binder, WebRequest request) {
+    binder.registerCustomEditor(Role.class, roleEditor);
+    binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
   }
 
   @ModelAttribute("version")
