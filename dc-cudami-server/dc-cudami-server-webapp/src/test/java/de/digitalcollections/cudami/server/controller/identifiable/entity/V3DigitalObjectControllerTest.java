6bffd2fc113ea5e4b20e4d8284531384bf872d2d
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-06-15 11:36:48 +0200

    Add controller test for moved endpoint

--- dc-cudami-server/dc-cudami-server-webapp/src/test/java/de/digitalcollections/cudami/server/controller/identifiable/entity/V3DigitalObjectControllerTest.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/test/java/de/digitalcollections/cudami/server/controller/identifiable/entity/V3DigitalObjectControllerTest.java
@@ -9,9 +9,11 @@ import de.digitalcollections.cudami.server.controller.BaseControllerTest;
 import de.digitalcollections.cudami.server.model.CollectionBuilder;
 import de.digitalcollections.cudami.server.model.DigitalObjectBuilder;
 import de.digitalcollections.cudami.server.model.PageResponseBuilder;
+import de.digitalcollections.cudami.server.model.ProjectBuilder;
 import de.digitalcollections.model.file.MimeType;
 import de.digitalcollections.model.identifiable.entity.Collection;
 import de.digitalcollections.model.identifiable.entity.DigitalObject;
+import de.digitalcollections.model.identifiable.entity.Project;
 import de.digitalcollections.model.paging.PageRequest;
 import de.digitalcollections.model.paging.PageResponse;
 import java.util.Locale;
@@ -87,6 +89,43 @@ public class V3DigitalObjectControllerTest extends BaseControllerTest {
 
     when(digitalObjectService.getActiveCollections(
             any(DigitalObject.class), any(PageRequest.class)))
+        .thenReturn(expected);
+
+    testJson(path);
+  }
+
+  @DisplayName("can return the projects, a digital object belongs to")
+  @ParameterizedTest
+  @ValueSource(
+      strings = {
+        "/v3/digitalobjects/6bfbe6dc-2c14-4e61-b88b-ce56cea712c7/projects?pageNumber=0&pageSize=1"
+      })
+  public void projectsForDigitalObject(String path) throws Exception {
+    PageResponse<Project> expected =
+        (PageResponse)
+            new PageResponseBuilder<>()
+                .forRequestPage(0)
+                .forPageSize(1)
+                .withTotalElements(1)
+                .withContent(
+                    new ProjectBuilder()
+                        .createdAt("2020-07-16T11:51:33.981829")
+                        .withLabel(Locale.GERMAN, "Lateinische Handschriften")
+                        .withLabel(Locale.ENGLISH, "Latin Manuscripts")
+                        .lastModifiedAt("2020-11-04T15:46:42.81741")
+                        .withPreviewImage(
+                            "Lateinische_Handschriften_bsb00131281_27.jpg",
+                            "2780bee1-eeec-4b50-a95b-bba90793fc6a",
+                            "file:///cudami/image/jpg/2780/bee1/eeec/4b50/a95b/bba9/0793/fc6a/resource.jpg",
+                            MimeType.MIME_IMAGE_JPEG,
+                            "https://api.digitale-sammlungen.de/iiif/image/v2/2780bee1-eeec-4b50-a95b-bba90793fc6a")
+                        .withOpenPreviewImageInNewWindow()
+                        .withUuid("25198d8b-38d4-49f7-9ef0-d99b3e607e30")
+                        .withRefId(148)
+                        .build())
+                .build();
+
+    when(digitalObjectService.getProjects(any(DigitalObject.class), any(PageRequest.class)))
         .thenReturn(expected);
 
     testJson(path);
