24ad83928ef313af26459788c5de5c9db9fbabfb
David Förster <david.foerster@bsb-muenchen.de>
2021-08-03 15:47:28 +0200

    Add sql script to create table url_aliases, [mdz/webapps/internal/cudami/-/issues/140]

---
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V9.02.00__DDL_Create_table_url_aliases.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS url_aliases (
+  uuid UUID PRIMARY KEY,
+  website_uuid UUID NOT NULL,
+  slug VARCHAR(256) NOT NULL,
+  is_main_alias BOOLEAN NOT NULL,
+  last_published TIMESTAMP DEFAULT NULL,
+  target_type VARCHAR(20) NOT NULL,
+  target_uuid UUID NOT NULL,
+  target_language VARCHAR(2) DEFAULT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  
+  CONSTRAINT fk_domain FOREIGN KEY (website_uuid) REFERENCES websites(uuid),
+  CONSTRAINT fk_identifiable FOREIGN KEY (target_uuid) REFERENCES identifiables(uuid),
+  UNIQUE (website_uuid, slug)
+);
