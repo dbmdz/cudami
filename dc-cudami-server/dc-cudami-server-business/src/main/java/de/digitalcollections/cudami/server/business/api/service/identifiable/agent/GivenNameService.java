6902ec3af5eef81e76769d786eb543e79f543da1
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2021-09-13 16:22:52 +0200

    Remove not needed overridden methods

--- dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/api/service/identifiable/agent/GivenNameService.java
+++ dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/api/service/identifiable/agent/GivenNameService.java
@@ -2,11 +2,5 @@ package de.digitalcollections.cudami.server.business.api.service.identifiable.ag
 
 import de.digitalcollections.cudami.server.business.api.service.identifiable.IdentifiableService;
 import de.digitalcollections.model.identifiable.agent.GivenName;
-import de.digitalcollections.model.paging.PageRequest;
-import de.digitalcollections.model.paging.PageResponse;
 
-public interface GivenNameService extends IdentifiableService<GivenName> {
-
-  public PageResponse<GivenName> findByLanguageAndInitial(
-      PageRequest pageRequest, String language, String initial);
-}
+public interface GivenNameService extends IdentifiableService<GivenName> {}
