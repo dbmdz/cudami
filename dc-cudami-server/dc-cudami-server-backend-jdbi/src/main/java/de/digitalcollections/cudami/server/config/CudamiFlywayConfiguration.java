2db1876c0390e3a6ae69f7e8325e2531a66d1344
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2021-10-06 15:50:36 +0200

    Test fix

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/config/CudamiFlywayConfiguration.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/config/CudamiFlywayConfiguration.java
@@ -9,8 +9,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!TEST")
 public class CudamiFlywayConfiguration extends FlywayConfiguration {
 
   @Primary
