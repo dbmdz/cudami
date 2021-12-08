832285af4d91c9d81aeb6a01f20831146d59372b
David Förster <david.foerster@bsb-muenchen.de>
2021-12-08 11:00:08 +0100

    Add unique index to table url_aliases, [mdz/webapps/internal/cudami/-/issues/238]

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V9.02.00__DDL_Create_table_url_aliases.sql
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V9.02.00__DDL_Create_table_url_aliases.sql
@@ -13,3 +13,11 @@ CREATE TABLE IF NOT EXISTS url_aliases (
   CONSTRAINT fk_domain FOREIGN KEY (website_uuid) REFERENCES websites(uuid),
   UNIQUE (website_uuid, slug, target_language)
 );
+
+/* Sadly in an unique constraint null values are not considered equal. Nulls are always different.
+ * Therefore it is possible to have multiple identical slug target_language tuples that have null in the website_uuid coloumn.
+ * To overcome this drawback an unique index is necessary that only takes account of rows where the website_uuid is null.
+*/
+CREATE UNIQUE INDEX IF NOT EXISTS url_aliases_unique_website_uuid_is_null
+  ON url_aliases (slug, target_language)
+  WHERE website_uuid IS NULL;
