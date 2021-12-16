991888f25d940d68a2bf922ccd30f77a25a6811c
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-12-16 15:55:14 +0100

    Fix Internal Server Error

--- dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/advice/ExceptionAdvice.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/advice/ExceptionAdvice.java
@@ -4,8 +4,10 @@ import de.digitalcollections.cudami.server.business.api.service.exceptions.Valid
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,22 +17,27 @@ public class ExceptionAdvice {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionAdvice.class);
 
-  @ResponseStatus(HttpStatus.NOT_FOUND)
   @ExceptionHandler(UsernameNotFoundException.class)
-  public void handleNotFound() {}
-
   @ResponseStatus(HttpStatus.NOT_FOUND)
+  public void handleUsernameNotFoundException() {}
+
   @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
   public void handleHttpMediaTypeNotAcceptableException() {}
 
-  @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
   @ExceptionHandler(ValidationException.class)
+  @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
   public void handleValidationException(Exception exception) {
     LOGGER.error("Cannot process request: ", exception);
   }
 
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  public ResponseEntity<String> handleMissingServletRequestParameterException(Exception exception) {
+    return ResponseEntity.badRequest().body(exception.getMessage());
+  }
+
   @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public void handleAllOther(Exception exception) {
     LOGGER.error("exception stack trace", exception);
   }
