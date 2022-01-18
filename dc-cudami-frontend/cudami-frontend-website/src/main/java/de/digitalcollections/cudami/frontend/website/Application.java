c57735b84815385e1d7c7423c691078c1c6298ea
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-01-18 17:07:03 +0100

    Restructure modules replacing "templates" with "frontend" Added theming documentation

---
+++ dc-cudami-frontend/cudami-frontend-website/src/main/java/de/digitalcollections/cudami/frontend/website/Application.java
@@ -0,0 +1,14 @@
+package de.digitalcollections.cudami.frontend.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
---
+++ dc-cudami-frontend/cudami-frontend-website/src/main/java/de/digitalcollections/cudami/frontend/website/config/TemplateConfig.java
@@ -0,0 +1,25 @@
+package de.digitalcollections.cudami.frontend.website.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConfigurationProperties(prefix = "template")
+@ConstructorBinding
+public class TemplateConfig {
+
+  private final String name;
+  private final int navMaxLevel;
+
+  public TemplateConfig(String name, int navMaxLevel) {
+    this.name = name;
+    this.navMaxLevel = navMaxLevel;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getNavMaxLevel() {
+    return navMaxLevel;
+  }
+}
