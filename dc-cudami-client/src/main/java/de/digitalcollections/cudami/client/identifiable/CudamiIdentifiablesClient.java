0056091d5a05dea6b0ab70e98e7a54a4935a9538
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-01-18 16:38:18 +0100

    Refactored CudamiIdentifiablesClient to use Generics; refactored and tested CudamiCorporateBodiesClient

--- dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/identifiable/CudamiIdentifiablesClient.java
+++ dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/identifiable/CudamiIdentifiablesClient.java
@@ -8,62 +8,92 @@ import de.digitalcollections.model.paging.PageRequest;
 import de.digitalcollections.model.paging.PageResponse;
 import de.digitalcollections.model.paging.SearchPageRequest;
 import de.digitalcollections.model.paging.SearchPageResponse;
+import java.lang.reflect.InvocationTargetException;
 import java.net.http.HttpClient;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
-public class CudamiIdentifiablesClient extends CudamiBaseClient<Identifiable> {
+public class CudamiIdentifiablesClient<I extends Identifiable> extends CudamiBaseClient<I> {
+
+  protected final String baseEndpoint;
+  private Class<I> typeArgumentClass;
+
+  public CudamiIdentifiablesClient(
+      HttpClient http,
+      String serverUrl,
+      Class<I> identifiableClass,
+      ObjectMapper mapper,
+      String baseEndpoint) {
+    super(http, serverUrl, identifiableClass, mapper);
+    this.baseEndpoint = baseEndpoint;
+    this.typeArgumentClass = identifiableClass;
+  }
 
   public CudamiIdentifiablesClient(HttpClient http, String serverUrl, ObjectMapper mapper) {
-    super(http, serverUrl, Identifiable.class, mapper);
+    this(http, serverUrl, (Class<I>) Identifiable.class, mapper, "/v5/identifiables");
   }
 
   public long count() throws HttpException {
-    return Long.parseLong(doGetRequestForString("/v5/identifiables/count"));
+    return Long.parseLong(doGetRequestForString(baseEndpoint + "/count"));
   }
 
-  public Identifiable create() {
-    return new Identifiable();
+  public I create() {
+    try {
+      return typeArgumentClass.getDeclaredConstructor().newInstance();
+    } catch (InstantiationException
+        | IllegalAccessException
+        | InvocationTargetException
+        | NoSuchMethodException e) {
+      throw new RuntimeException(
+          "Cannot create new instance of " + typeArgumentClass.getName() + ": " + e, e);
+    }
   }
 
-  public PageResponse<Identifiable> find(PageRequest pageRequest) throws HttpException {
-    return doGetRequestForPagedObjectList("/v5/identifiables", pageRequest);
+  public PageResponse<I> find(PageRequest pageRequest) throws HttpException {
+    return doGetRequestForPagedObjectList(baseEndpoint, pageRequest);
   }
 
-  public SearchPageResponse<Identifiable> find(SearchPageRequest searchPageRequest)
-      throws HttpException {
-    return doGetSearchRequestForPagedObjectList("/v5/identifiables/search", searchPageRequest);
+  public SearchPageResponse<I> find(SearchPageRequest searchPageRequest) throws HttpException {
+    return doGetSearchRequestForPagedObjectList(baseEndpoint + "/search", searchPageRequest);
   }
 
-  public List<Identifiable> find(String searchTerm, int maxResults) throws HttpException {
+  public List<I> find(String searchTerm, int maxResults) throws HttpException {
     SearchPageRequest searchPageRequest = new SearchPageRequest(searchTerm, 0, maxResults, null);
-    SearchPageResponse<Identifiable> response = find(searchPageRequest);
+    SearchPageResponse<I> response = find(searchPageRequest);
     return response.getContent();
   }
 
-  public Identifiable findOne(UUID uuid) throws HttpException {
-    return doGetRequestForObject(String.format("/v5/identifiables/%s", uuid));
+  public I findOne(UUID uuid) throws HttpException {
+    return doGetRequestForObject(String.format(baseEndpoint + "/%s", uuid));
   }
 
-  public Identifiable findOne(UUID uuid, Locale locale) throws HttpException {
+  public I findOne(UUID uuid, Locale locale) throws HttpException {
     return findOne(uuid, locale.toString());
   }
 
-  public Identifiable findOne(UUID uuid, String locale) throws HttpException {
-    return doGetRequestForObject(String.format("/v5/identifiables/%s?locale=%s", uuid, locale));
+  public I findOne(UUID uuid, String locale) throws HttpException {
+    return doGetRequestForObject(String.format(baseEndpoint + "/%s?locale=%s", uuid, locale));
   }
 
-  public Identifiable findOneByIdentifier(String namespace, String id) throws HttpException {
-    return doGetRequestForObject(
-        String.format("/v5/identifiables/identifier/%s:%s.json", namespace, id));
+  public I findOneByIdentifier(String namespace, String id) throws HttpException {
+    return doGetRequestForObject(String.format(baseEndpoint + "/%s:%s.json", namespace, id));
   }
 
-  public Identifiable save(Identifiable identifiable) throws HttpException {
-    return doPostRequestForObject("/v5/identifiables", identifiable);
+  public I save(I identifiable) throws HttpException {
+    return doPostRequestForObject(baseEndpoint, identifiable);
   }
 
-  public Identifiable update(UUID uuid, Identifiable identifiable) throws HttpException {
-    return doPutRequestForObject(String.format("/v5/identifiables/%s", uuid), identifiable);
+  public I update(UUID uuid, I identifiable) throws HttpException {
+    return doPutRequestForObject(String.format(baseEndpoint + "/%s", uuid), identifiable);
+  }
+
+  /**
+   * Get the base endpoint path for testing purposes
+   *
+   * @return the relative base endpoint of the client
+   */
+  public String getBaseEndpoint() {
+    return baseEndpoint;
   }
 }
