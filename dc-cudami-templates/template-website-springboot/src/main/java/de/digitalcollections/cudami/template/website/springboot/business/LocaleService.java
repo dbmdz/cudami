09de38ae9372696d3ae820acc7e511c86130046d
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-01-18 17:07:03 +0100

    Restructure modules replacing "templates" with "frontend" Added theming documentation

--- dc-cudami-templates/template-website-springboot/src/main/java/de/digitalcollections/cudami/template/website/springboot/business/LocaleService.java
+++
@@ -1,8 +0,0 @@
-package de.digitalcollections.cudami.template.website.springboot.business;
-
-import java.util.Locale;
-
-public interface LocaleService {
-
-  public Locale getDefaultLanguage();
-}
