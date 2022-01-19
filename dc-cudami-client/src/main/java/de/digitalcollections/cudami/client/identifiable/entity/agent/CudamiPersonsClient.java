d4e072725d875b631c24a9686fbedff326449a09
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-01-19 13:32:41 +0100

    Refactored CudamiPersonsClient

--- dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/identifiable/entity/agent/CudamiPersonsClient.java
+++ dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/identifiable/entity/agent/CudamiPersonsClient.java
@@ -1,47 +1,33 @@
 package de.digitalcollections.cudami.client.identifiable.entity.agent;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.digitalcollections.cudami.client.CudamiBaseClient;
 import de.digitalcollections.cudami.client.exceptions.HttpException;
+import de.digitalcollections.cudami.client.identifiable.CudamiIdentifiablesClient;
 import de.digitalcollections.model.identifiable.entity.DigitalObject;
 import de.digitalcollections.model.identifiable.entity.agent.Person;
 import de.digitalcollections.model.identifiable.entity.work.Work;
 import de.digitalcollections.model.paging.PageRequest;
 import de.digitalcollections.model.paging.PageResponse;
-import de.digitalcollections.model.paging.SearchPageRequest;
-import de.digitalcollections.model.paging.SearchPageResponse;
 import java.net.http.HttpClient;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
-public class CudamiPersonsClient extends CudamiBaseClient<Person> {
+public class CudamiPersonsClient extends CudamiIdentifiablesClient<Person> {
 
   public CudamiPersonsClient(HttpClient http, String serverUrl, ObjectMapper mapper) {
-    super(http, serverUrl, Person.class, mapper);
-  }
-
-  public long count() throws HttpException {
-    return Long.parseLong(doGetRequestForString("/v5/persons/count"));
-  }
-
-  public Person create() {
-    return new Person();
+    super(http, serverUrl, Person.class, mapper, "/v5/persons");
   }
 
   @Deprecated(since = "5.0", forRemoval = true)
   /** @deprecated Please use {@link #find(SearchPageRequest)} instead */
   public PageResponse<Person> find(PageRequest pageRequest) throws HttpException {
-    return doGetRequestForPagedObjectList("/v5/persons", pageRequest);
-  }
-
-  public SearchPageResponse<Person> find(SearchPageRequest pageRequest) throws HttpException {
-    return doGetSearchRequestForPagedObjectList("/v5/persons", pageRequest);
+    return doGetRequestForPagedObjectList(baseEndpoint, pageRequest);
   }
 
   public PageResponse findByLanguageAndInitial(
       PageRequest pageRequest, String language, String initial) throws HttpException {
-    return findByLanguageAndInitial("/v5/persons", pageRequest, language, initial);
+    return findByLanguageAndInitial(baseEndpoint, pageRequest, language, initial);
   }
 
   public PageResponse<Person> findByLanguageAndInitial(
@@ -54,7 +40,7 @@ public class CudamiPersonsClient extends CudamiBaseClient<Person> {
       String initial)
       throws HttpException {
     return findByLanguageAndInitial(
-        "/v5/persons",
+        baseEndpoint,
         pageNumber,
         pageSize,
         sortField,
@@ -67,42 +53,26 @@ public class CudamiPersonsClient extends CudamiBaseClient<Person> {
   public PageResponse<Person> findByPlaceOfBirth(PageRequest pageRequest, UUID uuidGeoLocation)
       throws HttpException {
     return doGetRequestForPagedObjectList(
-        "/v5/persons/placeofbirth/" + uuidGeoLocation.toString(), pageRequest);
+        baseEndpoint + "/placeofbirth/" + uuidGeoLocation.toString(), pageRequest);
   }
 
   public PageResponse<Person> findByPlaceOfDeath(PageRequest pageRequest, UUID uuidGeoLocation)
       throws HttpException {
     return doGetRequestForPagedObjectList(
-        "/v5/persons/placeofdeath/" + uuidGeoLocation.toString(), pageRequest);
-  }
-
-  public Person findOne(UUID uuid) throws HttpException {
-    return doGetRequestForObject(String.format("/v5/persons/%s", uuid));
-  }
-
-  public Person findOneByIdentifier(String namespace, String id) throws HttpException {
-    return doGetRequestForObject(
-        String.format("/v5/persons/identifier?namespace=%s&id=%s", namespace, id));
+        baseEndpoint + "/placeofdeath/" + uuidGeoLocation.toString(), pageRequest);
   }
 
   public List getDigitalObjects(UUID uuidPerson) throws HttpException {
     return doGetRequestForObjectList(
-        String.format("/v5/persons/%s/digitalobjects", uuidPerson), DigitalObject.class);
+        String.format(baseEndpoint + "/%s/digitalobjects", uuidPerson), DigitalObject.class);
   }
 
   public List<Locale> getLanguages() throws HttpException {
-    return doGetRequestForObjectList("/v5/persons/languages", Locale.class);
+    return doGetRequestForObjectList(baseEndpoint + "/languages", Locale.class);
   }
 
   public List getWorks(UUID uuidPerson) throws HttpException {
-    return doGetRequestForObjectList(String.format("/v5/persons/%s/works", uuidPerson), Work.class);
-  }
-
-  public Person save(Person person) throws HttpException {
-    return doPostRequestForObject("/v5/persons", person);
-  }
-
-  public Person update(UUID uuid, Person person) throws HttpException {
-    return doPutRequestForObject(String.format("/v5/persons/%s", uuid), person);
+    return doGetRequestForObjectList(
+        String.format(baseEndpoint + "/%s/works", uuidPerson), Work.class);
   }
 }
---
+++ dc-cudami-client/src/test/java/de/digitalcollections/cudami/client/identifiable/entity/agent/CudamiPersonsClientTest.java
@@ -0,0 +1,86 @@
+package de.digitalcollections.cudami.client.identifiable.entity.agent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import de.digitalcollections.model.identifiable.entity.agent.Person;
+import de.digitalcollections.model.identifiable.entity.work.Work;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("The client for persons")
+class CudamiPersonsClientTest
+    extends BaseCudamiIdentifiablesClientTest<Person, CudamiPersonsClient> {
+
+  @Test
+  @DisplayName("can find by pageRequest, language and initial string")
+  public void findByPageRequestLanguageInitial() throws Exception {
+    client.findByLanguageAndInitial(buildExamplePageRequest(), "de", "a");
+    verifyHttpRequestByMethodAndRelativeURL(
+        "get",
+        "?language=de&initial=a&pageNumber=1&pageSize=2&sortBy=sortable.desc.nullsfirst&foo=eq:bar&gnarf=eq:krchch");
+  }
+
+  @Test
+  @DisplayName("can find by language, initial string and dedicated paging attributes")
+  public void findByLanguageInitialAndPagingAttributes() throws Exception {
+    client.findByLanguageAndInitial(1, 2, "sortable", "asc", "NATIVE", "de", "a");
+    verifyHttpRequestByMethodAndRelativeURL(
+        "get", "?language=de&initial=a&pageNumber=1&pageSize=2&sortBy=sortable.asc");
+  }
+
+  @Test
+  @DisplayName("can find by the place of birth")
+  public void findByPlaceOfBirth() throws Exception {
+    UUID placeUuid = UUID.randomUUID();
+    client.findByPlaceOfBirth(buildExamplePageRequest(), placeUuid);
+    verifyHttpRequestByMethodAndRelativeURL(
+        "get",
+        "/placeofbirth/"
+            + placeUuid
+            + "?pageNumber=1&pageSize=2&sortBy=sortable.desc.nullsfirst&foo=eq:bar&gnarf=eq:krchch");
+  }
+
+  @Test
+  @DisplayName("can find by the place of death")
+  public void findByPlaceOfDeath() throws Exception {
+    UUID placeUuid = UUID.randomUUID();
+    client.findByPlaceOfDeath(buildExamplePageRequest(), placeUuid);
+    verifyHttpRequestByMethodAndRelativeURL(
+        "get",
+        "/placeofdeath/"
+            + placeUuid
+            + "?pageNumber=1&pageSize=2&sortBy=sortable.desc.nullsfirst&foo=eq:bar&gnarf=eq:krchch");
+  }
+
+  @Test
+  @DisplayName("can find DigitalObjects for a person")
+  public void findDigitalObjectsForPersons() throws Exception {
+    UUID personUuid = UUID.randomUUID();
+    client.getDigitalObjects(personUuid);
+    verifyHttpRequestByMethodAndRelativeURL("get", "/" + personUuid + "/digitalobjects");
+  }
+
+  @Test
+  @DisplayName("can return the languages for all persons")
+  public void getLanguages() throws Exception {
+    client.getLanguages();
+    verifyHttpRequestByMethodAndRelativeURL("get", "/languages");
+  }
+
+  @Test
+  @DisplayName("can return all works of a person")
+  public void getWorks() throws Exception {
+    String bodyJson = "[{\"entityType\":\"WORK\",\"identifiableType\":\"ENTITY\"}]";
+    when(httpResponse.body()).thenReturn(bodyJson.getBytes(StandardCharsets.UTF_8));
+
+    UUID personUuid = UUID.randomUUID();
+    List<Work> works = client.getWorks(personUuid);
+    assertThat(works).isNotNull();
+    assertThat(works.get(0)).isExactlyInstanceOf(Work.class);
+    verifyHttpRequestByMethodAndRelativeURL("get", "/" + personUuid + "/works");
+  }
+}
