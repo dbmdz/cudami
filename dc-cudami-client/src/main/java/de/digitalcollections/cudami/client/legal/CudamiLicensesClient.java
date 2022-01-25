81a0a246ab98d8d5ea742be1c7b5a0ceed809869
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-01-25 14:50:40 +0100

    Add licenses client Add forConfig method Add type to identifiables client

--- dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/legal/CudamiLicensesClient.java
+++ dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/legal/CudamiLicensesClient.java
@@ -1,55 +1,45 @@
 package de.digitalcollections.cudami.client.legal;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.digitalcollections.cudami.client.CudamiRestClient;
-import de.digitalcollections.model.exception.TechnicalException;
+import de.digitalcollections.cudami.client.CudamiBaseClient;
+import de.digitalcollections.cudami.client.exceptions.HttpException;
 import de.digitalcollections.model.legal.License;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLEncoder;
+import de.digitalcollections.model.paging.PageRequest;
+import de.digitalcollections.model.paging.PageResponse;
+import de.digitalcollections.model.paging.SearchPageRequest;
+import de.digitalcollections.model.paging.SearchPageResponse;
 import java.net.http.HttpClient;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.UUID;
 
-public class CudamiLicensesClient extends CudamiRestClient<License> {
+public class CudamiLicensesClient extends CudamiBaseClient<License> {
+
+  private static final String BASE_ENDPOINT_URL = "/v5/licenses";
 
   public CudamiLicensesClient(HttpClient http, String serverUrl, ObjectMapper mapper) {
-    super(http, serverUrl, License.class, mapper, "/v5/licenses");
+    super(http, serverUrl, License.class, mapper);
   }
 
-  /**
-   * @param url (not validated) url string of license to be deleted
-   * @throws java.net.MalformedURLException thrown if given url is malformed
-   * @throws TechnicalException in case of connection problems
-   */
-  public void deleteByUrl(String url) throws TechnicalException, MalformedURLException {
-    deleteByUrl(URI.create(url).toURL());
+  public License create() {
+    return new License();
   }
 
-  /**
-   * @param url valid url of license to be deleted
-   * @throws TechnicalException in case of connection problems
-   */
-  public void deleteByUrl(URL url) throws TechnicalException {
-    doDeleteRequestForString(
-        String.format(
-            "%s?url=%s", baseEndpoint, URLEncoder.encode(url.toString(), StandardCharsets.UTF_8)));
+  public PageResponse<License> find(PageRequest pageRequest) throws HttpException {
+    return doGetRequestForPagedObjectList(BASE_ENDPOINT_URL, pageRequest);
   }
 
-  @Deprecated
-  public void deleteByUuids(List<UUID> uuids) {
-    throw new UnsupportedOperationException(
-        "Spec https://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html not explicitely allows a body in DELETE methods. Java HTTP RequestBuilder does not allow.");
+  public SearchPageResponse<License> find(SearchPageRequest pageRequest) throws HttpException {
+    return this.doGetSearchRequestForPagedObjectList(BASE_ENDPOINT_URL, pageRequest);
   }
 
-  public License getByUrl(String url) throws TechnicalException {
-    return doGetRequestForObject(
-        String.format("%s?url=%s", baseEndpoint, URLEncoder.encode(url, StandardCharsets.UTF_8)));
+  public License findOne(UUID uuid) throws HttpException {
+    return doGetRequestForObject(String.format(BASE_ENDPOINT_URL + "/%s", uuid));
   }
 
-  public License getByUrl(URL url) throws TechnicalException {
-    return getByUrl(url.toString());
+  public License save(License license) throws HttpException {
+    return doPostRequestForObject(BASE_ENDPOINT_URL, license);
+  }
+
+  public License update(UUID uuid, License license) throws HttpException {
+    return doPutRequestForObject(String.format(BASE_ENDPOINT_URL + "/%s", uuid), license);
   }
 }
