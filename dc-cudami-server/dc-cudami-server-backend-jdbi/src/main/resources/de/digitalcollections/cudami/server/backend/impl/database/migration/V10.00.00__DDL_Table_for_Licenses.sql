706dde902c9dcd1101600f08561b081e6c90f866
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-01-27 15:39:56 +0100

    Add database migration and integration test for license repository

---
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V10.00.00__DDL_Table_for_Licenses.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS licenses (
+  uuid UUID NOT NULL PRIMARY KEY UNIQUE,
+  acronym VARCHAR,
+  label JSONB,
+  url VARCHAR NOT NULL UNIQUE,
+
+  created TIMESTAMP NOT NULL,
+  last_modified TIMESTAMP NOT NULL
+);
