26169541448e71820a45717704ddb0972257ea0d
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-01-31 15:56:01 +0100

    Add license service Add license controller with first test

---
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/legal/LicenseController.java
@@ -0,0 +1,165 @@
+package de.digitalcollections.cudami.server.controller.legal;
+
+import de.digitalcollections.cudami.server.business.api.service.exceptions.IdentifiableServiceException;
+import de.digitalcollections.cudami.server.business.api.service.exceptions.ServiceException;
+import de.digitalcollections.cudami.server.business.api.service.legal.LicenseService;
+import de.digitalcollections.model.filter.FilterCriterion;
+import de.digitalcollections.model.filter.Filtering;
+import de.digitalcollections.model.legal.License;
+import de.digitalcollections.model.paging.Order;
+import de.digitalcollections.model.paging.PageRequest;
+import de.digitalcollections.model.paging.PageResponse;
+import de.digitalcollections.model.paging.Sorting;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.net.URL;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "License controller")
+public class LicenseController {
+
+  private final LicenseService service;
+
+  public LicenseController(LicenseService service) {
+    this.service = service;
+  }
+
+  @Operation(summary = "Get count of licenses")
+  @GetMapping(
+      value = {"/v5/licenses/count"},
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public long count() {
+    return service.count();
+  }
+
+  @Operation(summary = "Delete license by given url")
+  @DeleteMapping(
+      value = {"/v5/licenses"},
+      params = "url",
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Void> deleteByUrl(@RequestParam(name = "url", required = true) String url) {
+    // WARNING: a DELETE request with param seems not to be spec allowed?
+    // an url as path variable is technically not possible (unescaping leads to not allowed
+    // characters in url)
+    service.deleteByUrl(url);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @Operation(summary = "Delete a license by uuid")
+  @DeleteMapping(
+      value = {
+        "/v5/licenses/{uuid:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}"
+      },
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Void> deleteByUuid(
+      @Parameter(example = "", description = "UUID of the license") @PathVariable("uuid")
+          UUID uuid) {
+    service.deleteByUuid(uuid);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @Operation(summary = "Delete licenses by given uuid list")
+  @DeleteMapping(
+      value = {"/v5/licenses"},
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Void> deleteByUuids(@RequestBody List<UUID> uuids) {
+    // WARNING: a DELETE request with body seems not to be spec allowed?
+    // FIXME: How to implement deleteByUrl (also with body? how to distinguish these both methods?
+    // give param?)
+    service.deleteByUuids(uuids);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @Operation(summary = "Get all licenses as (filtered, sorted, paged) list")
+  @GetMapping(
+      value = {"/v5/licenses"},
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public PageResponse<License> find(
+      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
+      @RequestParam(name = "pageSize", required = false, defaultValue = "25") int pageSize,
+      @RequestParam(name = "sortBy", required = false) List<Order> sortBy,
+      @RequestParam(name = "label", required = false) FilterCriterion<String> labelCriterion,
+      @RequestParam(name = "locale", required = false) FilterCriterion<String> localeCriterion) {
+    PageRequest pageRequest = new PageRequest(pageNumber, pageSize);
+    if (sortBy != null) {
+      Sorting sorting = new Sorting(sortBy);
+      pageRequest.setSorting(sorting);
+    }
+    if (labelCriterion != null || localeCriterion != null) {
+      Filtering filtering = new Filtering();
+      if (labelCriterion != null) {
+        filtering.add(Filtering.defaultBuilder().add("label", labelCriterion).build());
+      }
+      if (localeCriterion != null) {
+        filtering.add(
+            Filtering.defaultBuilder()
+                .add(
+                    new FilterCriterion<Locale>(
+                        "locale",
+                        localeCriterion.getOperation(),
+                        Locale.forLanguageTag(localeCriterion.getValue().toString())))
+                .build());
+      }
+      pageRequest.setFiltering(filtering);
+    }
+    return service.find(pageRequest);
+  }
+
+  @Operation(summary = "Get a license by url")
+  @GetMapping(
+      value = {"/v5/licenses"},
+      params = "url",
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public License getByUrl(@RequestParam(name = "url", required = true) URL url) {
+    return service.getByUrl(url);
+  }
+
+  @Operation(summary = "Get a license by uuid")
+  @GetMapping(
+      value = {
+        "/v5/licenses/{uuid:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}"
+      },
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public License getByUuid(@PathVariable UUID uuid) {
+    return service.getByUuid(uuid);
+  }
+
+  @Operation(summary = "Save a newly created license")
+  @PostMapping(
+      value = {"/v5/licenses"},
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public License save(@RequestBody License license, BindingResult errors)
+      throws IdentifiableServiceException, ServiceException {
+    return service.save(license);
+  }
+
+  @Operation(summary = "Update a license")
+  @PutMapping(
+      value = {"/v5/licenses/{uuid}"},
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public License update(
+      @Parameter(example = "", description = "UUID of the license") @PathVariable("uuid") UUID uuid,
+      @RequestBody License license,
+      BindingResult errors)
+      throws IdentifiableServiceException, ServiceException {
+    assert Objects.equals(uuid, license.getUuid());
+    return service.update(license);
+  }
+}
