0a21b821b99e1f1d4078ffb13bbceb2b70dc0b2d
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-02-01 16:29:31 +0100

    Flyway-Migration

---
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V10.01.01__DDL_Table_for_Digitalobjects_Downloadresources.sql
@@ -0,0 +1,10 @@
+ALTER TABLE fileresources_linkeddata ADD PRIMARY KEY(uuid);
+CREATE TABLE IF NOT EXISTS digitalobject_downloadresources (
+  digitalobject_uuid UUID NOT NULL,
+  linkeddata_fileresource_uuid UUID NOT NULL UNIQUE,
+  sortIndex SMALLINT,
+
+  PRIMARY KEY (digitalobject_uuid, linkeddata_fileresource_uuid),
+  FOREIGN KEY (digitalobject_uuid) REFERENCES digitalobjects(uuid),
+  FOREIGN KEY (linkeddata_fileresource_uuid) REFERENCES fileresources_linkeddata(uuid)
+);
