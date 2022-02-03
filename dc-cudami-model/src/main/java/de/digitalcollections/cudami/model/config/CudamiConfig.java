485104b54de3fb77cbb2bc5f935c97f254188771
Matthias Lindinger <morpheus-87@users.noreply.github.com>
2022-02-03 08:45:03 +0100

    Configuration validation (#1470)
    * Fix member sorting
    
    * Add validation for urlalias.maxLength

--- dc-cudami-model/src/main/java/de/digitalcollections/cudami/model/config/CudamiConfig.java
+++ dc-cudami-model/src/main/java/de/digitalcollections/cudami/model/config/CudamiConfig.java
@@ -5,17 +5,8 @@ import java.util.List;
 import java.util.Locale;
 
 public class CudamiConfig {
-
-  UrlAlias urlAlias;
   Defaults defaults;
-
-  public UrlAlias getUrlAlias() {
-    return urlAlias;
-  }
-
-  public void setUrlAlias(UrlAlias urlAlias) {
-    this.urlAlias = urlAlias;
-  }
+  UrlAlias urlAlias;
 
   public Defaults getDefaults() {
     return defaults;
@@ -25,30 +16,15 @@ public class CudamiConfig {
     this.defaults = defaults;
   }
 
-  public static class UrlAlias {
+  public UrlAlias getUrlAlias() {
+    return urlAlias;
+  }
 
-    private List<EntityType> generationExcludes;
-    private int maxLength = -1;
-
-    public List<EntityType> getGenerationExcludes() {
-      return this.generationExcludes;
-    }
-
-    public void setGenerationExcludes(List<EntityType> generationExcludes) {
-      this.generationExcludes = generationExcludes;
-    }
-
-    public int getMaxLength() {
-      return maxLength;
-    }
-
-    public void setMaxLength(int maxLength) {
-      this.maxLength = maxLength;
-    }
+  public void setUrlAlias(UrlAlias urlAlias) {
+    this.urlAlias = urlAlias;
   }
 
   public static class Defaults {
-
     String language;
     Locale locale;
 
@@ -66,6 +42,35 @@ public class CudamiConfig {
 
     public void setLocale(Locale locale) {
       this.locale = locale;
+    }
+  }
+
+  public static class UrlAlias {
+    private static final int DB_MAX_LENGTH = 256;
+
+    private List<EntityType> generationExcludes;
+    private int maxLength = -1;
+
+    public List<EntityType> getGenerationExcludes() {
+      return this.generationExcludes;
+    }
+
+    public void setGenerationExcludes(List<EntityType> generationExcludes) {
+      this.generationExcludes = generationExcludes;
+    }
+
+    public int getMaxLength() {
+      return maxLength;
+    }
+
+    public void setMaxLength(int maxLength) {
+      if (maxLength > DB_MAX_LENGTH) {
+        throw new RuntimeException(
+            "The maxLength you configured is invalid, because it is greater than "
+                + DB_MAX_LENGTH
+                + " (this is the greatest possible length in the database)!");
+      }
+      this.maxLength = maxLength;
     }
   }
 }
