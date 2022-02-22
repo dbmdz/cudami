6311f6c2dcb00ae49ec59503a42530b74d5e0dcd
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-02-22 15:03:09 +0100

    Fixed find method to use base url instead of search endpoint

--- dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/identifiable/entity/geo/location/CudamiHumanSettlementsClient.java
+++ dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/identifiable/entity/geo/location/CudamiHumanSettlementsClient.java
@@ -2,12 +2,21 @@ package de.digitalcollections.cudami.client.identifiable.entity.geo.location;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.digitalcollections.cudami.client.identifiable.entity.CudamiEntitiesClient;
+import de.digitalcollections.model.exception.TechnicalException;
 import de.digitalcollections.model.identifiable.entity.geo.location.HumanSettlement;
+import de.digitalcollections.model.paging.SearchPageRequest;
+import de.digitalcollections.model.paging.SearchPageResponse;
 import java.net.http.HttpClient;
 
 public class CudamiHumanSettlementsClient extends CudamiEntitiesClient<HumanSettlement> {
 
   public CudamiHumanSettlementsClient(HttpClient http, String serverUrl, ObjectMapper mapper) {
     super(http, serverUrl, HumanSettlement.class, mapper, "/v5/humansettlements");
+  }
+
+  @Override
+  public SearchPageResponse<HumanSettlement> find(SearchPageRequest searchPageRequest)
+      throws TechnicalException {
+    return doGetSearchRequestForPagedObjectList(baseEndpoint, searchPageRequest);
   }
 }
