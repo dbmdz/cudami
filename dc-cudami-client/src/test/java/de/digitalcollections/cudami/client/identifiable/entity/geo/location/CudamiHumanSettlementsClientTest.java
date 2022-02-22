048899d6a64dcbccf994ad622886213e44f75dff
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-02-22 15:03:09 +0100

    Fixed find method to use base url instead of search endpoint

--- dc-cudami-client/src/test/java/de/digitalcollections/cudami/client/identifiable/entity/geo/location/CudamiHumanSettlementsClientTest.java
+++ dc-cudami-client/src/test/java/de/digitalcollections/cudami/client/identifiable/entity/geo/location/CudamiHumanSettlementsClientTest.java
@@ -1,9 +1,43 @@
 package de.digitalcollections.cudami.client.identifiable.entity.geo.location;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 import de.digitalcollections.cudami.client.identifiable.entity.BaseCudamiEntitiesClientTest;
 import de.digitalcollections.model.identifiable.entity.geo.location.HumanSettlement;
+import de.digitalcollections.model.paging.SearchPageRequest;
+import de.digitalcollections.model.paging.SearchPageResponse;
+import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("The client for HumanSettlements")
 class CudamiHumanSettlementsClientTest
-    extends BaseCudamiEntitiesClientTest<HumanSettlement, CudamiHumanSettlementsClient> {}
+    extends BaseCudamiEntitiesClientTest<HumanSettlement, CudamiHumanSettlementsClient> {
+
+  @Test
+  @DisplayName("can execute the find method with a SearchPageRequest")
+  @Override
+  public void testFindWithSearchPageRequest() throws Exception {
+    String bodyJson = "{}";
+    when(httpResponse.body()).thenReturn(bodyJson.getBytes(StandardCharsets.UTF_8));
+
+    SearchPageRequest searchPageRequest = new SearchPageRequest();
+    SearchPageResponse<HumanSettlement> response = client.find(searchPageRequest);
+    assertThat(response).isNotNull();
+
+    verifyHttpRequestByMethodAndRelativeURL("get", "?pageNumber=0&pageSize=0");
+  }
+
+  @Test
+  @DisplayName("can execute the find method with a search term and max results")
+  @Override
+  public void testFindWithSearchTermAndMaxResults() throws Exception {
+    String bodyJson = "{\"content\":[]}";
+    when(httpResponse.body()).thenReturn(bodyJson.getBytes(StandardCharsets.UTF_8));
+
+    assertThat(client.find("foo", 100)).isNotNull();
+
+    verifyHttpRequestByMethodAndRelativeURL("get", "?pageNumber=0&pageSize=100&searchTerm=foo");
+  }
+}
