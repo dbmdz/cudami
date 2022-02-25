76fc1cc1ba50ea56730eb5d4767dc458dbe82270
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-02-25 12:00:17 +0100

    Fill license per bifunction

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/test/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/entity/DigitalObjectRepositoryImplTest.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/test/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/entity/DigitalObjectRepositoryImplTest.java
@@ -95,7 +95,7 @@ class DigitalObjectRepositoryImplTest {
     // Insert a license with uuid
     LicenseRepositoryImpl licenseRepository = new LicenseRepositoryImpl(jdbi, cudamiConfig);
     licenseRepository.save(EXISTING_LICENSE);
-    
+
     // Insert a corporate body with UUID
     CorporateBody creator =
         new CorporateBodyBuilder()
