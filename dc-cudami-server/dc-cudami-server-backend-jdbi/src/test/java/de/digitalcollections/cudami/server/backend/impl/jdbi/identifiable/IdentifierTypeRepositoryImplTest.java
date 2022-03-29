ddb0500d033a391ffd8115b3cac4cca4f6c95894
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-03-29 16:39:41 +0200

    Tested and fixed find method

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/test/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/IdentifierTypeRepositoryImplTest.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/test/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/IdentifierTypeRepositoryImplTest.java
@@ -4,7 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import de.digitalcollections.cudami.model.config.CudamiConfig;
 import de.digitalcollections.cudami.server.backend.impl.database.config.SpringConfigBackendDatabase;
+import de.digitalcollections.model.filter.FilterCriterion;
+import de.digitalcollections.model.filter.FilterOperation;
+import de.digitalcollections.model.filter.Filtering;
 import de.digitalcollections.model.identifiable.IdentifierType;
+import de.digitalcollections.model.paging.PageRequest;
+import de.digitalcollections.model.paging.PageRequestBuilder;
+import de.digitalcollections.model.paging.PageResponse;
+import java.util.List;
 import java.util.UUID;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.BeforeEach;
@@ -155,5 +162,41 @@ class IdentifierTypeRepositoryImplTest {
     expected.setLabel("otherlabel");
     expected.setPattern("otherpattern");
     expected.setNamespace("othernamespace-" + System.currentTimeMillis());
+  }
+
+  @Test
+  @DisplayName("can find identifier types")
+  void find() {
+    // Insert two identifier types
+    String namespace1 = "type-namespace-" + System.currentTimeMillis();
+    String namespace2 = "type-namespace-" + (System.currentTimeMillis() + 1);
+
+    IdentifierType type1 = new IdentifierType();
+    type1.setLabel("type-label-1");
+    type1.setNamespace(namespace1);
+    type1.setPattern("type-pattern-1");
+    type1 = repo.save(type1);
+
+    IdentifierType type2 = new IdentifierType();
+    type2.setLabel("type-label-2");
+    type2.setNamespace(namespace2);
+    type2.setPattern("type-pattern-2");
+    type2 = repo.save(type2);
+
+    PageRequest pageRequest =
+        new PageRequestBuilder()
+            .pageNumber(0)
+            .pageSize(99)
+            .filtering(
+                new Filtering(
+                    List.of(new FilterCriterion("namespace", FilterOperation.EQUALS, namespace1))))
+            .build();
+    PageResponse<IdentifierType> pageResponse = repo.find(pageRequest);
+    List<IdentifierType> actualContent = pageResponse.getContent();
+    assertThat(actualContent).hasSize(1);
+    IdentifierType actual = actualContent.get(0);
+    assertThat(actual.getLabel()).isEqualTo(type1.getLabel());
+    assertThat(actual.getNamespace()).isEqualTo(type1.getNamespace());
+    assertThat(actual.getPattern()).isEqualTo(type1.getPattern());
   }
 }
