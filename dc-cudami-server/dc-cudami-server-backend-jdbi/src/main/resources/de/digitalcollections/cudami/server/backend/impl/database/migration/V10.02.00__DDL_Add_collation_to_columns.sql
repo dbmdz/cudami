dc02a4bdb9a13d91e18b23f5dfba86b047e4b87a
David Förster <david.foerster@bsb-muenchen.de>
2022-03-30 11:05:21 +0200

    Reformat SQL migration script

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V10.02.00__DDL_Add_collation_to_columns.sql
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V10.02.00__DDL_Add_collation_to_columns.sql
@@ -11,7 +11,8 @@ ALTER TABLE corporatebodies ALTER COLUMN homepage_url TYPE varchar COLLATE "ucs_
 
 ALTER TABLE geolocations ALTER COLUMN geolocation_type TYPE varchar COLLATE "ucs_basic";
 
-ALTER TABLE items ALTER COLUMN "language" TYPE varchar COLLATE "ucs_basic",
+ALTER TABLE items
+  ALTER COLUMN "language" TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN publication_date TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN publication_place TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN publisher TYPE varchar COLLATE "ucs_basic",
@@ -21,45 +22,54 @@ ALTER TABLE persons ALTER COLUMN gender TYPE varchar COLLATE "ucs_basic";
 
 ALTER TABLE websites ALTER COLUMN url TYPE varchar COLLATE "ucs_basic";
 
-ALTER TABLE fileresources ALTER COLUMN filename TYPE varchar COLLATE "ucs_basic",
+ALTER TABLE fileresources
+  ALTER COLUMN filename TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN mimetype TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN uri TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN http_base_url TYPE varchar COLLATE "ucs_basic";
 
-ALTER TABLE fileresources_linkeddata ALTER COLUMN context TYPE varchar(512) COLLATE "ucs_basic",
+ALTER TABLE fileresources_linkeddata
+  ALTER COLUMN context TYPE varchar(512) COLLATE "ucs_basic",
   ALTER COLUMN object_type TYPE varchar(512) COLLATE "ucs_basic";
 
 ALTER TABLE givennames ALTER COLUMN gender TYPE varchar COLLATE "ucs_basic";
 
-ALTER TABLE headwords ALTER COLUMN "label" TYPE varchar(256) COLLATE "ucs_basic",
+ALTER TABLE headwords
+  ALTER COLUMN "label" TYPE varchar(256) COLLATE "ucs_basic",
   ALTER COLUMN locale TYPE varchar COLLATE "ucs_basic";
 
 ALTER TABLE humansettlements ALTER COLUMN settlement_type TYPE varchar COLLATE "ucs_basic";
 
-ALTER TABLE identifiers ALTER COLUMN "namespace" TYPE varchar COLLATE "ucs_basic",
+ALTER TABLE identifiers
+  ALTER COLUMN "namespace" TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN identifier TYPE varchar COLLATE "ucs_basic";
 
-ALTER TABLE identifiertypes ALTER COLUMN "label" TYPE varchar COLLATE "ucs_basic",
+ALTER TABLE identifiertypes
+  ALTER COLUMN "label" TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN "namespace" TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN pattern TYPE varchar COLLATE "ucs_basic";
 
-ALTER TABLE licenses ALTER COLUMN acronym TYPE varchar COLLATE "ucs_basic",
+ALTER TABLE licenses
+  ALTER COLUMN acronym TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN url TYPE varchar COLLATE "ucs_basic";
 
 ALTER TABLE predicates ALTER COLUMN value TYPE varchar COLLATE "ucs_basic";
 
 ALTER TABLE rendering_templates ALTER COLUMN "name" TYPE varchar COLLATE "ucs_basic";
 
-ALTER TABLE url_aliases ALTER COLUMN slug TYPE varchar(256) COLLATE "ucs_basic",
+ALTER TABLE url_aliases
+  ALTER COLUMN slug TYPE varchar(256) COLLATE "ucs_basic",
   ALTER COLUMN target_identifiable_type TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN target_entity_type TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN target_language TYPE varchar(2) COLLATE "ucs_basic";
 
-ALTER TABLE users ALTER COLUMN email TYPE varchar COLLATE "ucs_basic",
+ALTER TABLE users
+  ALTER COLUMN email TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN firstname TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN lastname TYPE varchar COLLATE "ucs_basic";
 
-ALTER TABLE versions ALTER COLUMN description TYPE varchar COLLATE "ucs_basic",
+ALTER TABLE versions
+  ALTER COLUMN description TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN type_key TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN instance_key TYPE varchar COLLATE "ucs_basic",
   ALTER COLUMN instance_version_key TYPE varchar COLLATE "ucs_basic",
