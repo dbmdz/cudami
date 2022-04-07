e840317e161cebf3304247188b9aaa5185e70f5d
Matthias Lindinger <morpheus-87@users.noreply.github.com>
2022-04-07 15:56:19 +0200

    Apply suggestions from code review
    Co-authored-by: David Förster <david.foerster@bsb-muenchen.de>

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V10.00.00__DDL_Table_for_Licenses.sql
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V10.00.00__DDL_Table_for_Licenses.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS licenses (
   uuid UUID NOT NULL PRIMARY KEY,
-  acronym VARCHAR,
+  acronym VARCHAR COLLATE "ucs_basic",
   label JSONB,
-  url VARCHAR NOT NULL UNIQUE,
+  url VARCHAR COLLATE "ucs_basic" NOT NULL UNIQUE,
 
   created TIMESTAMP NOT NULL,
   last_modified TIMESTAMP NOT NULL
