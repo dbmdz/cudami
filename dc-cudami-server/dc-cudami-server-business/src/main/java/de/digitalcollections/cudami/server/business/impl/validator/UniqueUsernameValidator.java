f1f22178d836ab96f16ae71cccdd236966e773ab
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-04-11 15:02:55 +0200

    Fix circular dependency

--- dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/impl/validator/UniqueUsernameValidator.java
+++ dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/impl/validator/UniqueUsernameValidator.java
@@ -2,17 +2,18 @@ package de.digitalcollections.cudami.server.business.impl.validator;
 
 import de.digitalcollections.cudami.server.business.api.service.security.UserService;
 import de.digitalcollections.model.security.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 /** Validates that username does not exist, yet. */
-@Component
 public class UniqueUsernameValidator implements Validator {
 
-  @Autowired UserService userService;
+  private final UserService userService;
+
+  public UniqueUsernameValidator(UserService userService) {
+    this.userService = userService;
+  }
 
   @Override
   public boolean supports(Class<?> clazz) {
