07274aa0fabd8d982c93b2a1d4562bc11f5cd49d
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-04-12 11:11:05 +0200

    Review fixes

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V10.02.01__DDL_Table_for_Digitalobjects_Downloadresources.sql
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V10.02.01__DDL_Table_for_Digitalobjects_Downloadresources.sql
@@ -1,7 +1,7 @@
 ALTER TABLE fileresources_linkeddata ADD PRIMARY KEY(uuid);
 CREATE TABLE IF NOT EXISTS digitalobject_linkeddataresources (
   digitalobject_uuid UUID NOT NULL,
-  linkeddata_fileresource_uuid UUID NOT NULL UNIQUE,
+  linkeddata_fileresource_uuid UUID NOT NULL,
   sortIndex SMALLINT,
 
   PRIMARY KEY (digitalobject_uuid, linkeddata_fileresource_uuid),
