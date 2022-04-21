bc5bef950419f2cb41608efe918910b803ae1712
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-04-21 11:17:05 +0200

    Update dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/impl/service/identifiable/IdentifiableServiceImpl.java
    Co-authored-by: David Förster <david.foerster@bsb-muenchen.de>

--- dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/impl/service/identifiable/IdentifiableServiceImpl.java
+++ dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/impl/service/identifiable/IdentifiableServiceImpl.java
@@ -330,7 +330,7 @@ public class IdentifiableServiceImpl<I extends Identifiable> implements Identifi
       throw new IdentifiableServiceException(e.getMessage());
     }
     try {
-      // If we don't want no UrlAliases for this kind of identifiable, we return early
+      // If we do not want any UrlAliases for this kind of identifiable, we return early
       if (IdentifiableUrlAliasAlignHelper.checkIdentifiableExcluded(identifiable, cudamiConfig)) {
         return repository.getByUuid(identifiable.getUuid());
       }
