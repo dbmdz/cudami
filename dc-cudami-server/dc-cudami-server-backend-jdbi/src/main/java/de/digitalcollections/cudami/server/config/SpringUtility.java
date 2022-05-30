f1777846d17191680b783861683c11e20158d58f
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-05-30 14:40:42 +0200

    Fix warning on Autowired annotation

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/config/SpringUtility.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/config/SpringUtility.java
@@ -1,7 +1,6 @@
 package de.digitalcollections.cudami.server.config;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -9,11 +8,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringUtility implements ApplicationContextAware {
 
-  @Autowired private static ApplicationContext applicationContext;
+  private static ApplicationContext applicationContext;
 
+  @Override
   @SuppressFBWarnings(value = {"ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD"})
   public void setApplicationContext(final ApplicationContext applicationContext) {
-    this.applicationContext = applicationContext;
+    SpringUtility.applicationContext = applicationContext;
   }
 
   /*
