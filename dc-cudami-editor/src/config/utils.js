bf7164b92b9f363548ff1a18c620fb66f12da04e
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-06-01 13:13:11 +0200

    Fix copy&paste of iframe, image and video blocks

--- dc-cudami-editor/src/config/utils.js
+++ dc-cudami-editor/src/config/utils.js
@@ -1,3 +1,6 @@
+export const getAttributes = (attrs, dom) =>
+  Object.fromEntries(attrs.map((attr) => [attr, dom.getAttribute(attr)]))
+
 export const markActive = (type) => (state) => {
   const {from, $from, to, empty} = state.selection
 
