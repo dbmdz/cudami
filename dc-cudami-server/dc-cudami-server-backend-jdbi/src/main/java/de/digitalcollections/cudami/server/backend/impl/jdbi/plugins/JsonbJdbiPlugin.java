542929ee4645279924ac8457840084e420294a36
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-06-09 14:26:53 +0200

    Fixed persistence of notes

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/plugins/JsonbJdbiPlugin.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/plugins/JsonbJdbiPlugin.java
@@ -3,6 +3,8 @@ package de.digitalcollections.cudami.server.backend.impl.jdbi.plugins;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.digitalcollections.commons.jdbi.JsonbArgumentFactory;
 import de.digitalcollections.commons.jdbi.JsonbColumnMapperFactory;
+import de.digitalcollections.commons.jdbi.JsonbListArgumentFactory;
+import de.digitalcollections.commons.jdbi.JsonbListColumnMapperFactory;
 import de.digitalcollections.model.geo.CoordinateLocation;
 import de.digitalcollections.model.identifiable.entity.CustomAttributes;
 import de.digitalcollections.model.text.LocalizedStructuredContent;
@@ -31,6 +33,8 @@ public class JsonbJdbiPlugin implements JdbiPlugin {
     db.registerArgument(new JsonbArgumentFactory<>(RenderingHints.class, objectMapper));
     db.registerArgument(new JsonbArgumentFactory<>(RenderingHintsPreviewImage.class, objectMapper));
     db.registerArgument(new JsonbArgumentFactory<>(TimeValue.class, objectMapper));
+    db.registerArgument(
+        new JsonbListArgumentFactory<>(LocalizedStructuredContent.class, objectMapper));
 
     // column mapper
     db.registerColumnMapper(new JsonbColumnMapperFactory(CoordinateLocation.class, objectMapper));
@@ -42,5 +46,7 @@ public class JsonbJdbiPlugin implements JdbiPlugin {
     db.registerColumnMapper(
         new JsonbColumnMapperFactory(RenderingHintsPreviewImage.class, objectMapper));
     db.registerColumnMapper(new JsonbColumnMapperFactory(TimeValue.class, objectMapper));
+    db.registerColumnMapper(
+        new JsonbListColumnMapperFactory(LocalizedStructuredContent.class, objectMapper));
   }
 }
