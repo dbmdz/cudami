cc584381ac467be7413a99fd2099a95a4366c0b8
David Förster <david.foerster@bsb-muenchen.de>
2022-07-05 19:18:32 +0200

    Add placeholder prop to `SearchTermTemplates`

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/SearchTermTemplates.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/SearchTermTemplates.java
@@ -1,8 +1,8 @@
 package de.digitalcollections.cudami.server.backend.impl.jdbi.identifiable;
 
 public enum SearchTermTemplates {
-  ILIKE_SEARCH("%s.%s ILIKE '%%' || :searchTerm || '%%'"),
-  ILIKE_STARTS_WITH("%s.%s ILIKE :searchTerm || '%%'"),
+  ILIKE_SEARCH("%s.%s ILIKE '%%' || :searchTerm || '%%'", "searchTerm"),
+  ILIKE_STARTS_WITH("%s.%s ILIKE :searchTerm || '%%'", "searchTerm"),
   // FYI: [JSON Path
   // Functions](https://www.postgresql.org/docs/12/functions-json.html#FUNCTIONS-SQLJSON-PATH)
   // and [Data type](https://www.postgresql.org/docs/12/datatype-json.html#DATATYPE-JSONPATH)
@@ -14,13 +14,16 @@ public enum SearchTermTemplates {
   // example in
   // https://www.postgresql.org/docs/12/functions-json.html#FUNCTIONS-SQLJSON-PATH
   JSONB_PATH(
-      "jsonb_path_exists(%s.%s, ('$.%s ? (@ like_regex \"' || :searchTerm || '\" flag \"iq\")')::jsonpath)"),
-  ARRAY_CONTAINS("%s.%s @> :searchTermArray::TEXT[]");
+      "jsonb_path_exists(%s.%s, ('$.%s ? (@ like_regex \"' || :searchTerm || '\" flag \"iq\")')::jsonpath)",
+      "searchTerm"),
+  ARRAY_CONTAINS("%s.%s @> :searchTermArray::TEXT[]", "searchTermArray");
 
   private final String template;
+  public final String placeholder;
 
-  SearchTermTemplates(String template) {
+  SearchTermTemplates(String template, String placeholder) {
     this.template = template;
+    this.placeholder = placeholder;
   }
 
   public String renderTemplate(Object... values) {
