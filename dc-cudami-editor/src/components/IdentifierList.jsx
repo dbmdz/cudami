d2f59f2cd406ea85ce0282c1bb993debc393d382
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-07-12 15:23:32 +0200

    Add optimised design for the list of identifiers

--- dc-cudami-editor/src/components/IdentifierList.jsx
+++ dc-cudami-editor/src/components/IdentifierList.jsx
@@ -1,14 +1,17 @@
-import {List, ListInlineItem} from 'reactstrap'
+import sortBy from 'lodash/sortBy'
+import {Badge, List, ListInlineItem} from 'reactstrap'
 
 const IdentifierList = ({identifiers = [], identifierTypes = []}) =>
   identifiers.length > 0 ? (
     <List className="mb-0" type="inline">
-      {identifiers.map(({id, namespace}) => (
-        <ListInlineItem key={`${namespace}:${id}`}>{`${
-          identifierTypes.find(
-            (identifierType) => identifierType.namespace === namespace,
-          )?.label ?? namespace
-        }: ${id}`}</ListInlineItem>
+      {sortBy(identifiers, ['namespace']).map(({id, namespace}) => (
+        <ListInlineItem key={`${namespace}:${id}`}>
+          <Badge className="pb-1 pt-1" color="primary" pill>{`${
+            identifierTypes.find(
+              (identifierType) => identifierType.namespace === namespace,
+            )?.label ?? namespace
+          }: ${id}`}</Badge>
+        </ListInlineItem>
       ))}
     </List>
   ) : null
