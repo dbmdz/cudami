ca7b442b027fa84c31a584665499da1fb291f1e7
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-07-14 11:21:41 +0200

    Add view for a single item

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/work/ItemsController.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/work/ItemsController.java
@@ -3,7 +3,9 @@ package de.digitalcollections.cudami.admin.controller.identifiable.entity.work;
 import de.digitalcollections.cudami.admin.util.LanguageSortingHelper;
 import de.digitalcollections.cudami.client.CudamiClient;
 import de.digitalcollections.cudami.client.identifiable.entity.work.CudamiItemsClient;
+import de.digitalcollections.model.exception.ResourceNotFoundException;
 import de.digitalcollections.model.exception.TechnicalException;
+import de.digitalcollections.model.identifiable.entity.DigitalObject;
 import de.digitalcollections.model.identifiable.entity.work.Item;
 import de.digitalcollections.model.list.paging.PageRequest;
 import de.digitalcollections.model.list.paging.PageResponse;
@@ -11,11 +13,13 @@ import de.digitalcollections.model.list.sorting.Order;
 import de.digitalcollections.model.list.sorting.Sorting;
 import java.util.List;
 import java.util.Locale;
+import java.util.UUID;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -46,6 +50,18 @@ public class ItemsController {
     return service.find(pageRequest);
   }
 
+  @GetMapping("/api/items/{uuid}/digitalobjects")
+  @ResponseBody
+  public PageResponse<DigitalObject> getDigitalObjects(
+      @PathVariable UUID uuid,
+      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
+      @RequestParam(name = "pageSize", required = false, defaultValue = "25") int pageSize,
+      @RequestParam(name = "searchTerm", required = false) String searchTerm)
+      throws TechnicalException {
+    PageRequest pageRequest = new PageRequest(searchTerm, pageNumber, pageSize);
+    return service.findDigitalObjects(uuid, pageRequest);
+  }
+
   @GetMapping("/items")
   public String list(Model model) throws TechnicalException {
     final Locale displayLocale = LocaleContextHolder.getLocale();
@@ -58,5 +74,23 @@ public class ItemsController {
   @ModelAttribute("menu")
   protected String module() {
     return "items";
+  }
+
+  @GetMapping("/items/{uuid}")
+  public String view(@PathVariable UUID uuid, Model model)
+      throws TechnicalException, ResourceNotFoundException {
+    Item item = service.getByUuid(uuid);
+    if (item == null) {
+      throw new ResourceNotFoundException();
+    }
+
+    Locale displayLocale = LocaleContextHolder.getLocale();
+    List<Locale> existingDigitalObjectLanguages =
+        languageSortingHelper.sortLanguages(
+            displayLocale, service.getLanguagesOfDigitalObjects(uuid));
+    model
+        .addAttribute("item", item)
+        .addAttribute("existingDigitalObjectLanguages", existingDigitalObjectLanguages);
+    return "items/view";
   }
 }
