f43fb555b6220399cdbbc5c3e4d92486b0e84bff
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-08-17 11:04:58 +0200

    First idea

--- dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/identifiable/AbstractIdentifiableController.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/identifiable/AbstractIdentifiableController.java
@@ -23,6 +23,23 @@ public abstract class AbstractIdentifiableController<T extends Identifiable> {
 
   public ResponseEntity<T> getByIdentifier(HttpServletRequest request)
       throws IdentifiableServiceException, ValidationException {
+    Pair<String, String> namespaceAndId = extractNamespaceAndId(request);
+
+    T identifiable =
+        getService().getByIdentifier(namespaceAndId.getLeft(), namespaceAndId.getRight());
+    return new ResponseEntity<>(
+        identifiable, identifiable != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+  }
+
+  /**
+   * Extract the namespace and identifier from the HttpServletRequest
+   *
+   * @param request the HttpServletRequest
+   * @return Pair of namespace and identifier
+   * @throws ValidationException in case of a missing namespace or malformed parameter
+   */
+  public static Pair<String, String> extractNamespaceAndId(HttpServletRequest request)
+      throws ValidationException {
     Pair<String, String> namespaceAndId =
         ParameterHelper.extractPairOfStringsFromUri(request.getRequestURI(), "^.*?/identifier/");
     if (namespaceAndId.getLeft().isBlank()
@@ -30,11 +47,7 @@ public abstract class AbstractIdentifiableController<T extends Identifiable> {
       throw new ValidationException(
           "No namespace and/or id were provided in a colon separated manner");
     }
-
-    T identifiable =
-        getService().getByIdentifier(namespaceAndId.getLeft(), namespaceAndId.getRight());
-    return new ResponseEntity<>(
-        identifiable, identifiable != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+    return namespaceAndId;
   }
 
   /**
