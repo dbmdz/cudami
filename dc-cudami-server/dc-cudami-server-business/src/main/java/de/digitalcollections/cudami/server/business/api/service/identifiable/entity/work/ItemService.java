534642375ddeddb879d210b1416114311f24df41
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-08-30 14:22:11 +0200

    Refactorings to deliver a (yet partially recursively) filled DigitalObject

--- dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/api/service/identifiable/entity/work/ItemService.java
+++ dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/api/service/identifiable/entity/work/ItemService.java
@@ -1,8 +1,5 @@
 package de.digitalcollections.cudami.server.business.api.service.identifiable.entity.work;
 
-import de.digitalcollections.cudami.server.business.api.service.exceptions.ConflictException;
-import de.digitalcollections.cudami.server.business.api.service.exceptions.IdentifiableServiceException;
-import de.digitalcollections.cudami.server.business.api.service.exceptions.ValidationException;
 import de.digitalcollections.cudami.server.business.api.service.identifiable.entity.EntityService;
 import de.digitalcollections.model.identifiable.entity.DigitalObject;
 import de.digitalcollections.model.identifiable.entity.work.Item;
@@ -15,9 +12,6 @@ import java.util.Set;
 import java.util.UUID;
 
 public interface ItemService extends EntityService<Item> {
-
-  boolean addDigitalObject(UUID uuid, UUID digitalObjectUuid)
-      throws ConflictException, ValidationException, IdentifiableServiceException;
 
   boolean addWork(UUID uuid, UUID workUuid);
 
