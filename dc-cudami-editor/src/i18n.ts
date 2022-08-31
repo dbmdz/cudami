425e064dff0e0b3933afddc009adfe6520838964
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-08-31 14:21:53 +0200

    Fix imports

--- dc-cudami-editor/src/i18n.ts
+++ dc-cudami-editor/src/i18n.ts
@@ -1,13 +1,13 @@
-import i18n from 'i18next'
+import {createInstance} from 'i18next'
 import {initReactI18next} from 'react-i18next'
 
-import deLanguageNames from './locales/de/languages.json'
-import de from './locales/de/translation.json'
-import enLanguageNames from './locales/en/languages.json'
-import en from './locales/en/translation.json'
+import * as deLanguageNames from './locales/de/languages.json'
+import * as de from './locales/de/translation.json'
+import * as enLanguageNames from './locales/en/languages.json'
+import * as en from './locales/en/translation.json'
 
 export default function (locale: string) {
-  const instance = i18n.createInstance()
+  const instance = createInstance()
   instance.use(initReactI18next).init({
     fallbackLng: 'en',
     interpolation: {
