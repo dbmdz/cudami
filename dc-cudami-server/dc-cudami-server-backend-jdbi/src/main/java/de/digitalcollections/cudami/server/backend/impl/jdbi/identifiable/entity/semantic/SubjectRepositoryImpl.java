57076c7dda1db98300a9b52dcc630f0f99eb12d7
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-09-27 15:05:37 +0200

    Starting with subject persistence

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/entity/semantic/SubjectRepositoryImpl.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/entity/semantic/SubjectRepositoryImpl.java
@@ -6,6 +6,7 @@ import de.digitalcollections.cudami.server.backend.impl.jdbi.JdbiRepositoryImpl;
 import de.digitalcollections.model.identifiable.entity.semantic.Subject;
 import de.digitalcollections.model.list.paging.PageRequest;
 import de.digitalcollections.model.list.paging.PageResponse;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import org.jdbi.v3.core.Jdbi;
@@ -17,8 +18,18 @@ public class SubjectRepositoryImpl extends JdbiRepositoryImpl implements Subject
 
   // TODO
   public static final String TABLE_NAME = "subjects";
-  public static final String TABLE_ALIAS = "sj";
-  public static final String MAPPING_PREFIX = "sj";
+  public static final String TABLE_ALIAS = "subj";
+  public static final String MAPPING_PREFIX = "subj";
+
+  public static final String SQL_INSERT_FIELDS =
+      " uuid, label, type, identifiers, created, last_modified";
+  public static final String SQL_INSERT_VALUES =
+      " :uuid, :label::JSONB, :type, :identifiers, :created, :lastModified";
+  public static final String SQL_REDUCED_FIELDS_SUBJECTS =
+      String.format(
+          " %1$s.uuid as %2$s_uuid, %1$s.label as %2$s_label, %1$s.identifiers as %2$s_identifiers, %1$s.type as %2$s_type, %1$s.identifiers as %2$s_identifiers, %1$s.created as %2$s_created, %1$s.last_modified as %2$s_last_modified",
+          TABLE_ALIAS, MAPPING_PREFIX);
+  public static final String SQL_FULL_FIELDS_SUBJECTS = SQL_REDUCED_FIELDS_SUBJECTS;
 
   public SubjectRepositoryImpl(Jdbi dbi, CudamiConfig cudamiConfig) {
     super(
@@ -28,14 +39,48 @@ public class SubjectRepositoryImpl extends JdbiRepositoryImpl implements Subject
 
   @Override
   public Subject getByUuid(UUID uuid) {
-    // TODO Auto-generated method stub
-    return null;
+    final String sql =
+        "SELECT "
+            + SQL_FULL_FIELDS_SUBJECTS
+            + " FROM "
+            + tableName
+            + " AS "
+            + tableAlias
+            + " WHERE uuid = :uuid";
+
+    Subject subject =
+        dbi.withHandle(
+            h -> h.createQuery(sql).bind("uuid", uuid).mapTo(Subject.class).findOne().orElse(null));
+
+    return subject;
   }
 
   @Override
   public Subject save(Subject subject) {
-    // TODO Auto-generated method stub
-    return null;
+    subject.setUuid(UUID.randomUUID());
+    subject.setCreated(LocalDateTime.now());
+    subject.setLastModified(LocalDateTime.now());
+
+    final String sql =
+        "INSERT INTO "
+            + tableName
+            + "("
+            + SQL_INSERT_FIELDS
+            + ")"
+            + " VALUES ("
+            + SQL_INSERT_VALUES
+            + ")"
+            + " RETURNING *";
+
+    Subject result =
+        dbi.withHandle(
+            h ->
+                h.createQuery(sql)
+                    .bindBean(subject)
+                    .mapToBean(Subject.class)
+                    .findOne()
+                    .orElse(null));
+    return result;
   }
 
   @Override
