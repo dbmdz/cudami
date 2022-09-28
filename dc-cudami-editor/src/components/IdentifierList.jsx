1324decb47bd14184a71bb226aec0d44ee0e27bd
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-09-28 14:57:46 +0200

    Move identifier label to tooltip

--- dc-cudami-editor/src/components/IdentifierList.jsx
+++ dc-cudami-editor/src/components/IdentifierList.jsx
@@ -4,15 +4,21 @@ import {Badge, List, ListInlineItem} from 'reactstrap'
 const IdentifierList = ({identifiers = [], identifierTypes = []}) =>
   identifiers.length > 0 ? (
     <List className="mb-0" type="inline">
-      {sortBy(identifiers, ['namespace']).map(({id, namespace}) => (
-        <ListInlineItem key={`${namespace}:${id}`}>
-          <Badge className="border border-dark p-2" color="light" pill>{`${
-            identifierTypes.find(
-              (identifierType) => identifierType.namespace === namespace,
-            )?.label ?? namespace
-          }: ${id}`}</Badge>
-        </ListInlineItem>
-      ))}
+      {sortBy(identifiers, ['namespace']).map(({id, namespace}) => {
+        const identifierType = identifierTypes.find(
+          (identifierType) => identifierType.namespace === namespace,
+        )
+        return (
+          <ListInlineItem key={`${namespace}:${id}`}>
+            <Badge
+              className="border border-dark p-2"
+              color="light"
+              pill
+              title={identifierType?.label}
+            >{`${namespace}:${id}`}</Badge>
+          </ListInlineItem>
+        )
+      })}
     </List>
   ) : null
 
