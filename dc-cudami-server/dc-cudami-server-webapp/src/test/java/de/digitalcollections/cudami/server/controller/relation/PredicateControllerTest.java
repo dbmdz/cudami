ebf59d92f8b02c30318d557f6a0341ae5b3fb210
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-10-20 16:02:25 +0200

    Refactoring after review

--- dc-cudami-server/dc-cudami-server-webapp/src/test/java/de/digitalcollections/cudami/server/controller/relation/PredicateControllerTest.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/test/java/de/digitalcollections/cudami/server/controller/relation/PredicateControllerTest.java
@@ -20,9 +20,9 @@ class PredicateControllerTest extends BaseControllerTest {
 
   @MockBean private PredicateService predicateService;
 
-  @DisplayName("can create a predicate")
+  @DisplayName("can save a predicate without UUID")
   @Test
-  public void create() throws Exception {
+  public void save() throws Exception {
     UUID uuid = UUID.fromString("bb5885f6-fd24-48e9-99f6-1f1a49d239bf");
     Predicate predicate =
         Predicate.builder()
@@ -31,11 +31,85 @@ class PredicateControllerTest extends BaseControllerTest {
             .uuid(uuid)
             .build();
 
+    // We save a predicate without UUID
     String jsonBody =
         "{\"objectType\":\"PREDICATE\",\"value\":\"foo\",\"label\": {" + "\"\": \"bar\"" + "}}";
 
     when(predicateService.save(any(Predicate.class))).thenReturn(predicate);
 
     testPostJson("/v6/predicates", jsonBody, "/v6/relation/predicates/predicates_response.json");
+    testPostJson("/v5/predicates", jsonBody, "/v6/relation/predicates/predicates_response.json");
+    testPostJson("/v3/predicates", jsonBody, "/v6/relation/predicates/predicates_response.json");
+    testPostJson(
+        "/latest/predicates", jsonBody, "/v6/relation/predicates/predicates_response.json");
+  }
+
+  @DisplayName(
+      "can save a predicate without UUID but with value in path (old style, where save and update were equal)")
+  @Test
+  public void saveWithValueInPath() throws Exception {
+    UUID uuid = UUID.fromString("bb5885f6-fd24-48e9-99f6-1f1a49d239bf");
+    Predicate predicate =
+        Predicate.builder()
+            .value("foo")
+            .label(new LocalizedText(Locale.ROOT, "bar"))
+            .uuid(uuid)
+            .build();
+
+    // We save a predicate without UUID
+    String jsonBody =
+        "{\"objectType\":\"PREDICATE\",\"value\":\"foo\",\"label\": {" + "\"\": \"bar\"" + "}}";
+
+    when(predicateService.save(any(Predicate.class))).thenReturn(predicate);
+
+    // Yes, it really works with put!
+    testPutJson("/v6/predicates/foo", jsonBody, "/v6/relation/predicates/predicates_response.json");
+    testPutJson("/v5/predicates/foo", jsonBody, "/v6/relation/predicates/predicates_response.json");
+    testPutJson("/v3/predicates/foo", jsonBody, "/v6/relation/predicates/predicates_response.json");
+    testPutJson(
+        "/latest/predicates/foo", jsonBody, "/v6/relation/predicates/predicates_response.json");
+  }
+
+  @DisplayName("can update a predicate by its value, without uuid (used by old clients)")
+  @Test
+  public void updateByValueWithoutUuid() throws Exception {
+    UUID uuid = UUID.fromString("bb5885f6-fd24-48e9-99f6-1f1a49d239bf");
+    Predicate predicate =
+        Predicate.builder()
+            .value("foo")
+            .label(new LocalizedText(Locale.ROOT, "bar"))
+            .uuid(uuid)
+            .build();
+
+    // We update a predicate without UUID
+    String jsonBody =
+        "{\"objectType\":\"PREDICATE\",\"value\":\"foo\",\"label\": {" + "\"\": \"bar\"" + "}}";
+    when(predicateService.save(any(Predicate.class))).thenReturn(predicate);
+
+    testPutJson("/v6/predicates/foo", jsonBody, "/v6/relation/predicates/predicates_response.json");
+  }
+
+  @DisplayName("can update a predicate by its uuid")
+  @Test
+  public void updateByUuid() throws Exception {
+    UUID uuid = UUID.fromString("bb5885f6-fd24-48e9-99f6-1f1a49d239bf");
+    Predicate predicate =
+        Predicate.builder()
+            .value("foo")
+            .label(new LocalizedText(Locale.ROOT, "bar"))
+            .uuid(uuid)
+            .build();
+
+    // We update a predicate without UUID
+    String jsonBody =
+        "{\"uuid\":\""
+            + uuid
+            + "\",\"objectType\":\"PREDICATE\",\"value\":\"foo\",\"label\": {"
+            + "\"\": \"bar\""
+            + "}}";
+    when(predicateService.save(any(Predicate.class))).thenReturn(predicate);
+
+    testPutJson(
+        "/v6/predicates/" + uuid, jsonBody, "/v6/relation/predicates/predicates_response.json");
   }
 }
