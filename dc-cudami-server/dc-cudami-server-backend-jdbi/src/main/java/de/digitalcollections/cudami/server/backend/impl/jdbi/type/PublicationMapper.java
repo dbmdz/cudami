352ab928663d697819b53bb6e1b8c6d8f74f570b
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-10-27 15:14:29 +0200

    Preparing work for new publisher modelling

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/type/PublicationMapper.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/type/PublicationMapper.java
@@ -1,14 +1,14 @@
 package de.digitalcollections.cudami.server.backend.impl.jdbi.type;
 
 import de.digitalcollections.model.identifiable.Identifiable;
-import de.digitalcollections.model.identifiable.entity.work.Publication;
+import de.digitalcollections.model.identifiable.entity.work.Publisher;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.jdbi.v3.core.array.SqlArrayType;
 
-public class PublicationMapper implements SqlArrayType<Publication> {
+public class PublicationMapper implements SqlArrayType<Publisher> {
 
   @Override
   public String getTypeName() {
@@ -16,18 +16,16 @@ public class PublicationMapper implements SqlArrayType<Publication> {
   }
 
   @Override
-  public Object convertArrayElement(Publication element) {
+  public Object convertArrayElement(Publisher element) {
     Predicate<List<?>> isNullOrEmpty = list -> list == null || list.isEmpty();
     return "("
-        + (isNullOrEmpty.test(element.getPublicationLocations())
+        + (isNullOrEmpty.test(element.getLocations())
             ? "{}"
-            : String.format("{%s}", commaSeparatedUuids(element.getPublicationLocations())))
-        + (isNullOrEmpty.test(element.getPublishers())
-            ? ",{}"
-            : String.format(",{%s}", commaSeparatedUuids(element.getPublishers())))
-        + (isNullOrEmpty.test(element.getPublishersPresentation())
-            ? ",{}"
-            : String.format(",{%s}", commaSeparatedStrings(element.getPublishersPresentation())))
+            : String.format("{%s}", commaSeparatedUuids(element.getLocations())))
+        + (element.getAgent() == null ? ",null" : String.format(",%s", element.getAgent()))
+        + (element.getPublisherPresentation() == null
+            ? ",null"
+            : String.format(",%s", element.getPublisherPresentation()))
         + ")";
   }
 
