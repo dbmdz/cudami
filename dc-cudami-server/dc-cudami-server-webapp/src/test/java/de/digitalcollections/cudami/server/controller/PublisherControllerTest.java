a2030c4d443f67da0f4c6c7a603dcc466ec7772c
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-11-02 15:52:33 +0100

    Implemented equality / contains filtering of a list

--- dc-cudami-server/dc-cudami-server-webapp/src/test/java/de/digitalcollections/cudami/server/controller/PublisherControllerTest.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/test/java/de/digitalcollections/cudami/server/controller/PublisherControllerTest.java
@@ -1,17 +1,23 @@
 package de.digitalcollections.cudami.server.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import de.digitalcollections.cudami.server.business.api.service.PublisherService;
 import de.digitalcollections.model.identifiable.entity.work.Publisher;
+import de.digitalcollections.model.list.filtering.FilterCriterion;
+import de.digitalcollections.model.list.filtering.Filtering;
 import de.digitalcollections.model.list.paging.PageRequest;
 import de.digitalcollections.model.list.paging.PageResponse;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -106,9 +112,9 @@ class PublisherControllerTest extends BaseControllerTest {
     testDeleteSuccessful("/v6/publishers/12345678-1234-1234-1234-123456789012");
   }
 
-  @DisplayName("can find publishers")
+  @DisplayName("can page publishers")
   @Test
-  public void find() throws Exception {
+  public void page() throws Exception {
     UUID uuid = UUID.fromString("12345678-1234-1234-1234-123456789012");
     Publisher publisher = Publisher.builder().publisherPresentation("foo").uuid(uuid).build();
 
@@ -121,5 +127,89 @@ class PublisherControllerTest extends BaseControllerTest {
     when(publisherService.find(any(PageRequest.class))).thenReturn(pageResponse);
 
     testJson("/v6/publishers?pageSize=1&pageNumber=0", "/v6/publishers/list.json");
+  }
+
+  @DisplayName("can filter publishers with agent and one location")
+  @Test
+  public void filterOneAgentOneLocation() throws Exception {
+    UUID agentUuid = UUID.fromString("23456789-2345-2345-2345-234567890123");
+    UUID locationUuid = UUID.randomUUID();
+
+    PageRequest expectedPageRequest =
+        PageRequest.builder()
+            .pageNumber(0)
+            .pageSize(1)
+            .filtering(
+                Filtering.builder()
+                    .add(
+                        FilterCriterion.builder()
+                            .withExpression("agent_uuid")
+                            .isEquals(agentUuid)
+                            .build())
+                    .add(
+                        FilterCriterion.builder()
+                            .withExpression("location_uuid")
+                            .isEquals(locationUuid)
+                            .build())
+                    .build())
+            .build();
+
+    testHttpGetWithExpectedStatus(
+        "/v6/publishers?pageSize=1&pageNumber=0&agent_uuid=eq:"
+            + agentUuid
+            + "&location_uuid=eq:"
+            + locationUuid,
+        200);
+    ArgumentCaptor<PageRequest> pageRequestArgumentCaptor =
+        ArgumentCaptor.forClass(PageRequest.class);
+    verify(publisherService, times(1)).find(pageRequestArgumentCaptor.capture());
+
+    assertThat(pageRequestArgumentCaptor.getValue()).isEqualTo(expectedPageRequest);
+  }
+
+  @DisplayName("can filter publishers with agent and multiple locations")
+  @Test
+  public void filterOneAgentMultipleLocations() throws Exception {
+    UUID agentUuid = UUID.fromString("23456789-2345-2345-2345-234567890123");
+    UUID locationUuid1 = UUID.randomUUID();
+    UUID locationUuid2 = UUID.randomUUID();
+
+    PageRequest expectedPageRequest =
+        PageRequest.builder()
+            .pageNumber(0)
+            .pageSize(1)
+            .filtering(
+                Filtering.builder()
+                    .add(
+                        FilterCriterion.builder()
+                            .withExpression("agent_uuid")
+                            .isEquals(agentUuid)
+                            .build())
+                    .add(
+                        FilterCriterion.builder()
+                            .withExpression("location_uuid")
+                            .isEquals(locationUuid1)
+                            .build())
+                    .add(
+                        FilterCriterion.builder()
+                            .withExpression("location_uuid")
+                            .isEquals(locationUuid2)
+                            .build())
+                    .build())
+            .build();
+
+    testHttpGetWithExpectedStatus(
+        "/v6/publishers?pageSize=1&pageNumber=0&agent_uuid=eq:"
+            + agentUuid
+            + "&location_uuid=eq:"
+            + locationUuid1
+            + "&location_uuid=eq:"
+            + locationUuid2,
+        200);
+    ArgumentCaptor<PageRequest> pageRequestArgumentCaptor =
+        ArgumentCaptor.forClass(PageRequest.class);
+    verify(publisherService, times(1)).find(pageRequestArgumentCaptor.capture());
+
+    assertThat(pageRequestArgumentCaptor.getValue()).isEqualTo(expectedPageRequest);
   }
 }
