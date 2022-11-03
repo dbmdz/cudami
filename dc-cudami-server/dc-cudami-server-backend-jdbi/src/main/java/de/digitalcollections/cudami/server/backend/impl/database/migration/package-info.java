7c20e41933845731270ad09a32370d8191054a21
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-11-03 07:42:04 +0100

    Fix javadoc error

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/database/migration/package-info.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/database/migration/package-info.java
@@ -1,8 +1,6 @@
-@SuppressFBWarnings(
+@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(
     value = {
       "THROWS_METHOD_THROWS_CLAUSE_BASIC_EXCEPTION",
       "THROWS_METHOD_THROWS_RUNTIMEEXCEPTION"
     })
 package de.digitalcollections.cudami.server.backend.impl.database.migration;
-
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
--- dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/package-info.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/package-info.java
@@ -1,4 +1,2 @@
-@SuppressFBWarnings(value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
+@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = {"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
 package de.digitalcollections.cudami.server.controller;
-
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
