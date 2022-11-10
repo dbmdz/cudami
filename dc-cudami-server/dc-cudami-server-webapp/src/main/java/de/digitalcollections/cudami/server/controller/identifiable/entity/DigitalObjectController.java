51d8d1c2588c313ad996bec9a101558cc0dc249a
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-11-10 15:34:10 +0100

    Add missing import

--- dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/identifiable/entity/DigitalObjectController.java
+++ dc-cudami-server/dc-cudami-server-webapp/src/main/java/de/digitalcollections/cudami/server/controller/identifiable/entity/DigitalObjectController.java
@@ -24,6 +24,7 @@ import java.util.Locale;
 import java.util.Objects;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
