b2d6ed29647ca384294bbf122bdb1c30e118dfb0
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-11-11 10:35:34 +0100

    Add "NotBlank" validation and error messages for label field

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/business/impl/validator/LabelNotBlankValidator.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/business/impl/validator/LabelNotBlankValidator.java
@@ -1,6 +1,9 @@
 package de.digitalcollections.cudami.admin.business.impl.validator;
 
 import de.digitalcollections.model.text.LocalizedText;
+import java.util.Locale;
+import java.util.Map;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -16,11 +19,24 @@ public class LabelNotBlankValidator implements Validator {
   @Override
   public void validate(Object target, Errors errors) {
     LocalizedText localizedText = (LocalizedText) target;
-    if (localizedText == null
-        || localizedText.isEmpty()
-        || localizedText.values().stream().allMatch(t -> t == null || t.trim().isBlank())) {
-      // error message key: see hibernate-validator library
-      errors.rejectValue("label", "javax.validation.constraints.NotBlank.message");
+    if (localizedText == null || localizedText.isEmpty()) {
+      errors.reject("label", "validation.label.NotBlank");
+      return;
+    }
+    Locale displayLocale = LocaleContextHolder.getLocale();
+    for (Map.Entry<Locale, String> entry : localizedText.entrySet()) {
+      Locale locale = entry.getKey();
+      String text = entry.getValue();
+      if (text == null || text.isBlank()) {
+        errors.reject(
+            "validation.label.language.NotBlank",
+            new String[] {locale.getDisplayLanguage(displayLocale)},
+            "validation.label.NotBlank");
+        // error message key: see hibernate-validator library:
+        errors.rejectValue(
+            "label['" + locale.getLanguage() + "']",
+            "javax.validation.constraints.NotBlank.message");
+      }
     }
   }
 }
