83a70396b69940b710d2895dbe17c3c4b51bb9df
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-11-11 14:47:41 +0100

    TipTap editor: set initial content to preserve content e.g. after validation error roundtrip to server

--- dc-cudami-admin/src/main/resources/static/js/tiptap/tiptap-customizations.js
+++ dc-cudami-admin/src/main/resources/static/js/tiptap/tiptap-customizations.js
@@ -5,85 +5,87 @@ import Subscript from '@tiptap/extension-subscript'
 import Superscript from '@tiptap/extension-superscript'
 import Underline from '@tiptap/extension-underline'
 
-const CustomLink = Link.extend({
-  addAttributes() {
-    return {
-      ...this.parent?.(),
-      title: {
-        default: null
-      }
-    };
-  }
-});
+        const CustomLink = Link.extend({
+          addAttributes() {
+            return {
+              ...this.parent?.(),
+              title: {
+                default: null
+              }
+            };
+          }
+        });
 
-class MiniEditor extends Editor {
-  constructor(idEditorElement, idEditorMenu, idJsonField) {
-    super({
-      element: document.getElementById(idEditorElement),
-      extensions: [
-        CustomLink.configure({
-          openOnClick: false
-        }),
-        StarterKit,
-        Subscript,
-        Superscript,
-        Underline
-      ],
-      autofocus: true,
-      editable: true,
-      injectCSS: true,
-      onCreate( { editor }) {
-        // The editor is ready.
-        editor.schema.nodes.bulletList.name = 'bullet_list';
-        editor.schema.nodes.listItem.name = 'list_item';
-        editor.schema.nodes.orderedList.name = 'ordered_list';
-      },
-      onFocus( { editor, event }) {
-        // The editor is focused.
-        editor.updateMenuButtons(editor);
-      },
-      onSelectionUpdate( { editor }) {
-        // The selection has changed.
-        editor.updateMenuButtons(editor);
-      },
-      onUpdate( { editor }) {
-        // The content has changed.
-        editor.updateMenuButtons(editor);
-        var json = JSON.stringify(editor.getJSON());
-        document.getElementById(idJsonField).value = json;
-      }
-    });
-    this.idEditorMenu = idEditorMenu;
-  };
+        class MiniEditor extends Editor {
+          constructor(idEditorElement, idEditorMenu, idJsonField) {
+            super({
+              element: document.getElementById(idEditorElement),
+              extensions: [
+                CustomLink.configure({
+                  openOnClick: false
+                }),
+                StarterKit,
+                Subscript,
+                Superscript,
+                Underline
+              ],
+              autofocus: true,
+              editable: true,
+              injectCSS: true,
+              onCreate( { editor }) {
+                // The editor is ready.
+                editor.schema.nodes.bulletList.name = 'bullet_list';
+                editor.schema.nodes.listItem.name = 'list_item';
+                editor.schema.nodes.orderedList.name = 'ordered_list';
 
-  updateMenuButtons(editor) {
-    let editorMenu = document.getElementById(this.idEditorMenu);
-    var matches = editorMenu.querySelectorAll("*[data-value]");
-    for (let i = 0; i < matches.length; i++) {
-      var menuItem = matches[i];
-      var value = menuItem.getAttribute("data-value");
-      if (value === "undo") {
-        if (editor.can().undo()) {
-          menuItem.removeAttribute('disabled');
-        } else {
-          menuItem.setAttribute('disabled', true);
-        }
-      } else if (value === "redo") {
-        if (editor.can().redo()) {
-          menuItem.removeAttribute('disabled');
-        } else {
-          menuItem.setAttribute('disabled', true);
-        }
-      } else {
-        if (editor.isActive(value)) {
-          menuItem.classList.add("is-active");
-        } else {
-          menuItem.classList.remove("is-active");
-        }
-      }
-    }
-  }
-};
+                editor.commands.setContent(JSON.parse(document.getElementById(idJsonField).value));
+              },
+              onFocus( { editor, event }) {
+                // The editor is focused.
+                editor.updateMenuButtons(editor);
+              },
+              onSelectionUpdate( { editor }) {
+                // The selection has changed.
+                editor.updateMenuButtons(editor);
+              },
+              onUpdate( { editor }) {
+                // The content has changed.
+                editor.updateMenuButtons(editor);
+                var json = JSON.stringify(editor.getJSON());
+                document.getElementById(idJsonField).value = json;
+              }
+            });
+            this.idEditorMenu = idEditorMenu;
+          }
 
-export { CustomLink };
-export { MiniEditor };
+          updateMenuButtons(editor) {
+            let editorMenu = document.getElementById(this.idEditorMenu);
+            var matches = editorMenu.querySelectorAll("*[data-value]");
+            for (let i = 0; i < matches.length; i++) {
+              var menuItem = matches[i];
+              var value = menuItem.getAttribute("data-value");
+              if (value === "undo") {
+                if (editor.can().undo()) {
+                  menuItem.removeAttribute('disabled');
+                } else {
+                  menuItem.setAttribute('disabled', true);
+                }
+              } else if (value === "redo") {
+                if (editor.can().redo()) {
+                  menuItem.removeAttribute('disabled');
+                } else {
+                  menuItem.setAttribute('disabled', true);
+                }
+              } else {
+                if (editor.isActive(value)) {
+                  menuItem.classList.add("is-active");
+                } else {
+                  menuItem.classList.remove("is-active");
+                }
+              }
+            }
+          }
+        }
+
+        export { CustomLink };
+        export { MiniEditor };
