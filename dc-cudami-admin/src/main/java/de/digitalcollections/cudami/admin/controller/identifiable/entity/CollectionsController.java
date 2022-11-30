44ae2a27b4b8a3a2ebfeebbcc604a9567949b41c
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-11-30 17:35:11 +0100

    Add collection table add show search clear button, search on enter and search highlighting add renderDateTime

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/CollectionsController.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/CollectionsController.java
@@ -8,11 +8,6 @@ import de.digitalcollections.cudami.client.identifiable.entity.CudamiCollections
 import de.digitalcollections.model.exception.ResourceNotFoundException;
 import de.digitalcollections.model.exception.TechnicalException;
 import de.digitalcollections.model.identifiable.entity.Collection;
-import de.digitalcollections.model.identifiable.entity.digitalobject.DigitalObject;
-import de.digitalcollections.model.list.paging.PageRequest;
-import de.digitalcollections.model.list.paging.PageResponse;
-import de.digitalcollections.model.list.sorting.Order;
-import de.digitalcollections.model.list.sorting.Sorting;
 import de.digitalcollections.model.view.BreadcrumbNavigation;
 import de.digitalcollections.model.view.BreadcrumbNode;
 import java.util.List;
@@ -22,19 +17,12 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /** Controller for collection management pages. */
 @Controller
@@ -52,39 +40,6 @@ public class CollectionsController extends AbstractController {
     this.service = client.forCollections();
   }
 
-  @PostMapping("/api/collections/{uuid}/digitalobjects")
-  public ResponseEntity addDigitalObjects(
-      @PathVariable UUID uuid, @RequestBody List<DigitalObject> digitalObjects)
-      throws TechnicalException {
-    boolean successful = service.addDigitalObjects(uuid, digitalObjects);
-    if (successful) {
-      return new ResponseEntity<>(successful, HttpStatus.OK);
-    }
-    return new ResponseEntity<>(successful, HttpStatus.NOT_FOUND);
-  }
-
-  @PostMapping("/api/collections/{collectionUuid}/collections/{subcollectionUuid}")
-  public ResponseEntity addSubcollection(
-      @PathVariable UUID collectionUuid, @PathVariable UUID subcollectionUuid)
-      throws TechnicalException {
-    boolean successful = service.addSubcollection(collectionUuid, subcollectionUuid);
-    if (successful) {
-      return new ResponseEntity<>(successful, HttpStatus.OK);
-    }
-    return new ResponseEntity<>(successful, HttpStatus.NOT_FOUND);
-  }
-
-  @PostMapping("/api/collections/{collectionUuid}/collections")
-  public ResponseEntity addSubcollections(
-      @PathVariable UUID collectionUuid, @RequestBody List<Collection> subcollections)
-      throws TechnicalException {
-    boolean successful = service.addSubcollections(collectionUuid, subcollections);
-    if (successful) {
-      return new ResponseEntity<>(successful, HttpStatus.OK);
-    }
-    return new ResponseEntity<>(successful, HttpStatus.NOT_FOUND);
-  }
-
   @GetMapping("/collections/new")
   public String create(
       Model model,
@@ -95,12 +50,6 @@ public class CollectionsController extends AbstractController {
     model.addAttribute("parentType", parentType);
     model.addAttribute("parentUuid", parentUuid);
     return "collections/create";
-  }
-
-  @GetMapping("/api/collections/new")
-  @ResponseBody
-  public Collection create() throws TechnicalException {
-    return service.create();
   }
 
   @GetMapping("/collections/{uuid}/edit")
@@ -125,82 +74,6 @@ public class CollectionsController extends AbstractController {
     return "collections/edit";
   }
 
-  @GetMapping("/api/collections/search")
-  @ResponseBody
-  public PageResponse<Collection> find(
-      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
-      @RequestParam(name = "pageSize", required = false, defaultValue = "5") int pageSize,
-      @RequestParam(name = "searchTerm", required = false) String searchTerm,
-      @RequestParam(name = "sortBy", required = false) List<Order> sortBy)
-      throws TechnicalException {
-    PageRequest pageRequest = new PageRequest(searchTerm, pageNumber, pageSize);
-    if (sortBy != null) {
-      Sorting sorting = new Sorting(sortBy);
-      pageRequest.setSorting(sorting);
-    }
-    return service.find(pageRequest);
-  }
-
-  @GetMapping("/api/collections/{uuid}/digitalobjects")
-  @ResponseBody
-  public PageResponse<DigitalObject> findDigitalObjects(
-      @PathVariable UUID uuid,
-      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
-      @RequestParam(name = "pageSize", required = false, defaultValue = "25") int pageSize,
-      @RequestParam(name = "searchTerm", required = false) String searchTerm)
-      throws TechnicalException {
-    PageRequest pageRequest = new PageRequest(searchTerm, pageNumber, pageSize);
-    return service.findDigitalObjects(uuid, pageRequest);
-  }
-
-  @GetMapping("/api/collections/{uuid}/collections")
-  @ResponseBody
-  public PageResponse<Collection> findSubcollections(
-      @PathVariable UUID uuid,
-      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
-      @RequestParam(name = "pageSize", required = false, defaultValue = "25") int pageSize,
-      @RequestParam(name = "searchTerm", required = false) String searchTerm)
-      throws TechnicalException {
-    PageRequest pageRequest = new PageRequest(searchTerm, pageNumber, pageSize);
-    return service.findSubcollections(uuid, pageRequest);
-  }
-
-  @GetMapping("/api/collections")
-  @ResponseBody
-  public PageResponse<Collection> findTop(
-      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
-      @RequestParam(name = "pageSize", required = false, defaultValue = "25") int pageSize,
-      @RequestParam(name = "searchTerm", required = false) String searchTerm,
-      @RequestParam(name = "sortBy", required = false) List<Order> sortBy)
-      throws TechnicalException {
-    PageRequest pageRequest = new PageRequest(searchTerm, pageNumber, pageSize);
-    if (sortBy != null) {
-      Sorting sorting = new Sorting(sortBy);
-      pageRequest.setSorting(sorting);
-    }
-    return service.findTopCollections(pageRequest);
-  }
-
-  @GetMapping("/api/collections/identifier/{namespace}:{id}")
-  @ResponseBody
-  public Collection getByIdentifier(@PathVariable String namespace, @PathVariable String id)
-      throws TechnicalException {
-    return service.getByIdentifier(namespace, id);
-  }
-
-  @GetMapping("/api/collections/{refId:[0-9]+}")
-  @ResponseBody
-  public Collection getByRefId(@PathVariable long refId) throws TechnicalException {
-    return service.getByRefId(refId);
-  }
-
-  @GetMapping(
-      "/api/collections/{uuid:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}")
-  @ResponseBody
-  public Collection getByUuid(@PathVariable UUID uuid) throws TechnicalException {
-    return service.getByUuid(uuid);
-  }
-
   @GetMapping("/collections")
   public String list(Model model) throws TechnicalException {
     final Locale displayLocale = LocaleContextHolder.getLocale();
@@ -213,59 +86,6 @@ public class CollectionsController extends AbstractController {
   @ModelAttribute("menu")
   protected String module() {
     return "collections";
-  }
-
-  @DeleteMapping("/api/collections/{collectionUuid}/digitalobjects/{digitalobjectUuid}")
-  @ResponseBody
-  public ResponseEntity removeDigitalObject(
-      @PathVariable UUID collectionUuid, @PathVariable UUID digitalobjectUuid)
-      throws TechnicalException {
-    boolean successful = service.removeDigitalObject(collectionUuid, digitalobjectUuid);
-    if (successful) {
-      return new ResponseEntity<>(successful, HttpStatus.OK);
-    }
-    return new ResponseEntity<>(successful, HttpStatus.NOT_FOUND);
-  }
-
-  @DeleteMapping("/api/collections/{collectionUuid}/collections/{subcollectionUuid}")
-  public ResponseEntity removeSubcollection(
-      @PathVariable UUID collectionUuid, @PathVariable UUID subcollectionUuid)
-      throws TechnicalException {
-    boolean successful = service.removeSubcollection(collectionUuid, subcollectionUuid);
-    if (successful) {
-      return new ResponseEntity<>(successful, HttpStatus.OK);
-    }
-    return new ResponseEntity<>(successful, HttpStatus.NOT_FOUND);
-  }
-
-  @PostMapping("/api/collections")
-  public ResponseEntity save(
-      @RequestBody Collection collection,
-      @RequestParam(name = "parentType", required = false) String parentType,
-      @RequestParam(name = "parentUuid", required = false) UUID parentUuid) {
-    try {
-      Collection collectionDb = null;
-      if ("collection".equals(parentType)) {
-        collectionDb = service.saveWithParentCollection(collection, parentUuid);
-      } else {
-        collectionDb = service.save(collection);
-      }
-      return ResponseEntity.status(HttpStatus.CREATED).body(collectionDb);
-    } catch (TechnicalException e) {
-      LOGGER.error("Cannot save collection: ", e);
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-    }
-  }
-
-  @PutMapping("/api/collections/{uuid}")
-  public ResponseEntity update(@PathVariable UUID uuid, @RequestBody Collection collection) {
-    try {
-      Collection collectionDb = service.update(uuid, collection);
-      return ResponseEntity.ok(collectionDb);
-    } catch (TechnicalException e) {
-      LOGGER.error("Cannot save collection with uuid={}", uuid, e);
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-    }
   }
 
   @GetMapping(
