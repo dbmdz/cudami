ae691c95a6db4488eaead00aced84e8de4e43b58
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-12-07 16:16:24 +0100

    Refactor table for projects

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/ProjectsAPIController.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/ProjectsAPIController.java
@@ -1,6 +1,8 @@
 package de.digitalcollections.cudami.admin.controller.identifiable.entity;
 
+import de.digitalcollections.cudami.admin.business.api.service.exceptions.ServiceException;
 import de.digitalcollections.cudami.admin.controller.AbstractPagingAndSortingController;
+import de.digitalcollections.cudami.admin.model.bootstraptable.BTResponse;
 import de.digitalcollections.cudami.admin.util.LanguageSortingHelper;
 import de.digitalcollections.cudami.client.CudamiClient;
 import de.digitalcollections.cudami.client.CudamiLocalesClient;
@@ -10,8 +12,7 @@ import de.digitalcollections.model.identifiable.entity.Project;
 import de.digitalcollections.model.identifiable.entity.digitalobject.DigitalObject;
 import de.digitalcollections.model.list.paging.PageRequest;
 import de.digitalcollections.model.list.paging.PageResponse;
-import de.digitalcollections.model.list.sorting.Order;
-import de.digitalcollections.model.list.sorting.Sorting;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.List;
 import java.util.UUID;
 import org.slf4j.Logger;
@@ -61,20 +62,20 @@ public class ProjectsAPIController extends AbstractPagingAndSortingController<Pr
     return service.create();
   }
 
+  @SuppressFBWarnings
   @GetMapping("/api/projects")
   @ResponseBody
-  public PageResponse<Project> find(
-      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
-      @RequestParam(name = "pageSize", required = false, defaultValue = "25") int pageSize,
-      @RequestParam(name = "searchTerm", required = false) String searchTerm,
-      @RequestParam(name = "sortBy", required = false) List<Order> sortBy)
-      throws TechnicalException {
-    PageRequest pageRequest = new PageRequest(searchTerm, pageNumber, pageSize);
-    if (sortBy != null) {
-      Sorting sorting = new Sorting(sortBy);
-      pageRequest.setSorting(sorting);
-    }
-    return service.find(pageRequest);
+  public BTResponse<Project> find(
+      @RequestParam(name = "offset", required = false, defaultValue = "0") int offset,
+      @RequestParam(name = "limit", required = false, defaultValue = "1") int limit,
+      @RequestParam(name = "search", required = false) String searchTerm,
+      @RequestParam(name = "sort", required = false, defaultValue = "url") String sort,
+      @RequestParam(name = "order", required = false, defaultValue = "asc") String order,
+      @RequestParam(name = "itemLocale", required = false) String itemLocale)
+      throws TechnicalException, ServiceException {
+    PageResponse<Project> pageResponse =
+        super.find(localeService, service, offset, limit, searchTerm, sort, order, itemLocale);
+    return new BTResponse<>(pageResponse);
   }
 
   @GetMapping("/api/projects/{uuid}")
