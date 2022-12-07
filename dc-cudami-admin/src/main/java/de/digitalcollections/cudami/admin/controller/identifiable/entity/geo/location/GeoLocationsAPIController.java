3e57e27f0cbd30921bd0a3a4f4a32d64b403d90d
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-12-07 16:48:49 +0100

    Refactor table for geolocations

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/geo/location/GeoLocationsAPIController.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/entity/geo/location/GeoLocationsAPIController.java
@@ -1,16 +1,16 @@
 package de.digitalcollections.cudami.admin.controller.identifiable.entity.geo.location;
 
-import de.digitalcollections.commons.springmvc.controller.AbstractController;
+import de.digitalcollections.cudami.admin.business.api.service.exceptions.ServiceException;
+import de.digitalcollections.cudami.admin.controller.AbstractPagingAndSortingController;
+import de.digitalcollections.cudami.admin.model.bootstraptable.BTResponse;
 import de.digitalcollections.cudami.admin.util.LanguageSortingHelper;
 import de.digitalcollections.cudami.client.CudamiClient;
+import de.digitalcollections.cudami.client.CudamiLocalesClient;
 import de.digitalcollections.cudami.client.identifiable.entity.geo.location.CudamiGeoLocationsClient;
 import de.digitalcollections.model.exception.TechnicalException;
 import de.digitalcollections.model.identifiable.entity.geo.location.GeoLocation;
-import de.digitalcollections.model.list.paging.PageRequest;
 import de.digitalcollections.model.list.paging.PageResponse;
-import de.digitalcollections.model.list.sorting.Order;
-import de.digitalcollections.model.list.sorting.Sorting;
-import java.util.List;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,16 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 /** Controller for all public "GeoLocations" endpoints (API). */
 @RestController
-public class GeoLocationsAPIController extends AbstractController {
+public class GeoLocationsAPIController extends AbstractPagingAndSortingController<GeoLocation> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(GeoLocationsAPIController.class);
 
   private final LanguageSortingHelper languageSortingHelper;
+  private final CudamiLocalesClient localeService;
   private final CudamiGeoLocationsClient service;
 
   public GeoLocationsAPIController(
       LanguageSortingHelper languageSortingHelper, CudamiClient client) {
     this.languageSortingHelper = languageSortingHelper;
+    this.localeService = client.forLocales();
     this.service = client.forGeoLocations();
   }
 
@@ -39,19 +41,19 @@ public class GeoLocationsAPIController extends AbstractController {
     return service.create();
   }
 
+  @SuppressFBWarnings
   @GetMapping("/api/geolocations")
   @ResponseBody
-  public PageResponse<GeoLocation> find(
-      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
-      @RequestParam(name = "pageSize", required = false, defaultValue = "25") int pageSize,
-      @RequestParam(name = "searchTerm", required = false) String searchTerm,
-      @RequestParam(name = "sortBy", required = false) List<Order> sortBy)
-      throws TechnicalException {
-    PageRequest pageRequest = new PageRequest(searchTerm, pageNumber, pageSize);
-    if (sortBy != null) {
-      Sorting sorting = new Sorting(sortBy);
-      pageRequest.setSorting(sorting);
-    }
-    return service.find(pageRequest);
+  public BTResponse<GeoLocation> find(
+      @RequestParam(name = "offset", required = false, defaultValue = "0") int offset,
+      @RequestParam(name = "limit", required = false, defaultValue = "1") int limit,
+      @RequestParam(name = "search", required = false) String searchTerm,
+      @RequestParam(name = "sort", required = false, defaultValue = "url") String sort,
+      @RequestParam(name = "order", required = false, defaultValue = "asc") String order,
+      @RequestParam(name = "itemLocale", required = false) String itemLocale)
+      throws TechnicalException, ServiceException {
+    PageResponse<GeoLocation> pageResponse =
+        super.find(localeService, service, offset, limit, searchTerm, sort, order, itemLocale);
+    return new BTResponse<>(pageResponse);
   }
 }
