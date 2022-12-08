82429665891f9bf75782e582a974696013f1bedd
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-12-08 15:07:43 +0100

    Replace `var` types with explicit types

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/test/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/entity/WebsiteRepositoryImplTest.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/test/java/de/digitalcollections/cudami/server/backend/impl/jdbi/identifiable/entity/WebsiteRepositoryImplTest.java
@@ -76,14 +76,14 @@ class WebsiteRepositoryImplTest {
   @Test
   @DisplayName("save a website with notes")
   void saveWebsiteWithNotes() {
-    var noteContent1 = new StructuredContent();
+    StructuredContent noteContent1 = new StructuredContent();
     noteContent1.addContentBlock(new Text("eine Bemerkung"));
-    var note1 = new LocalizedStructuredContent();
+    LocalizedStructuredContent note1 = new LocalizedStructuredContent();
     note1.put(Locale.GERMAN, noteContent1);
 
-    var noteContent2 = new StructuredContent();
+    StructuredContent noteContent2 = new StructuredContent();
     noteContent2.addContentBlock(new Text("zweite Bemerkung"));
-    var note2 = new LocalizedStructuredContent();
+    LocalizedStructuredContent note2 = new LocalizedStructuredContent();
     note2.put(Locale.GERMAN, noteContent2);
     Website website =
         Website.builder()
