779996eece9f8e532ca3cf2f5a6fbe4749ee6254
David Förster <david.foerster@bsb-muenchen.de>
2022-12-16 17:51:20 +0100

    Extend function in V13.10.00 migration
    Fix all locales

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V13.10.00__DML_replace_empty_label_language_by_undetermined.sql
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V13.10.00__DML_replace_empty_label_language_by_undetermined.sql
@@ -1,9 +1,9 @@
-/*
+/**
  * This can be a quite long running script due to updating the table identifiables.
  * Therefore it should already be run manually before being executed by flyway.
  *
  * Use `psql -1 -f this_file.sql ...` to run this script transactionally.
-*/
+ */
 
 CREATE OR REPLACE FUNCTION replace_language (jsonb) RETURNS jsonb
 RETURNS NULL ON NULL INPUT
@@ -12,18 +12,37 @@ PARALLEL SAFE
 LANGUAGE plpgsql AS
 $body$
 DECLARE
-  new_json jsonb;
+  new_json jsonb := '{}';
+  key text;
+  json_entry jsonb;
+  new_key text;
 BEGIN
-  IF NOT $1 ? '' THEN
-    RETURN $1;
-  END IF;
-  new_json := $1 || jsonb_build_object('und', ($1 ->> '') || (CASE WHEN $1 ->> 'und' IS NULL THEN '' ELSE ' ' || ($1 ->> 'und') END));
-  new_json := new_json - '';
+  FOR key IN select jsonb_object_keys($1) LOOP
+    -- check the key and make changes where necessary
+    IF key IN ('', 'und') THEN
+      -- only once
+      CONTINUE WHEN new_json ? 'und';
+      /* being carful may lead to unwanted results
+         anyhow it is better than losing data */
+      json_entry := jsonb_build_object('und',
+        trim(both from CASE WHEN $1 -> '' IS NOT NULL THEN $1 ->> '' ELSE '' END
+          || CASE WHEN $1 -> 'und' IS NOT NULL THEN ' ' || ($1 ->> 'und') ELSE '' END));
+    ELSIF key ~* '[_#]' THEN
+      new_key := regexp_replace(key, '[_#]+', '-', 'ig');
+      -- de_DE_#Latn would now be de-DE-Latn. Sadly we need de-Latn-DE though
+      new_key := regexp_replace(new_key, '^(\w+)-(\w+)-(\w+)$', '\1-\3-\2');
+      json_entry := jsonb_build_object(new_key, $1 -> key);
+    ELSE
+      -- take this entry as is
+      json_entry := jsonb_build_object(key, $1 -> key);
+    END IF;
+    -- add the new part
+    new_json := new_json || json_entry;
+  END LOOP;
   RETURN new_json;
 END;
 $body$;
 
-UPDATE identifiables SET "label" = replace_language("label") WHERE "label" ? '';
 
-DROP FUNCTION replace_language;
+UPDATE identifiables SET "label" = replace_language("label") WHERE "label" ? '';
 
