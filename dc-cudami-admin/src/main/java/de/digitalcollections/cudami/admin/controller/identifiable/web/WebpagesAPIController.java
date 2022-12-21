c50d791d190cc601f037d14277afbebb849aa6a0
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-12-21 08:31:37 +0100

    Change annotation from Controller to RestController

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/web/WebpagesAPIController.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/web/WebpagesAPIController.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,9 +24,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /** Controller for webpage management pages. */
-@Controller
+@RestController
 public class WebpagesAPIController extends AbstractPagingAndSortingController<Webpage> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(WebpagesAPIController.class);
