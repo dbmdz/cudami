40e05f79e1c4fa35e001cd47533c3f033e3f601e
datazuul <ralf.eichinger@gmail.com>
2023-01-10 22:22:29 +0100

    Add special filtering handling for label and name fields configurable

--- dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/CudamiRestClient.java
+++ dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/CudamiRestClient.java
@@ -23,6 +23,7 @@ public class CudamiRestClient<T extends UniqueObject> extends BaseRestClient<T> 
 
   public static final String API_VERSION_PREFIX = "/v6";
   private static final Logger LOGGER = LoggerFactory.getLogger(CudamiRestClient.class);
+  private final boolean specialHandlingForLabelAndNameFiltering;
 
   public CudamiRestClient(
       HttpClient http,
@@ -30,7 +31,18 @@ public class CudamiRestClient<T extends UniqueObject> extends BaseRestClient<T> 
       Class<T> targetType,
       ObjectMapper mapper,
       String baseEndpoint) {
+    this(http, serverUrl, targetType, mapper, baseEndpoint, true);
+  }
+
+  public CudamiRestClient(
+      HttpClient http,
+      String serverUrl,
+      Class<T> targetType,
+      ObjectMapper mapper,
+      String baseEndpoint,
+      boolean specialHandlingForLabelAndNameFiltering) {
     super(http, serverUrl, targetType, mapper, baseEndpoint);
+    this.specialHandlingForLabelAndNameFiltering = specialHandlingForLabelAndNameFiltering;
   }
 
   public long count() throws TechnicalException {
@@ -71,34 +83,36 @@ public class CudamiRestClient<T extends UniqueObject> extends BaseRestClient<T> 
 
   @Override
   protected String filterCriterionToUrlParam(FilterCriterion filterCriterion) {
-    Matcher labelOrName = Pattern.compile("^(label|name)").matcher(filterCriterion.getExpression());
-    if (labelOrName.find()) {
-      if (filterCriterion.getValue() == null || !(filterCriterion.getValue() instanceof String)) {
-        return "";
+    if (specialHandlingForLabelAndNameFiltering) {
+      Matcher labelOrName =
+          Pattern.compile("^(label|name)").matcher(filterCriterion.getExpression());
+      if (labelOrName.find()) {
+        if (filterCriterion.getValue() == null || !(filterCriterion.getValue() instanceof String)) {
+          return "";
+        }
+        String value = (String) filterCriterion.getValue();
+        if (filterCriterion.getOperation() == FilterOperation.EQUALS) {
+          value = String.format("\"%s\"", value);
+        } else if (filterCriterion.getOperation() != FilterOperation.CONTAINS) {
+          throw new UnsupportedOperationException(
+              "`label` and `name` can only be filtered by using CONTAINS (should be preferred) or EQUALS!");
+        }
+        String urlParams =
+            String.format(
+                "%s=%s", labelOrName.group(1), URLEncoder.encode(value, StandardCharsets.UTF_8));
+        Matcher matchLanguage =
+            Pattern.compile("\\.([\\w_-]+)$").matcher(filterCriterion.getExpression());
+        if (matchLanguage.find()) {
+          // there is a language defined
+          return urlParams
+              + String.format(
+                  "&%sLanguage=%s",
+                  labelOrName.group(1),
+                  URLEncoder.encode(matchLanguage.group(1), StandardCharsets.UTF_8));
+        }
+        return urlParams;
       }
-      String value = (String) filterCriterion.getValue();
-      if (filterCriterion.getOperation() == FilterOperation.EQUALS) {
-        value = String.format("\"%s\"", value);
-      } else if (filterCriterion.getOperation() != FilterOperation.CONTAINS) {
-        throw new UnsupportedOperationException(
-            "`label` and `name` can only be filtered by using CONTAINS (should be preferred) or EQUALS!");
-      }
-      String urlParams =
-          String.format(
-              "%s=%s", labelOrName.group(1), URLEncoder.encode(value, StandardCharsets.UTF_8));
-      Matcher matchLanguage =
-          Pattern.compile("\\.([\\w_-]+)$").matcher(filterCriterion.getExpression());
-      if (matchLanguage.find()) {
-        // there is a language defined
-        return urlParams
-            + String.format(
-                "&%sLanguage=%s",
-                labelOrName.group(1),
-                URLEncoder.encode(matchLanguage.group(1), StandardCharsets.UTF_8));
-      }
-      return urlParams;
     }
-
     return super.filterCriterionToUrlParam(filterCriterion);
   }
 }
