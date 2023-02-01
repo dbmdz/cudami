819d4605e356d8e569824cf9468ffc6b4bda0037
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2023-02-01 16:20:11 +0100

    Remove obsolete series

--- dc-cudami-server/dc-cudami-server-business/src/main/java/de/digitalcollections/cudami/server/business/api/service/identifiable/entity/work/SeriesService.java
+++
@@ -1,5 +0,0 @@
-package de.digitalcollections.cudami.server.business.api.service.identifiable.entity.work;
-
-import de.digitalcollections.model.identifiable.entity.manifestation.Series;
-
-public interface SeriesService extends WorkService<Series> {}
