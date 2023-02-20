d56b1533b6c2a7fc01ca1eb0c79b61c9bdeb19f8
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2023-02-20 12:19:40 +0100

    Fix duplicate migration

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/resources/de/digitalcollections/cudami/server/backend/impl/database/migration/V14.04.00__DDL_drop_tags_and_recreate.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS tags;
-
-CREATE TABLE IF NOT EXISTS tags (
-  uuid UUID PRIMARY KEY,
-  value VARCHAR collate "ucs_basic",
-  created TIMESTAMP NOT NULL,
-  last_modified TIMESTAMP NOT NULL,
-  UNIQUE (value)
-);
