db306287cacf4f2a1aaf7a0be857cdede7764a3a
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2023-03-20 16:33:05 +0100

    Delete method with fixed list size and search term, use find with paging instead

--- dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/identifiable/CudamiIdentifierTypesClient.java
+++ dc-cudami-client/src/main/java/de/digitalcollections/cudami/client/identifiable/CudamiIdentifierTypesClient.java
@@ -5,23 +5,13 @@ import de.digitalcollections.cudami.client.CudamiRestClient;
 import de.digitalcollections.model.exception.TechnicalException;
 import de.digitalcollections.model.exception.http.client.ResourceNotFoundException;
 import de.digitalcollections.model.identifiable.IdentifierType;
-import de.digitalcollections.model.list.paging.PageRequest;
-import de.digitalcollections.model.list.paging.PageResponse;
 import java.net.http.HttpClient;
-import java.util.List;
 import java.util.UUID;
 
 public class CudamiIdentifierTypesClient extends CudamiRestClient<IdentifierType> {
 
   public CudamiIdentifierTypesClient(HttpClient http, String serverUrl, ObjectMapper mapper) {
     super(http, serverUrl, IdentifierType.class, mapper, API_VERSION_PREFIX + "/identifiertypes");
-  }
-
-  public List<IdentifierType> find(String searchTerm, int maxResults) throws TechnicalException {
-    // FIXME replace with filtering
-    PageRequest pageRequest = new PageRequest(searchTerm, 0, maxResults, null);
-    PageResponse<IdentifierType> response = find(pageRequest);
-    return response.getContent();
   }
 
   public IdentifierType getByNamespace(String namespace) throws TechnicalException {
