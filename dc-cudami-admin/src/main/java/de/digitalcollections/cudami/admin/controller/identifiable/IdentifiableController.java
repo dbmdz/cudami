69c7c95032023cd655b9a9d8f26ea2ca2efb7ec5
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2023-03-22 15:53:49 +0100

    Extend AbstractIdentifiablesController and refactor find method

--- dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/IdentifiableController.java
+++ dc-cudami-admin/src/main/java/de/digitalcollections/cudami/admin/controller/identifiable/IdentifiableController.java
@@ -1,6 +1,6 @@
 package de.digitalcollections.cudami.admin.controller.identifiable;
 
-import de.digitalcollections.commons.springmvc.controller.AbstractController;
+import de.digitalcollections.cudami.admin.business.i18n.LanguageService;
 import de.digitalcollections.cudami.client.CudamiClient;
 import de.digitalcollections.cudami.client.identifiable.CudamiIdentifiablesClient;
 import de.digitalcollections.cudami.client.identifiable.entity.CudamiHeadwordEntriesClient;
@@ -11,6 +11,7 @@ import de.digitalcollections.model.identifiable.IdentifiableObjectType;
 import de.digitalcollections.model.identifiable.entity.HeadwordEntry;
 import de.digitalcollections.model.list.paging.PageRequest;
 import de.digitalcollections.model.list.paging.PageResponse;
+import de.digitalcollections.model.list.sorting.Order;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Controller;
@@ -21,13 +22,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class IdentifiableController extends AbstractController {
+public class IdentifiableController
+    extends AbstractIdentifiablesController<Identifiable, CudamiIdentifiablesClient<Identifiable>> {
 
   CudamiHeadwordEntriesClient headwordEntriesService;
-  CudamiIdentifiablesClient service;
 
-  public IdentifiableController(CudamiClient client) {
-    this.service = client.forIdentifiables();
+  public IdentifiableController(CudamiClient client, LanguageService languageService) {
+    super(client.forIdentifiables(), languageService);
     this.headwordEntriesService = client.forHeadwordEntries();
   }
 
@@ -37,12 +38,12 @@ public class IdentifiableController extends AbstractController {
     switch (identifiableObjectType) {
       case ARTICLE:
         return "forward:/articles/" + uuid;
-        //        List<SubtopicImpl> subtopics =
+        // List<SubtopicImpl> subtopics =
         // subtopicService.getSubtopicsOfEntity(entity.getUuid());
-        //        if (!subtopics.isEmpty()) {
-        //          Subtopic subtopic = subtopics.get(0);
-        //          return "forward:/knowledge/node/" + subtopic.getUuid().toString();
-        //        }
+        // if (!subtopics.isEmpty()) {
+        // Subtopic subtopic = subtopics.get(0);
+        // return "forward:/knowledge/node/" + subtopic.getUuid().toString();
+        // }
 
       case COLLECTION:
         return "forward:/collections/" + uuid;
@@ -52,36 +53,36 @@ public class IdentifiableController extends AbstractController {
         return "forward:/digitalobjects/" + uuid;
       case GEO_LOCATION:
         return "forward:/geolocations/" + uuid;
-        //        GeoLocation geoLocation = geoLocationsService.findOne(entity.getUuid());
-        //        GeoLocationType geoLocationType = geoLocation.getGeoLocationType();
-        //        switch (geoLocationType) {
-        //          case CANYON:
-        //            return "forward:/geo/canyons/" + uuid;
-        //          case CAVE:
-        //            return "forward:/geo/caves/" + uuid;
-        //          case CONTINENT:
-        //            return "forward:/geo/continents/" + uuid;
-        //          case COUNTRY:
-        //            return "forward:/geo/countries/" + uuid;
-        //          case CREEK:
-        //            return "forward:/geo/creeks/" + uuid;
-        //          case HUMAN_SETTLEMENT:
-        //            return "forward:/geo/human_settlements/" + uuid;
-        //          case LAKE:
-        //            return "forward:/geo/lakes/" + uuid;
-        //          case MOUNTAIN:
-        //            return "forward:/geo/mountains/" + uuid;
-        //          case OCEAN:
-        //            return "forward:/geo/oceans/" + uuid;
-        //          case RIVER:
-        //            return "forward:/geo/rivers/" + uuid;
-        //          case SEA:
-        //            return "forward:/geo/seas/" + uuid;
-        //          case STILL_WATERS:
-        //            return "forward:/geo/still_waters/" + uuid;
-        //          case VALLEY:
-        //            return "forward:/geo/valleys/" + uuid;
-        //        }
+        // GeoLocation geoLocation = geoLocationsService.findOne(entity.getUuid());
+        // GeoLocationType geoLocationType = geoLocation.getGeoLocationType();
+        // switch (geoLocationType) {
+        // case CANYON:
+        // return "forward:/geo/canyons/" + uuid;
+        // case CAVE:
+        // return "forward:/geo/caves/" + uuid;
+        // case CONTINENT:
+        // return "forward:/geo/continents/" + uuid;
+        // case COUNTRY:
+        // return "forward:/geo/countries/" + uuid;
+        // case CREEK:
+        // return "forward:/geo/creeks/" + uuid;
+        // case HUMAN_SETTLEMENT:
+        // return "forward:/geo/human_settlements/" + uuid;
+        // case LAKE:
+        // return "forward:/geo/lakes/" + uuid;
+        // case MOUNTAIN:
+        // return "forward:/geo/mountains/" + uuid;
+        // case OCEAN:
+        // return "forward:/geo/oceans/" + uuid;
+        // case RIVER:
+        // return "forward:/geo/rivers/" + uuid;
+        // case SEA:
+        // return "forward:/geo/seas/" + uuid;
+        // case STILL_WATERS:
+        // return "forward:/geo/still_waters/" + uuid;
+        // case VALLEY:
+        // return "forward:/geo/valleys/" + uuid;
+        // }
       case HEADWORD_ENTRY:
         HeadwordEntry headwordEntry = headwordEntriesService.getByUuid(identifiable.getUuid());
         UUID headwordUuid = headwordEntry.getHeadword().getUuid();
@@ -104,10 +105,25 @@ public class IdentifiableController extends AbstractController {
 
   @GetMapping(value = "/identifiables")
   @ResponseBody
-  public List<Identifiable> find(@RequestParam(name = "term") String searchTerm)
+  public List<Identifiable> find(
+      @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
+      @RequestParam(name = "pageSize", required = false, defaultValue = "25") int pageSize,
+      @RequestParam(name = "searchField", required = false, defaultValue = "label")
+          String searchField,
+      @RequestParam(name = "term", required = false) String searchTerm,
+      @RequestParam(name = "sortBy", required = false) List<Order> sortBy)
       throws TechnicalException {
-    // FIXME replace with filtering
-    PageRequest pageRequest = new PageRequest(searchTerm, 0, 25, null);
+    // TODO: find code using "term" instead "searchTerm" and change it to "searchTerm"
+    String dataLanguage = null;
+    PageRequest pageRequest =
+        createPageRequest(
+            Identifiable.class,
+            pageNumber,
+            pageSize,
+            sortBy,
+            searchField,
+            searchTerm,
+            dataLanguage);
     PageResponse<Identifiable> response = service.find(pageRequest);
     return response.getContent();
   }
@@ -115,7 +131,8 @@ public class IdentifiableController extends AbstractController {
   @GetMapping(value = {"/identifiables/{namespace:[a-zA-Z_\\-]+}:{id:.+}"})
   public String view(@PathVariable String namespace, @PathVariable String id, Model model)
       throws TechnicalException, ResourceNotFoundException {
-    Identifiable identifiable = service.getByIdentifier(namespace, id);
+    Identifiable identifiable =
+        ((CudamiIdentifiablesClient) service).getByIdentifier(namespace, id);
     if (identifiable == null) {
       throw new ResourceNotFoundException("get entity by identifier with " + namespace + ":" + id);
     }
