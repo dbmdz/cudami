911133a090e8d5212ffc4932e42bfc3a09789140
David Förster <david.foerster@bsb-muenchen.de>
2023-04-28 13:19:59 +0200

    Fix `LocalDateRangeMapper` for BC dates

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/type/LocalDateRangeMapper.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/backend/impl/jdbi/type/LocalDateRangeMapper.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Type;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,10 +15,27 @@ import org.jdbi.v3.core.argument.ArgumentFactory;
 import org.jdbi.v3.core.config.ConfigRegistry;
 import org.jdbi.v3.core.mapper.ColumnMapper;
 import org.jdbi.v3.core.statement.StatementContext;
+import org.springframework.util.StringUtils;
 
 public class LocalDateRangeMapper implements ArgumentFactory, ColumnMapper<LocalDateRange> {
 
+  private static final DateTimeFormatter formatter =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd GG", Locale.ROOT);
+
   public LocalDateRangeMapper() {}
+
+  private String toSqlDate(LocalDate date) {
+    if (date == null) return "";
+    // SQL does not need AD era
+    return "\"%s\"".formatted(date.format(formatter).replaceAll("\\sAD$", ""));
+  }
+
+  private LocalDate fromSqlDate(String date) {
+    if (!StringUtils.hasText(date)) return null;
+    // we must add the era if missing
+    if (!date.matches(".+\\s(BC|AD)$")) date += " AD";
+    return LocalDate.parse(date, formatter);
+  }
 
   @Override
   public Optional<Argument> build(Type type, Object value, ConfigRegistry config) {
@@ -26,7 +45,8 @@ public class LocalDateRangeMapper implements ArgumentFactory, ColumnMapper<Local
     LocalDateRange v = (LocalDateRange) value;
     return Optional.of(
         (position, statement, ctx) -> {
-          statement.setString(position, String.format("[%s,%s]", v.getStart(), v.getEnd()));
+          statement.setString(
+              position, String.format("[%s,%s]", toSqlDate(v.getStart()), toSqlDate(v.getEnd())));
         });
   }
 
@@ -35,18 +55,19 @@ public class LocalDateRangeMapper implements ArgumentFactory, ColumnMapper<Local
       throws SQLException {
     String value = r.getString(columnNumber);
     if (value == null) return null;
+    // worst case: ["2023-01-01 BC",2023-01-02)
     Matcher valueParts =
         Pattern.compile(
-                "^(?<lowbound>\\[|\\()(?<lower>[\\d-]+),(?<upper>[\\d-]+)(?<upbound>\\]|\\))$")
+                "^(?<lowbound>[\\[(])\"?(?<lower>[\\d-]+(\\s\\w{2})?)?\"?,\"?(?<upper>[\\d-]+(\\s\\w{2})?)?\"?(?<upbound>[)\\]])$")
             .matcher(value);
     if (!valueParts.find()) return null;
-    LocalDate lower = LocalDate.parse(valueParts.group("lower"));
-    LocalDate upper = LocalDate.parse(valueParts.group("upper"));
-    if (valueParts.group("lowbound").equals("(")) {
+    LocalDate lower = fromSqlDate(valueParts.group("lower"));
+    LocalDate upper = fromSqlDate(valueParts.group("upper"));
+    if (lower != null && valueParts.group("lowbound").equals("(")) {
       // lower bound is not included so we add one day
       lower = lower.plusDays(1);
     }
-    if (valueParts.group("upbound").equals(")")) {
+    if (upper != null && valueParts.group("upbound").equals(")")) {
       // upper bound is not included so we subtract one day
       upper = upper.minusDays(1);
     }
