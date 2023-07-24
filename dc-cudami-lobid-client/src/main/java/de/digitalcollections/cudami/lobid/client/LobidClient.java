89463e8be6c5dd9298d19c99135f2fd376ad5b03
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2023-07-24 16:46:18 +0200

    Complete missing Lobid-Clients and additional field mappings

--- dc-cudami-lobid-client/src/main/java/de/digitalcollections/cudami/lobid/client/LobidClient.java
+++ dc-cudami-lobid-client/src/main/java/de/digitalcollections/cudami/lobid/client/LobidClient.java
@@ -1,7 +1,7 @@
 package de.digitalcollections.cudami.lobid.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.digitalcollections.cudami.lobid.client.model.jackson.LobidObjectMapper;
+import de.digitalcollections.lobid.jackson.LobidObjectMapper;
 import java.net.http.HttpClient;
 import java.time.Duration;
 
@@ -9,9 +9,26 @@ public class LobidClient {
 
   protected final HttpClient http;
   private final LobidCorporateBodiesClient lobidCorporateBodiesClient;
+  private final LobidEventsClient lobidEventsClient;
+  private final LobidGeoLocationsClient lobidGeoLocationsClient;
+  private final LobidHumanSettlementsClient lobidHumanSettlementsClient;
+  private final LobidPersonsClient lobidPersonsClient;
+  private final LobidSubjectsClient lobidSubjectsClient;
+  private final LobidWorksClient lobidWorksClient;
 
   public LobidClient() {
     this("https://lobid.org", new LobidObjectMapper());
+  }
+
+  public LobidClient(HttpClient http, String serverUrl, ObjectMapper mapper) {
+    this.http = http;
+    this.lobidCorporateBodiesClient = new LobidCorporateBodiesClient(http, serverUrl, mapper);
+    this.lobidEventsClient = new LobidEventsClient(http, serverUrl, mapper);
+    this.lobidGeoLocationsClient = new LobidGeoLocationsClient(http, serverUrl, mapper);
+    this.lobidHumanSettlementsClient = new LobidHumanSettlementsClient(http, serverUrl, mapper);
+    this.lobidPersonsClient = new LobidPersonsClient(http, serverUrl, mapper);
+    this.lobidSubjectsClient = new LobidSubjectsClient(http, serverUrl, mapper);
+    this.lobidWorksClient = new LobidWorksClient(http, serverUrl, mapper);
   }
 
   public LobidClient(String serverUrl, ObjectMapper mapper) {
@@ -24,12 +41,31 @@ public class LobidClient {
         mapper);
   }
 
-  public LobidClient(HttpClient http, String serverUrl, ObjectMapper mapper) {
-    this.http = http;
-    this.lobidCorporateBodiesClient = new LobidCorporateBodiesClient(http, serverUrl, mapper);
-  }
-
   public LobidCorporateBodiesClient forCorporateBodies() {
     return lobidCorporateBodiesClient;
+  }
+
+  public LobidEventsClient forEvents() {
+    return lobidEventsClient;
+  }
+
+  public LobidGeoLocationsClient forGeoLocations() {
+    return lobidGeoLocationsClient;
+  }
+
+  public LobidHumanSettlementsClient forHumanSettlements() {
+    return lobidHumanSettlementsClient;
+  }
+
+  public LobidPersonsClient forPersons() {
+    return lobidPersonsClient;
+  }
+
+  public LobidSubjectsClient forSubjects() {
+    return lobidSubjectsClient;
+  }
+
+  public LobidWorksClient forWorks() {
+    return lobidWorksClient;
   }
 }
--- dc-cudami-lobid-client/src/main/java/de/digitalcollections/cudami/lobid/client/LobidCorporateBodiesClient.java
+++ dc-cudami-lobid-client/src/main/java/de/digitalcollections/cudami/lobid/client/LobidCorporateBodiesClient.java
@@ -1,20 +1,11 @@
 package de.digitalcollections.cudami.lobid.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.digitalcollections.cudami.lobid.client.model.LobidCorporateBody;
-import de.digitalcollections.cudami.lobid.client.model.LobidDepiction;
-import de.digitalcollections.cudami.lobid.client.model.LobidHomepage;
+import de.digitalcollections.cudami.lobid.client.mapper.Lobid2DCModelMapper;
+import de.digitalcollections.lobid.model.LobidCorporateBody;
 import de.digitalcollections.model.exception.TechnicalException;
-import de.digitalcollections.model.file.MimeType;
-import de.digitalcollections.model.identifiable.Identifier;
 import de.digitalcollections.model.identifiable.entity.agent.CorporateBody;
-import de.digitalcollections.model.identifiable.resource.ImageFileResource;
-import de.digitalcollections.model.text.LocalizedText;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.util.List;
-import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,50 +20,7 @@ public class LobidCorporateBodiesClient extends LobidBaseClient<LobidCorporateBo
   public CorporateBody getByGndId(String gndId) throws TechnicalException {
     LobidCorporateBody lobidCorporateBody =
         doGetRequestForObject(String.format("/gnd/%s.json", gndId));
-    CorporateBody corporateBody = mapLobidToModel(lobidCorporateBody);
-    return corporateBody;
-  }
-
-  private CorporateBody mapLobidToModel(LobidCorporateBody lobidCorporateBody) {
-    CorporateBody corporateBody = new CorporateBody();
-
-    // label
-    String labelText = lobidCorporateBody.getPreferredName();
-    List<String> abbreviatedNameForTheCorporateBody =
-        lobidCorporateBody.getAbbreviatedNameForTheCorporateBody();
-    if (labelText != null && abbreviatedNameForTheCorporateBody != null) {
-      labelText = labelText + " (" + abbreviatedNameForTheCorporateBody.get(0) + ")";
-    }
-    corporateBody.setLabel(new LocalizedText(Locale.GERMAN, labelText));
-
-    // identifier
-    String gndIdentifier = lobidCorporateBody.getGndIdentifier();
-    if (gndIdentifier != null) {
-      corporateBody.addIdentifier(Identifier.builder().namespace("gnd").id(gndIdentifier).build());
-    }
-
-    // homepage
-    List<LobidHomepage> homepage = lobidCorporateBody.getHomepage();
-    if (homepage != null && !homepage.isEmpty()) {
-      String homepageId = homepage.get(0).getId();
-      try {
-        corporateBody.setHomepageUrl(URI.create(homepageId).toURL());
-      } catch (MalformedURLException ex) {
-        LOGGER.warn("Invalid homepage URL: " + homepageId);
-      }
-    }
-
-    // preview image = logo
-    List<LobidDepiction> depiction = lobidCorporateBody.getDepiction();
-    if (depiction != null && !depiction.isEmpty()) {
-      String thumbnailUrl = depiction.get(0).getThumbnail();
-      ImageFileResource previewImage = new ImageFileResource();
-      previewImage.setLabel(new LocalizedText(Locale.GERMAN, "Logo " + labelText));
-      previewImage.setUri(URI.create(thumbnailUrl));
-      previewImage.setMimeType(MimeType.MIME_IMAGE);
-      corporateBody.setPreviewImage(previewImage);
-    }
-
+    CorporateBody corporateBody = Lobid2DCModelMapper.mapToCorporateBody(lobidCorporateBody, null);
     return corporateBody;
   }
 }
