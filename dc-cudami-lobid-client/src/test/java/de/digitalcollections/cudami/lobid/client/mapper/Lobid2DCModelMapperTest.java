47c700f5066833c26512de4b44c60f9de034b59c
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2023-07-24 16:46:18 +0200

    Complete missing Lobid-Clients and additional field mappings

---
+++ dc-cudami-lobid-client/src/test/java/de/digitalcollections/cudami/lobid/client/mapper/Lobid2DCModelMapperTest.java
@@ -0,0 +1,21 @@
+package de.digitalcollections.cudami.lobid.client.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import de.digitalcollections.lobid.model.LobidGeometry;
+import de.digitalcollections.model.geo.CoordinateLocation;
+import org.junit.jupiter.api.Test;
+
+class Lobid2DCModelMapperTest {
+
+  @Test
+  void testMapGeometryToCoordinateLocation() {
+    LobidGeometry lobidGeometry = new LobidGeometry();
+    lobidGeometry.setType("Point");
+    lobidGeometry.setAsWKT(new String[] {"Point ( +012.573850 -048.881259 )"});
+    CoordinateLocation coordinateLocation =
+        Lobid2DCModelMapper.mapGeometryToCoordinateLocation(lobidGeometry);
+    assertEquals(coordinateLocation.getLatitude(), 12.573850d);
+    assertEquals(coordinateLocation.getLongitude(), -48.881259d);
+  }
+}
