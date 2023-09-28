13379534184b4d2a2b0ac0427e77f78dece0bfa3
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2023-09-28 08:50:09 +0200

    Fix code style

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/test/java/de/digitalcollections/cudami/server/backend/impl/model/TestModelFixture.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/test/java/de/digitalcollections/cudami/server/backend/impl/model/TestModelFixture.java
@@ -19,8 +19,12 @@ public class TestModelFixture {
 
   public static <E extends Entity> E createEntity(
       Class<E> cls, Map<Locale, String> labelMap, Map<Locale, String> descriptionMap)
-      throws InstantiationException, IllegalAccessException, IllegalArgumentException,
-          InvocationTargetException, NoSuchMethodException, SecurityException {
+      throws InstantiationException,
+          IllegalAccessException,
+          IllegalArgumentException,
+          InvocationTargetException,
+          NoSuchMethodException,
+          SecurityException {
     E entity = cls.getConstructor().newInstance();
     LocalizedText labelText = createLocalizedText(labelMap);
     entity.setLabel(labelText);
@@ -39,15 +43,23 @@ public class TestModelFixture {
 
   public static DigitalObject createDigitalObject(
       Map<Locale, String> labelMap, Map<Locale, String> descriptionMap)
-      throws InstantiationException, IllegalAccessException, IllegalArgumentException,
-          InvocationTargetException, NoSuchMethodException, SecurityException {
+      throws InstantiationException,
+          IllegalAccessException,
+          IllegalArgumentException,
+          InvocationTargetException,
+          NoSuchMethodException,
+          SecurityException {
     return createEntity(DigitalObject.class, labelMap, descriptionMap);
   }
 
   public static EntityRelation createEntityRelation(
       Map<Locale, String> subjectLabels, Map<Locale, String> objectLabels, String predicate)
-      throws InstantiationException, IllegalAccessException, IllegalArgumentException,
-          InvocationTargetException, NoSuchMethodException, SecurityException {
+      throws InstantiationException,
+          IllegalAccessException,
+          IllegalArgumentException,
+          InvocationTargetException,
+          NoSuchMethodException,
+          SecurityException {
     Entity subject = createEntity(Entity.class, subjectLabels, Map.of());
     subject.setUuid(UUID.randomUUID());
     Entity object = createEntity(Entity.class, objectLabels, Map.of());
