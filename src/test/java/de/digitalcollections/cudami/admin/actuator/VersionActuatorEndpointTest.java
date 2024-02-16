773cfddabcafd027a37d2d92284a017304e926ec
David Förster <david.foerster@bsb-muenchen.de>
2024-02-16 18:55:13 +0100

    Fix pom and test

--- src/test/java/de/digitalcollections/cudami/admin/actuator/VersionActuatorEndpointTest.java
+++ src/test/java/de/digitalcollections/cudami/admin/actuator/VersionActuatorEndpointTest.java
@@ -11,10 +11,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {VersionInfo.class, VersionActuatorEndpoint.class})
+@ActiveProfiles("default")
 @SpringBootConfiguration()
 class VersionActuatorEndpointTest {
 
