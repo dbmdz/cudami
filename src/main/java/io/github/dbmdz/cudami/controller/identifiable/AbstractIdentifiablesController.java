d4848b6152deea5d5db6c33bca051e0dc829d194
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2024-09-04 07:38:24 +0200

    Fix code style

--- src/main/java/io/github/dbmdz/cudami/controller/identifiable/AbstractIdentifiablesController.java
+++ src/main/java/io/github/dbmdz/cudami/controller/identifiable/AbstractIdentifiablesController.java
@@ -126,12 +126,12 @@ public class AbstractIdentifiablesController<
         return "forward:/fileresources/" + uuid;
       case ARTICLE:
         return "forward:/articles/" + uuid;
-        // List<SubtopicImpl> subtopics =
-        // subtopicService.getSubtopicsOfEntity(entity.getUuid());
-        // if (!subtopics.isEmpty()) {
-        // Subtopic subtopic = subtopics.get(0);
-        // return "forward:/knowledge/node/" + subtopic.getUuid().toString();
-        // }
+      // List<SubtopicImpl> subtopics =
+      // subtopicService.getSubtopicsOfEntity(entity.getUuid());
+      // if (!subtopics.isEmpty()) {
+      // Subtopic subtopic = subtopics.get(0);
+      // return "forward:/knowledge/node/" + subtopic.getUuid().toString();
+      // }
 
       case COLLECTION:
         return "forward:/collections/" + uuid;
@@ -144,36 +144,36 @@ public class AbstractIdentifiablesController<
       case GEO_LOCATION:
       case HUMAN_SETTLEMENT:
         return "forward:/geolocations/" + uuid;
-        // GeoLocation geoLocation = geoLocationsService.findOne(entity.getUuid());
-        // GeoLocationType geoLocationType = geoLocation.getGeoLocationType();
-        // switch (geoLocationType) {
-        // case CANYON:
-        // return "forward:/geo/canyons/" + uuid;
-        // case CAVE:
-        // return "forward:/geo/caves/" + uuid;
-        // case CONTINENT:
-        // return "forward:/geo/continents/" + uuid;
-        // case COUNTRY:
-        // return "forward:/geo/countries/" + uuid;
-        // case CREEK:
-        // return "forward:/geo/creeks/" + uuid;
-        // case HUMAN_SETTLEMENT:
-        // return "forward:/geo/human_settlements/" + uuid;
-        // case LAKE:
-        // return "forward:/geo/lakes/" + uuid;
-        // case MOUNTAIN:
-        // return "forward:/geo/mountains/" + uuid;
-        // case OCEAN:
-        // return "forward:/geo/oceans/" + uuid;
-        // case RIVER:
-        // return "forward:/geo/rivers/" + uuid;
-        // case SEA:
-        // return "forward:/geo/seas/" + uuid;
-        // case STILL_WATERS:
-        // return "forward:/geo/still_waters/" + uuid;
-        // case VALLEY:
-        // return "forward:/geo/valleys/" + uuid;
-        // }
+      // GeoLocation geoLocation = geoLocationsService.findOne(entity.getUuid());
+      // GeoLocationType geoLocationType = geoLocation.getGeoLocationType();
+      // switch (geoLocationType) {
+      // case CANYON:
+      // return "forward:/geo/canyons/" + uuid;
+      // case CAVE:
+      // return "forward:/geo/caves/" + uuid;
+      // case CONTINENT:
+      // return "forward:/geo/continents/" + uuid;
+      // case COUNTRY:
+      // return "forward:/geo/countries/" + uuid;
+      // case CREEK:
+      // return "forward:/geo/creeks/" + uuid;
+      // case HUMAN_SETTLEMENT:
+      // return "forward:/geo/human_settlements/" + uuid;
+      // case LAKE:
+      // return "forward:/geo/lakes/" + uuid;
+      // case MOUNTAIN:
+      // return "forward:/geo/mountains/" + uuid;
+      // case OCEAN:
+      // return "forward:/geo/oceans/" + uuid;
+      // case RIVER:
+      // return "forward:/geo/rivers/" + uuid;
+      // case SEA:
+      // return "forward:/geo/seas/" + uuid;
+      // case STILL_WATERS:
+      // return "forward:/geo/still_waters/" + uuid;
+      // case VALLEY:
+      // return "forward:/geo/valleys/" + uuid;
+      // }
       case HEADWORD_ENTRY:
         HeadwordEntry headwordEntry =
             cudamiClient.forHeadwordEntries().getByUuid(identifiable.getUuid());
